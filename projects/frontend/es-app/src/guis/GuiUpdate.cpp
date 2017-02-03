5cd4e70113fb62731a38f34c42158a01cb93920b
digitalLumberjack <digitallumberjack@gmail.com>
2017-02-03 08:06:38 +0100

    CLION formatting of GuiUpdate.cpp

--- projects/frontend/es-app/src/guis/GuiUpdate.cpp
+++ projects/frontend/es-app/src/guis/GuiUpdate.cpp
@@ -1,165 +1,153 @@
 #include "guis/GuiUpdate.h"
 #include "guis/GuiMsgBox.h"
 
-#include "Window.h"
-#include <boost/thread.hpp>
-#include <string>
 #include "Log.h"
-#include "Settings.h"
 #include "RecalboxSystem.h"
 #include "Locale.h"
 
-GuiUpdate::GuiUpdate(Window* window) : GuiComponent(window), mBusyAnim(window)
-{
-	setSize((float)Renderer::getScreenWidth(), (float)Renderer::getScreenHeight());
-        mLoading = true;
-        mPingHandle = new boost::thread(boost::bind(&GuiUpdate::threadPing, this));
-        mBusyAnim.setSize(mSize);
+GuiUpdate::GuiUpdate(Window *window) : GuiComponent(window), mBusyAnim(window) {
+    setSize((float) Renderer::getScreenWidth(), (float) Renderer::getScreenHeight());
+    mLoading = true;
+    mPingHandle = new boost::thread(boost::bind(&GuiUpdate::threadPing, this));
+    mBusyAnim.setSize(mSize);
 }
 
-GuiUpdate::~GuiUpdate()
-{
-	mPingHandle->join();
+GuiUpdate::~GuiUpdate() {
+    mPingHandle->join();
 }
 
-bool GuiUpdate::input(InputConfig* config, Input input)
-{
-        return false;
+bool GuiUpdate::input(InputConfig *config, Input input) {
+    return false;
 }
 
-std::vector<HelpPrompt> GuiUpdate::getHelpPrompts()
-{
-	return std::vector<HelpPrompt>();
+std::vector<HelpPrompt> GuiUpdate::getHelpPrompts() {
+    return std::vector<HelpPrompt>();
 }
 
-void GuiUpdate::render(const Eigen::Affine3f& parentTrans)
-{
-        Eigen::Affine3f trans = parentTrans * getTransform();
+void GuiUpdate::render(const Eigen::Affine3f &parentTrans) {
+    Eigen::Affine3f trans = parentTrans * getTransform();
 
-        renderChildren(trans);
+    renderChildren(trans);
 
-        Renderer::setMatrix(trans);
-        Renderer::drawRect(0.f, 0.f, mSize.x(), mSize.y(), 0x00000011);
+    Renderer::setMatrix(trans);
+    Renderer::drawRect(0.f, 0.f, mSize.x(), mSize.y(), 0x00000011);
 
-        if(mLoading)
+    if (mLoading)
         mBusyAnim.render(trans);
 
 }
 
 void GuiUpdate::update(int deltaTime) {
-        GuiComponent::update(deltaTime);
-        mBusyAnim.update(deltaTime);
-        
-        Window* window = mWindow;
-        if(mState == 1){
-            window->pushGui(
-			    new GuiMsgBox(window, _("REALLY UPDATE?"), _("YES"),
-                           [this] { 
-                               mState = 2;
-                               mLoading = true;
-                               mHandle = new boost::thread(boost::bind(&GuiUpdate::threadUpdate, this));
+    GuiComponent::update(deltaTime);
+    mBusyAnim.update(deltaTime);
 
-					  }, _("NO"), [this] {
-                               mState = -1;
-                           })
-                                   
-            );
-            mState = 0;
-        }
-         
-        if(mState == 3){
-            window->pushGui(
-			    new GuiMsgBox(window, _("NETWORK CONNECTION NEEDED"), _("OK"), 
-                [this] {
-                    mState = -1;
-                })
-            );
-            mState = 0;
-        }
-        if(mState == 4){
-            window->pushGui(
-			    new GuiMsgBox(window, _("UPDATE DOWNLOADED, THE SYSTEM WILL NOW REBOOT"), _("OK"),
-                [this] {
-                    if(runRestartCommand() != 0) {
-                        LOG(LogWarning) << "Reboot terminated with non-zero result!";
-                    }
-                })
-            );
-            mState = 0;
-        }
-        if(mState == 5){
-            window->pushGui(
-                    new GuiMsgBox(window, mResult.first, _("OK"),
-                                  [this] {
-                                      mState = -1;
+    Window *window = mWindow;
+    if (mState == 1) {
+        window->pushGui(
+                new GuiMsgBox(window, _("REALLY UPDATE?"), _("YES"),
+                              [this] {
+                                  mState = 2;
+                                  mLoading = true;
+                                  mHandle = new boost::thread(boost::bind(&GuiUpdate::threadUpdate, this));
+
+                              }, _("NO"), [this] {
+                            mState = -1;
+                        })
+
+        );
+        mState = 0;
+    }
+
+    if (mState == 3) {
+        window->pushGui(
+                new GuiMsgBox(window, _("NETWORK CONNECTION NEEDED"), _("OK"),
+                              [this] {
+                                  mState = -1;
+                              })
+        );
+        mState = 0;
+    }
+    if (mState == 4) {
+        window->pushGui(
+                new GuiMsgBox(window, _("UPDATE DOWNLOADED, THE SYSTEM WILL NOW REBOOT"), _("OK"),
+                              [this] {
+                                  if (runRestartCommand() != 0) {
+                                      LOG(LogWarning) << "Reboot terminated with non-zero result!";
                                   }
-                    )
-            );
-            mState = 0;
-        }
-        if(mState == 6){
-            window->pushGui(
-			    new GuiMsgBox(window, _("NO UPDATE AVAILABLE"), _("OK"), 
-                [this] {
-                    mState = -1;
-                }));
-            mState = 0;
-        }
-        if(mState == -1){
-            delete this;
-        }
+                              })
+        );
+        mState = 0;
+    }
+    if (mState == 5) {
+        window->pushGui(
+                new GuiMsgBox(window, mResult.first, _("OK"),
+                              [this] {
+                                  mState = -1;
+                              }
+                )
+        );
+        mState = 0;
+    }
+    if (mState == 6) {
+        window->pushGui(
+                new GuiMsgBox(window, _("NO UPDATE AVAILABLE"), _("OK"),
+                              [this] {
+                                  mState = -1;
+                              }));
+        mState = 0;
+    }
+    if (mState == -1) {
+        delete this;
+    }
 }
 
-void GuiUpdate::threadUpdate() 
-{
-    std::pair<std::string,int> updateStatus = RecalboxSystem::getInstance()->updateSystem();
-    if(updateStatus.second == 0){
+void GuiUpdate::threadUpdate() {
+    std::pair<std::string, int> updateStatus = RecalboxSystem::getInstance()->updateSystem();
+    if (updateStatus.second == 0) {
         this->onUpdateOk();
-    }else {
+    } else {
         this->onUpdateError(updateStatus);
-    }  
+    }
 }
 
-void GuiUpdate::threadPing() 
-{
-        if(RecalboxSystem::getInstance()->ping()){
-            if(RecalboxSystem::getInstance()->canUpdate()){
-                this->onUpdateAvailable();
-            }else {
-                this->onNoUpdateAvailable();
+void GuiUpdate::threadPing() {
+    if (RecalboxSystem::getInstance()->ping()) {
+        if (RecalboxSystem::getInstance()->canUpdate()) {
+            this->onUpdateAvailable();
+        } else {
+            this->onNoUpdateAvailable();
 
-            }
-        }else {
-            this->onPingError();
         }
+    } else {
+        this->onPingError();
+    }
 }
-void GuiUpdate::onUpdateAvailable() 
-{	
+
+void GuiUpdate::onUpdateAvailable() {
     mLoading = false;
-    LOG(LogInfo) << "update available" << "\n";	
+    LOG(LogInfo) << "update available" << "\n";
     mState = 1;
 }
-void GuiUpdate::onNoUpdateAvailable() 
-{	
+
+void GuiUpdate::onNoUpdateAvailable() {
     mLoading = false;
-    LOG(LogInfo) << "no update available" << "\n";	
+    LOG(LogInfo) << "no update available" << "\n";
     mState = 6;
 }
-void GuiUpdate::onPingError() 
-{
-    LOG(LogInfo) << "ping nok" << "\n";	
+
+void GuiUpdate::onPingError() {
+    LOG(LogInfo) << "ping nok" << "\n";
     mLoading = false;
     mState = 3;
 }
-void GuiUpdate::onUpdateError(std::pair<std::string, int> result)
-{
+
+void GuiUpdate::onUpdateError(std::pair<std::string, int> result) {
     mLoading = false;
     mState = 5;
     mResult = result;
 }
 
-void GuiUpdate::onUpdateOk()
-{
+void GuiUpdate::onUpdateOk() {
     mLoading = false;
     mState = 4;
 }
