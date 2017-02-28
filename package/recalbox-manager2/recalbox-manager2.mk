c2bae21bc34040a78fe00677793c8c33e70f9756
Subs <frog2wah@gmail.com>
2017-02-28 15:05:01 +0100

    recalbox-manager2: cleanup unnecessary lines

--- package/recalbox-manager2/recalbox-manager2.mk
+++ package/recalbox-manager2/recalbox-manager2.mk
@@ -3,19 +3,10 @@
 # RECALBOX_MANAGER2
 #
 ################################################################################
-# RECALBOX_MANAGER2_VERSION = v1.0-beta6
 RECALBOX_MANAGER2_VERSION = v2.0-beta
-RECALBOX_MANAGER2_SITE = $(call github,djlechuck,recalboxmanager,$(RECALBOX_MANAGER2_VERSION))
 RECALBOX_MANAGER2_SITE = git://github.com/DjLeChuck/recalbox-manager
 RECALBOX_MANAGER2_DEPENDENCIES = nodejs
 
-# NPM = $(TARGET_CONFIGURE_OPTS) \
-	# LD="$(TARGET_CXX)" \
-	# npm_config_arch=arm \
-	# npm_config_target_arch=arm \
-	# npm_config_build_from_source=true \
-	# npm_config_nodedir=$(BUILD_DIR)/nodejs-$(NODEJS_VERSION) \
-	# $(HOST_DIR)/usr/bin/npm
 
 define RECALBOX_MANAGER2_BUILD_CMDS		
 	$(NPM) --prefix $(@D) run installboth
