97b9a614ceb6880bb2a3e06a34761fbe4a9d3d66
Supernature2k <dj_xizor@hotmail.com>
2017-06-01 17:19:58 +0200

    update submodule + themes.mk

--- package/recalbox-themes/recalbox-themes.mk
+++ package/recalbox-themes/recalbox-themes.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-RECALBOX_THEMES_VERSION = 4.1.X
+RECALBOX_THEMES_VERSION = 4.2.X
 RECALBOX_THEMES_SITE = $(call github,recalbox,recalbox-themes,$(RECALBOX_THEMES_VERSION))
 
 define RECALBOX_THEMES_INSTALL_TARGET_CMDS
         mkdir -p $(TARGET_DIR)/recalbox/share_init/system/.emulationstation/themes/
-	cp -r $(@D)/themes/recalbox \
+	cp -r $(@D)/themes/recalbox-multi \
 		$(TARGET_DIR)/recalbox/share_init/system/.emulationstation/themes/
 endef
 
