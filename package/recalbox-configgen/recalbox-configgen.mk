2fa29f0ffd8b183e281bffc2d39d8a1d117d5b40
Subs <frog2wah@gmail.com>
2017-11-15 18:04:11 +0100

    configgen: videomode has a new auto feature

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = a39e0b845a8824cf778085704c9d584ab70806ab
+RECALBOX_CONFIGGEN_VERSION = d061490fd0d62c7e5134f1932a591fc455b80028
 
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
