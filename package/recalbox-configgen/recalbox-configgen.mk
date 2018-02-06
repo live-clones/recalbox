91481a37144cd2cee0b9b110858335bc1d245f4c
Subs <frog2wah@gmail.com>
2018-02-06 23:28:12 +0100

    moonlight: update to v2.4.6 + bump configgen accordingly + update the default .conf file

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = 3aa61e3f51a7325059c8f36b8de2d09ebcdf5993
+RECALBOX_CONFIGGEN_VERSION = d9ee3953192e32b5cb04cdd44eea94738cc7ac86
 
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
