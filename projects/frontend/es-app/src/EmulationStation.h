6af2524f3a72335621e44caa0a9095af996e81bf
Supernature2k <dj_xizor@hotmail.com>
2018-02-15 17:09:43 +0000

    Update EmulationStation.h

--- projects/frontend/es-app/src/EmulationStation.h
+++ projects/frontend/es-app/src/EmulationStation.h
@@ -1,7 +1,1 @@
-#pragma once
 
-// These numbers and strings need to be manually updated for a new version.
-// Do this version number update as the very last commit for the new release version.
-#define PROGRAM_VERSION_STRING "4.1.0-dev"
-
-#define PROGRAM_BUILT_STRING __DATE__ " - " __TIME__
