384e8c13fc386855c970758220204e45ac42d827
Subs <frog2wah@gmail.com>
2018-02-19 08:27:07 +0100

    bump theme and ES + update es_settings.cfg if theme is recalbox + update default es_settings.cfg

--- package/recalbox-themes/recalbox-themes.mk
+++ package/recalbox-themes/recalbox-themes.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-RECALBOX_THEMES_VERSION = b699d204426720a36d356394fce66b055482b18f
+RECALBOX_THEMES_VERSION = 87d1f048ae67da61c40428a7f96bc8292251ab67
 RECALBOX_THEMES_SITE = https://gitlab.com/recalbox/recalbox-themes.git
 RECALBOX_THEMES_SITE_METHOD = git
 
 define RECALBOX_THEMES_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/recalbox/share_init/system/.emulationstation/themes/
-	cp -r $(@D)/themes/recalbox \
+	cp -r $(@D)/themes/recalbox-next \
 		$(TARGET_DIR)/recalbox/share_init/system/.emulationstation/themes/
 endef
 
