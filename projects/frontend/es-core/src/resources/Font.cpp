3e6d88cef36a0ad2f2d982649b1c7a2a03b4a21f
Supernature2k <dj_xizor@hotmail.com>
2018-04-04 11:28:06 +0000

    Add ubuntu_condensed.ttf as fallback font

--- projects/frontend/es-core/src/resources/Font.cpp
+++ projects/frontend/es-core/src/resources/Font.cpp
@@ -384,6 +384,7 @@ std::vector<std::string> getFallbackFontPaths()
 
 	// TODO
 	const char* paths[] = {
+	    "/usr/share/fonts/truetype/ubuntu_condensed.ttf",
 		"/usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf",// japanese, chinese, present on Debian
 		":/fontawesome_webfont.ttf",
 		 "/usr/share/fonts/truetype/fontawesome-webfont.ttf",
