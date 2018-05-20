ec26ecca4732eec25590de97df4f1dd701af6c85
Jean-Baptiste Demonte <jbdemonte@gmail.com>
2018-05-20 23:55:55 +0200

    FlatFolder branch

--- package/recalbox-themes/recalbox-themes.mk
+++ package/recalbox-themes/recalbox-themes.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_THEMES_VERSION = 972656e560e408159010666424c8802722a2a6d9
+RECALBOX_THEMES_VERSION = 50be59a4bcdf517c224ec18d11ba10e7b7e13d2b
 RECALBOX_THEMES_SITE = https://gitlab.com/recalbox/recalbox-themes.git
 RECALBOX_THEMES_SITE_METHOD = git
 
