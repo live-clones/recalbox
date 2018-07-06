96e644625f9740b6596ae31acc441cd61f6da69c
Bkg2k <bkg2k9@gmail.com>
2018-07-07 00:12:13 +0200

    Combine image copy with ARGB conversion

--- projects/frontend/es-core/src/ImageIO.cpp
+++ projects/frontend/es-core/src/ImageIO.cpp
@@ -1,7 +1,5 @@
 #include "ImageIO.h"
 
-#include <memory.h>
-
 #include "Log.h"
 
 
@@ -10,20 +8,21 @@ std::vector<unsigned char> ImageIO::loadFromMemoryRGBA32(const unsigned char * d
 	std::vector<unsigned char> rawData;
 	width = 0;
 	height = 0;
-	FIMEMORY * fiMemory = FreeImage_OpenMemory((BYTE *)data, size);
-	if (fiMemory != nullptr) {
+	FIMEMORY * fiMemory = FreeImage_OpenMemory((BYTE *)data, (DWORD)size);
+	if (fiMemory != nullptr)
+	{
 		//detect the filetype from data
 		FREE_IMAGE_FORMAT format = FreeImage_GetFileTypeFromMemory(fiMemory);
 		if (format != FIF_UNKNOWN && FreeImage_FIFSupportsReading(format))
 		{
 			//file type is supported. load image
-			FIBITMAP * fiBitmap = FreeImage_LoadFromMemory(format, fiMemory);
+			FIBITMAP* fiBitmap = FreeImage_LoadFromMemory(format, fiMemory);
 			if (fiBitmap != nullptr)
 			{
 				//loaded. convert to 32bit if necessary
 				if (FreeImage_GetBPP(fiBitmap) != 32)
 				{
-					FIBITMAP * fiConverted = FreeImage_ConvertTo32Bits(fiBitmap);
+					FIBITMAP* fiConverted = FreeImage_ConvertTo32Bits(fiBitmap);
 					if (fiConverted != nullptr)
 					{
 						//free original bitmap data
@@ -31,33 +30,26 @@ std::vector<unsigned char> ImageIO::loadFromMemoryRGBA32(const unsigned char * d
 						fiBitmap = fiConverted;
 					}
 				}
-				if (fiBitmap != nullptr)
-				{
-					width = FreeImage_GetWidth(fiBitmap);
-					height = FreeImage_GetHeight(fiBitmap);
-					//loop through scanlines and add all pixel data to the return vector
-					//this is necessary, because width*height*bpp might not be == pitch
-					rawData.resize(width * height *4);
-					unsigned char* tempData = rawData.data();
-					for (int i = (int)height; --i >= 0; )
-					{
-						const BYTE * scanLine = FreeImage_GetScanLine(fiBitmap, i);
-						memcpy(tempData + (i * width * 4), scanLine, width * 4);
-					}
-					//convert from BGRA to RGBA
-					for(size_t i = 0; i < width*height; i++)
-					{
-						RGBQUAD bgra = ((RGBQUAD *)tempData)[i];
-						RGBQUAD rgba;
-						rgba.rgbBlue = bgra.rgbRed;
-						rgba.rgbGreen = bgra.rgbGreen;
-						rgba.rgbRed = bgra.rgbBlue;
-						rgba.rgbReserved = bgra.rgbReserved;
-						((RGBQUAD *)tempData)[i] = rgba;
-					}
-					//free bitmap data
-					FreeImage_Unload(fiBitmap);
-				}
+        width = FreeImage_GetWidth(fiBitmap);
+        height = FreeImage_GetHeight(fiBitmap);
+        // loop through scanlines and add all pixel data to the return vector
+        // this is necessary, because width*height*bpp might not be == pitch
+        // do on-the-fly argb to abgr convertion
+        rawData.resize(width * height *4);
+        unsigned char* tempData = rawData.data();
+        int w = (int)width;
+        for (int y = (int)height; --y >= 0; )
+        {
+          unsigned int* argb = (unsigned int*)FreeImage_GetScanLine(fiBitmap, y);
+          unsigned int* abgr = (unsigned int*)(tempData + (y * width * 4));
+          for(int x = w ; --x >= 0;)
+          {
+            unsigned int c = argb[x];
+            abgr[x] = (c & 0xFF00FF00) | ((c & 0xFF) << 16) | ((c >> 16) & 0xFF);
+          }
+        }
+        //free bitmap data
+        FreeImage_Unload(fiBitmap);
 			}
 			else
 			{
