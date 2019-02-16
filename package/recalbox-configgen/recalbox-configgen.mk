fe25de08a9d87c436a3b65efceabf1ddc65e708b
paradadf <paradad@outlook.com>
2019-02-16 01:02:30 +0000

    Bump configgen fba to fbalpha

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = 4492f1d56d9b4f64a9a9810935ff53c79b80b32d
+RECALBOX_CONFIGGEN_VERSION = df863bbc003fb47e984437d093f56f4a9794be2f
 
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
