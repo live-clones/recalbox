279333bcc9d134821fa8c4c85040daf9e7724424
digitalLumberjack <digitallumberjack@gmail.com>
2019-03-23 10:29:54 +0100

    feat(netlify): deploy index.html on archive.recalbox.com when releasing in production

--- updatesv2/prepare_assets.sh
+++ updatesv2/prepare_assets.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 set -eu
 
-if [ -z "${1}" -o -z "${2}" -o -z "${3}" -o -z "${4}" ]; then
-  echo "This script need 4 parameters"
+if [ -z "${1}" -o -z "${2}" -o -z "${3}" -o -z "${4}" -o -z "${5}" ]; then
+  echo "This script need 5 parameters"
   exit 1
 fi
 
 RELEASE_TYPE="${1}"
 IMAGE_DIR="${2}"
 RELEASE_DIR="${3}"
-SKIP_IMAGES="${4}"
+NETLIFY_DIR="${4}"
+SKIP_IMAGES="${5}"
 
 RELEASE_DIR_UPGRADE="${RELEASE_DIR}/v2/upgrade"
 rm -rf "${RELEASE_DIR}"
@@ -22,26 +23,6 @@ if [ "${SKIP_IMAGES}" == "true" ]; then
   rm -rf "${RELEASE_DIR_UPGRADE}"/*/*.img*
 fi
 
-# Noobs compatibility
-
-if [ "${RELEASE_TYPE}" == "prod" ]; then
-  UPGRADE_DIR="${IMAGE_DIR}/"
-  SOURCE_NOOBS_DIR="./noobs/"
-  NOOBS_DIR="${RELEASE_DIR}/v1/noobs"
-  mkdir -p "${NOOBS_DIR}"
-
-  # Copy skeleton
-  cp -r "${SOURCE_NOOBS_DIR}"/* "${NOOBS_DIR}"
-  # Copy dist files
-  for arch in $(ls "${UPGRADE_DIR}" | grep rpi); do
-    echo "Creating directory ${NOOBS_DIR}/${arch}"
-    mkdir -p "${NOOBS_DIR}/${arch}"
-    for file in $(ls "${UPGRADE_DIR}/${arch}" | grep ".tar.xz"); do
-      echo "Copying file ${UPGRADE_DIR}/${arch}/${file} to ${NOOBS_DIR}/${arch}/${file}"
-      cp "${UPGRADE_DIR}/${arch}/${file}" "${NOOBS_DIR}/${arch}/${file}"
-    done
-  done
-fi
 # Template html
 cp "${RELEASE_TYPE}.template.html" "${RELEASE_DIR}/index.html"
 
@@ -49,10 +30,14 @@ OLDIFS="${IFS}"
 IFS='
 '
 echo "Subsitute all env var in the template"
-for ENVVAR in $(env | grep "CI_\|GITLAB"); do
+for ENVVAR in $(env | grep "CI_\|GITLAB\|RECALBOX"); do
   echo "envvar=$ENVVAR"
   NAME="$(echo $ENVVAR | cut -d'=' -f1)"
   VALUE="$(echo $ENVVAR | cut -d'=' -f2-)"
   sed -i "s|${NAME}|${VALUE}|g" "${RELEASE_DIR}/index.html"
 done
 IFS="${OLDIFS}"
+
+# Netlify directory
+mkdir "${NETLIFY_DIR}"
+cp "${RELEASE_DIR}/index.html" "${NETLIFY_DIR}/index.html"
