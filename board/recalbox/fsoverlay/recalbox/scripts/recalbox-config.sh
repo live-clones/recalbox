36ebf4e9089297ea35b04f6a6b0c0ef279c76d8f
Yann MORERE <yann.morere@gmail.com>
2019-04-01 07:15:53 +0000

    Update recalbox-config.sh

--- board/recalbox/fsoverlay/recalbox/scripts/recalbox-config.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/recalbox-config.sh
@@ -10,6 +10,7 @@ command="$1"
 mode="$2"
 extra1="$3"
 extra2="$4"
+extra3="$5"
 postMode="${@:3}"
 arch=`cat /recalbox/recalbox.arch`
 
@@ -469,7 +470,7 @@ fi
 
 if [ "$command" == "module" ];then
     modulename="$extra1"
-    map="$extra2"
+    map="$extra2 $extra3"
     # remove in all cases
     rmmod /lib/modules/`uname -r`/extra/${modulename}.ko 2>&1 | recallog
 
