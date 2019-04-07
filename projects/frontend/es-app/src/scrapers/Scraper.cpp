ea0af15ba0c5bb3baa5dd706c1f3ed15c131c0aa
Bkg2k <bkg2k9@gmail.com>
2019-04-07 21:38:58 +0200

    fix(scrapper): disconnect ScreenScraper until the new scraper is available.

--- projects/frontend/es-app/src/scrapers/Scraper.cpp
+++ projects/frontend/es-app/src/scrapers/Scraper.cpp
@@ -13,7 +13,7 @@
 const std::map<std::string, generate_scraper_requests_func> scraper_request_funcs = boost::assign::map_list_of
 	("TheGamesDB", &thegamesdb_generate_scraper_requests)
 	("Mamedb", &mamedb_generate_scraper_requests)
-	("Screenscraper", &screenscraper_generate_scraper_requests);
+	/*("Screenscraper", &screenscraper_generate_scraper_requests)*/;
 
 std::unique_ptr<ScraperSearchHandle> startScraperSearch(const ScraperSearchParams& params)
 {
