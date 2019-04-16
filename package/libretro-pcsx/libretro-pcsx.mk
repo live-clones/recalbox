8531ecf6868d709477c08bec41885957b9c8383f
paradadf <paradad@outlook.com>
2019-04-16 00:38:42 +0000

    Remove vibration patch

--- package/libretro-pcsx/libretro-pcsx.mk
+++ package/libretro-pcsx/libretro-pcsx.mk
@@ -15,10 +15,4 @@ define LIBRETRO_PCSX_INSTALL_TARGET_CMDS
 		$(TARGET_DIR)/usr/lib/libretro/pcsx_rearmed_libretro.so
 endef
 
-define LIBRETRO_PCSX_PRE_PATCH_FIXUP
-	$(SED) 's/\r//g' $(@D)/libpcsxcore/plugins.c
-endef
-
-LIBRETRO_PCSX_PRE_PATCH_HOOKS += LIBRETRO_PCSX_PRE_PATCH_FIXUP
-
 $(eval $(generic-package))
