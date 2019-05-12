dc92b7dfad93fb133becf088d210246a25b053d1
Bkg2k <bkg2k9@gmail.com>
2019-05-12 12:50:54 +0000

    Resolve "Add missing RetroArch ratio"

--- projects/frontend/es-app/src/LibretroRatio.cpp
+++ projects/frontend/es-app/src/LibretroRatio.cpp
@@ -3,45 +3,52 @@
 //
 
 #include "LibretroRatio.h"
+#include <Locale.h>
 
 LibretroRatio *LibretroRatio::sInstance = nullptr;
 
-LibretroRatio::LibretroRatio() {
+LibretroRatio::LibretroRatio()
+{
     ratioMap = new std::map<std::string, std::string>
     {
-        {"Auto",          "auto"},
-        {"4/3",           "4/3"},
-        {"16/9",          "16/9"},
-        {"16/10",         "16/10"},
-        {"16/15",         "16/15"},
-        {"21/9",          "21/9"},
-        {"1/1",           "1/1"},
-        {"2/1",           "2/1"},
-        {"3/2",           "3/2"},
-        {"3/4",           "3/4"},
-        {"4/1",           "4/1"},
-        {"9/16",          "9/16"},
-        {"5/4",           "5/4"},
-        {"6/5",           "6/5"},
-        {"7/9",           "7/9"},
-        {"8/3",           "8/3"},
-        {"8/7",           "8/7"},
-        {"19/12",         "19/12"},
-        {"19/14",         "19/14"},
-        {"30/17",         "30/17"},
-        {"32/9",          "32/9"},
-        {"Square pixel",  "squarepixel"},
-        {"Custom",        "custom"}
+        { _("Auto"),             "auto"         },
+        { "4/3",                 "4/3"          },
+        { "16/9",                "16/9"         },
+        { "16/10",               "16/10"        },
+        { "16/15",               "16/15"        },
+        { "21/9",                "21/9"         },
+        { "1/1",                 "1/1"          },
+        { "2/1",                 "2/1"          },
+        { "3/2",                 "3/2"          },
+        { "3/4",                 "3/4"          },
+        { "4/1",                 "4/1"          },
+        { "9/16",                "9/16"         },
+        { "5/4",                 "5/4"          },
+        { "6/5",                 "6/5"          },
+        { "7/9",                 "7/9"          },
+        { "8/3",                 "8/3"          },
+        { "8/7",                 "8/7"          },
+        { "19/12",               "19/12"        },
+        { "19/14",               "19/14"        },
+        { "30/17",               "30/17"        },
+        { "32/9",                "32/9 "        },
+        { _("Square pixel"),     "squarepixel"  },
+        { _("Retroarch Config"), "config"       },
+        { _("Retroarch Custom"), "custom"       },
+        { _("Core provided"),    "coreprovided" },
+        { _("Do not set"),       "none"         },
     };
 }
 
-LibretroRatio *LibretroRatio::getInstance() {
+LibretroRatio *LibretroRatio::getInstance()
+{
     if (sInstance == nullptr)
         sInstance = new LibretroRatio();
 
     return sInstance;
 }
 
-std::map<std::string, std::string> *LibretroRatio::getRatio() {
+std::map<std::string, std::string> *LibretroRatio::getRatio()
+{
     return ratioMap;
-};
+}
