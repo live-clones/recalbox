c71b1397351cd3b76f8f6fa52b1faf002f872a93
paradadf <paradad@outlook.com>
2019-05-31 21:20:32 +0000

    chore(configgen): bump configgen

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = d3b8f55dec401b538d3518b131444991b96a03ca
+RECALBOX_CONFIGGEN_VERSION = 4d046d43a42802bc0b5e92f4cfc4faf8e7191d06
 
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
