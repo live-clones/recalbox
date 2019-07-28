1f5441939975e273ffe2d79e05fbeacde02bcdf0
paradadf <paradad@outlook.com>
2019-07-28 23:09:29 +0000

    chore(libretro-vice): bump

--- package/libretro-vice/libretro-vice.mk
+++ package/libretro-vice/libretro-vice.mk
@@ -1,10 +1,10 @@
 ################################################################################
 #
-# VICE
+# VICE_X64
 #
 ################################################################################
 
-LIBRETRO_VICE_VERSION = 490f5e7b332a95aa91a505c3fc0f40c8f376822c
+LIBRETRO_VICE_VERSION = b8871f282ab7542a1caffef6f79b96b9fecb9314
 LIBRETRO_VICE_SITE = $(call github,libretro,vice-libretro,$(LIBRETRO_VICE_VERSION))
 
 define LIBRETRO_VICE_BUILD_CMDS
@@ -13,7 +13,7 @@ define LIBRETRO_VICE_BUILD_CMDS
 		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
 		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
 		SHARED="$(TARGET_SHARED)" \
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(RETROARCH_LIBRETRO_PLATFORM)"
+		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(RETROARCH_LIBRETRO_PLATFORM)" EMUTYPE=x64
 endef
 
 define LIBRETRO_VICE_INSTALL_TARGET_CMDS
