29f94544b88acbe5a8110acaa207c1fc16363766
paradadf <paradad@outlook.com>
2019-07-31 02:32:05 +0000

    chore: rename vice_x64 back to vice

--- package/libretro-vice_x64/libretro-vice_x64.mk
+++ package/libretro-vice_x64/libretro-vice_x64.mk
@@ -1,13 +1,13 @@
 ################################################################################
 #
-# VICE_X64
+# VICE
 #
 ################################################################################
 
-LIBRETRO_VICE_X64_VERSION = df6e5f45cd88035317107d5a33e483cd551351d5
-LIBRETRO_VICE_X64_SITE = $(call github,libretro,vice-libretro,$(LIBRETRO_VICE_X64_VERSION))
+LIBRETRO_VICE_VERSION = f509264e85dd18488d85612e70c1417b512345b6
+LIBRETRO_VICE_SITE = $(call github,libretro,vice-libretro,$(LIBRETRO_VICE_VERSION))
 
-define LIBRETRO_VICE_X64_BUILD_CMDS
+define LIBRETRO_VICE_BUILD_CMDS
 	$(SED) "s|-O2|-O3|g" $(@D)/Makefile.libretro
 	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
 		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
@@ -16,15 +16,15 @@ define LIBRETRO_VICE_X64_BUILD_CMDS
 		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(RETROARCH_LIBRETRO_PLATFORM)" EMUTYPE=x64
 endef
 
-define LIBRETRO_VICE_X64_INSTALL_TARGET_CMDS
+define LIBRETRO_VICE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/vice_x64_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/vice_x64_libretro.so
 endef
 
-define LIBRETRO_VICE_X64_PRE_PATCH_FIXUP
+define LIBRETRO_VICE_PRE_PATCH_FIXUP
 	$(SED) 's/\r//g' $(@D)/libretro/libretro-core.c
 endef
 
-LIBRETRO_VICE_X64_PRE_PATCH_HOOKS += LIBRETRO_VICE_X64_PRE_PATCH_FIXUP
+LIBRETRO_VICE_PRE_PATCH_HOOKS += LIBRETRO_VICE_PRE_PATCH_FIXUP
 
 $(eval $(generic-package))
