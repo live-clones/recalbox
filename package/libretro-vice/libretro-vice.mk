2f6b9e4cb7c295bd912e2b33dae8f94d3654f074
Bkg2k <bkg2k9@gmail.com>
2019-07-31 14:57:19 +0200

    fix(make): use make foreach loop

--- package/libretro-vice/libretro-vice.mk
+++ package/libretro-vice/libretro-vice.mk
@@ -7,96 +7,35 @@
 LIBRETRO_VICE_VERSION = f509264e85dd18488d85612e70c1417b512345b6
 LIBRETRO_VICE_SITE = $(call github,libretro,vice-libretro,$(LIBRETRO_VICE_VERSION))
 
-define LIBRETRO_VICE_BUILD_CMDS
-	$(SED) "s|-O2|-O3|g" $(@D)/Makefile.libretro
+LIBRETRO_VICE_SUBEMULATORS = x64 x64sc x128 xpet xplus4 xvic
+
+define LIBRETRO_VICE_BUILD_EMULATOR
+	find $(@D) -name *.o -delete; \
+	find $(@D) -name *.a -delete; \
 	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
 		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
 		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
 		SHARED="$(TARGET_SHARED)" \
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(RETROARCH_LIBRETRO_PLATFORM)" EMUTYPE=x64
-endef
-
-define LIBRETRO_VICE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/vice_x64_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/vice_x64_libretro.so
+		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(RETROARCH_LIBRETRO_PLATFORM)" EMUTYPE=$(strip $(1));
 endef
 
 define LIBRETRO_VICE_BUILD_CMDS
-	find $(@D) -name *.so -delete
-	$(SED) "s|-O2|-O3|g" $(@D)/Makefile.libretro
-	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
-		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
-		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
-		SHARED="$(TARGET_SHARED)" \
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(RETROARCH_LIBRETRO_PLATFORM)" EMUTYPE=x64sc
+	$(foreach emulator, $(LIBRETRO_VICE_SUBEMULATORS), $(call LIBRETRO_VICE_BUILD_EMULATOR, $(emulator)))
+endef
+
+define LIBRETRO_VICE_INSTALL_EMULATOR
+	@echo "Installing $(1)..."; \
+	$(INSTALL) -D $(@D)/vice_$(strip $(1))_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/vice_$(strip $(1))_libretro.so ;
 endef
 
 define LIBRETRO_VICE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/vice_x64sc_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/vice_x64sc_libretro.so
+	$(foreach emulator, $(LIBRETRO_VICE_SUBEMULATORS), $(call LIBRETRO_VICE_INSTALL_EMULATOR, $(emulator)))
 endef
 
-define LIBRETRO_VICE_BUILD_CMDS
-	find $(@D) -name *.so -delete
-	$(SED) "s|-O2|-O3|g" $(@D)/Makefile.libretro
-	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
-		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
-		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
-		SHARED="$(TARGET_SHARED)" \
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(RETROARCH_LIBRETRO_PLATFORM)" EMUTYPE=x128
-endef
-
-define LIBRETRO_VICE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/vice_x128_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/vice_x128_libretro.so
-endef
-
-define LIBRETRO_VICE_BUILD_CMDS
-	find $(@D) -name *.so -delete
-	$(SED) "s|-O2|-O3|g" $(@D)/Makefile.libretro
-	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
-		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
-		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
-		SHARED="$(TARGET_SHARED)" \
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(RETROARCH_LIBRETRO_PLATFORM)" EMUTYPE=xpet
-endef
-
-define LIBRETRO_VICE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/vice_xpet_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/vice_xpet_libretro.so
-endef
-
-define LIBRETRO_VICE_BUILD_CMDS
-	find $(@D) -name *.so -delete
-	$(SED) "s|-O2|-O3|g" $(@D)/Makefile.libretro
-	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
-		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
-		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
-		SHARED="$(TARGET_SHARED)" \
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(RETROARCH_LIBRETRO_PLATFORM)" EMUTYPE=xplus4
-endef
-
-define LIBRETRO_VICE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/vice_xplus4_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/vice_xplus4_libretro.so
-endef
-
-define LIBRETRO_VICE_BUILD_CMDS
-	find $(@D) -name *.so -delete
-	$(SED) "s|-O2|-O3|g" $(@D)/Makefile.libretro
-	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
-		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
-		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
-		SHARED="$(TARGET_SHARED)" \
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(RETROARCH_LIBRETRO_PLATFORM)" EMUTYPE=xvic
-endef
-
-define LIBRETRO_VICE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/vice_xvic_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/vice_xvic_libretro.so
-endef
 
 define LIBRETRO_VICE_PRE_PATCH_FIXUP
+	$(SED) "s|-O2|-O3|g" $(@D)/Makefile.libretro
 	$(SED) 's/\r//g' $(@D)/libretro/libretro-core.c
 endef
 
