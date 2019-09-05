c29244ead001f5b0f596dafad72ed702d64f7d5b
strodown <strodown@gmail.com>
2019-09-05 09:21:59 +0000

    Resolve "add rules for Nvidia-Driver in retroarch"

--- projects/configgen/configgen/generators/libretro/libretroRetroarch.py
+++ projects/configgen/configgen/generators/libretro/libretroRetroarch.py
@@ -1,5 +1,6 @@
 import recalboxFiles
-
+import platform
+import os 
 
 class LibretroRetroarch:
 
@@ -127,6 +128,14 @@ class LibretroRetroarch:
     def fillRetroarchConfiguration(self):
         settings = self.settings
         recalbox = self.system.config
+
+        # Nvidia driver?
+        arch = platform.machine()
+        if arch == "x86_64":
+            if os.path.exists("/etc/modprobe.d/blacklist.conf"):
+               for line in open("/etc/modprobe.d/blacklist.conf"):
+                   if "blacklist nouveau" in line:
+                       settings.setOption("video_threaded", "false")
 
         # Enable RetroArch option "quit_press_twice"
         quitPressTwice = self.recalboxSettings.getOption("global.quitpresstwice", "0")
