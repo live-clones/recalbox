74983f958b9a81136fbedb5cb130e8343c6a4202
strodown <strodown@gmail.com>
2019-10-22 16:43:58 +0200

    chore(supermodel): add in defconfig_x86-64

--- package/supermodel/supermodel.mk
+++ package/supermodel/supermodel.mk
@@ -12,12 +12,15 @@ SUPERMODEL_DEPENDENCIES = sdl2 zlib libpng libogg libvorbis sdl_sound sdl2_net
 
 define SUPERMODEL_FIX_SDL_CONFIG_PATH
 	$(SED) "s+sdl-config+$(STAGING_DIR)/usr/bin/sdl-config+g" $(@D)/Makefiles/Makefile.UNIX
+		$(SED) "s+(SDL_LIBS)+(STAGING_DIR)/bin/+g" $(@D)/Makefiles/Makefile.UNIX
 endef
+
+TARGET_LDFLAGS = 
 
 define SUPERMODEL_CONFIGURE_CMDS
 	ln -s $(@D)/Makefiles/Makefile.UNIX $(@D)/Makefile; \
 	$(SED) "s|-O2|-O3|g" $(@D)/Makefile
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_CC) -lpthread" $(@D)/ NET_BOARD=1 VERBOSE=1
+		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LDFLAGS="$(TARGET_LDFLAGS)" LD="$(TARGET_CC)" $(@D)/ NET_BOARD=1
 endef
 
 define SUPERMODEL_INSTALL_TARGET_CMDS
