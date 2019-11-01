90fbeb689d74d45e32a9ee2eca9a49a7e16deb77
strodown <strodown@gmail.com>
2019-11-01 01:06:04 +0100

    chore(ports): inital commit open lara

---
+++ package/libretro-openlara/libretro-openlara.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# OPENLARA
+#
+################################################################################
+
+LIBRETRO_OPENLARA_VERSION = 2c9e0a8ff49afcc6c1cd8c9e0ad8604312e79207
+LIBRETRO_OPENLARA_SITE = $(call github,libretro,OpenLara,$(LIBRETRO_OPENLARA_VERSION))
+
+define LIBRETRO_OPENLARA_BUILD_CMDS
+		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)/src/platform/libretro/ platform="$(RETROARCH_LIBRETRO_PLATFORM)"
+endef
+
+define LIBRETRO_OPENLARA_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/src/platform/libretro/openlara_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/openlara_libretro.so
+endef
+
+$(eval $(generic-package))
