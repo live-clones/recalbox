f4f7a0af9cfe0600bf59beac1200bc4da4d9ced3
Bkg2k <bkg2k9@gmail.com>
2019-11-06 13:31:32 +0100

    perf: refactor hash gui & thread

--- projects/frontend/es-app/src/guis/GuiHashStart.cpp
+++ projects/frontend/es-app/src/guis/GuiHashStart.cpp
@@ -9,193 +9,133 @@
 #include "GuiHashStart.h"
 #include "components/OptionListComponent.h"
 
-
 GuiHashStart::GuiHashStart(Window* window)
   : GuiComponent(window),
     mBusyAnim(window),
     mMenu(window, _("HASH NOW").c_str()),
-    mHandle(nullptr)
+    mState(State::Wait)
 {
-    addChild(&mMenu);
+  addChild(&mMenu);
 
-    mLoading = false;
+  mBusyAnim.setSize(Renderer::getDisplayWidthAsFloat(), Renderer::getDisplayHeightAsFloat());
 
-    mState = 0;
+  mFilter = std::make_shared<OptionListComponent<std::string> >(mWindow, _("FILTER"), false);
+  mFilter->add(_("Only missing hashs"), "missing", true);
+  mFilter->add(_("All Games"), "all", false);
+  mMenu.addWithLabel(mFilter, _("FILTER"));
 
-	mBusyAnim.setSize(Renderer::getDisplayWidthAsFloat(), Renderer::getDisplayHeightAsFloat());
+  // add systems (all with a platformid specified selected)
+  mSystems = std::make_shared<OptionListComponent<SystemData*> >(mWindow, _("HASH THESE SYSTEMS"), true);
+  for (auto it : SystemManager::Instance().GetVisibleSystemList())
+  {
+    if (RecalboxConf::Instance().isInList("global.netplay.systems", it->getName()))
+      mSystems->add(it->getFullName(), it, true);
+  }
+  mMenu.addWithLabel(mSystems, _("SYSTEMS"));
 
-    mFilter = std::make_shared< OptionListComponent<std::string> >(mWindow, _("FILTER"), false);
-    mFilter->add(_("Only missing hashs"), "missing", true);
-    mFilter->add(_("All Games"), "all", false);
-    mMenu.addWithLabel(mFilter, _("FILTER"));
-
-    // add systems (all with a platformid specified selected)
-    mSystems = std::make_shared< OptionListComponent<SystemData*> >(mWindow, _("HASH THESE SYSTEMS"), true);
-    for (auto it : SystemManager::Instance().GetVisibleSystemList())
+  mMenu.addButton(_("START"), "start", [this]
+  {
+    mWindow->pushGui(new GuiMsgBox(mWindow, _("THIS COULD TAKE A WHILE, CONFIRM?"), _("YES"), [this]
     {
-        if(RecalboxConf::Instance().isInList("global.netplay.systems", it->getName()))
-            mSystems->add(it->getFullName(), it, true);
-    }
-    mMenu.addWithLabel(mSystems, _("SYSTEMS"));
+      Start("HashThread");
+      mState = State::Hashing;
+    }, _("NO"), [this] { mState = State::Exit; }));
+  });
+  mMenu.addButton(_("BACK"), "back", [&]
+  { Close(); });
 
-    mMenu.addButton(_("START"), "start",[this] {mState = 1;});
-    mMenu.addButton(_("BACK"), "back", [&] { delete this; });
-
-    mMenu.setPosition((Renderer::getDisplayWidthAsFloat() - mMenu.getSize().x()) / 2, (Renderer::getDisplayHeightAsFloat() - mMenu.getSize().y()) / 2);
+  mMenu.setPosition((Renderer::getDisplayWidthAsFloat() - mMenu.getSize().x()) / 2,
+                    (Renderer::getDisplayHeightAsFloat() - mMenu.getSize().y()) / 2);
 }
 
-void GuiHashStart::start()
+void GuiHashStart::Run()
 {
-    for (auto system : mSystems->getSelectedObjects()) {
+  bool forceAll = mFilter->getSelected() == "all";
 
-    	std::string command = "/recalbox/scripts/recalbox-hash.sh -s \"" + system->getName() + "\" -t";
-
-	    auto cmdResult = RecalboxSystem::execute(command);
-
-	    //script output 1 if system is in fobidden list (like fba_libretro) so we skip to next system
-	    if (cmdResult.second == 1) {
-		    LOG(LogInfo) << "system \"" << system->getName() << "\"  can't be hashed";
-		    continue;
-	    }
-
-        Path xmlpath = system->getGamelistPath(false);
-
-        if(!xmlpath.Exists()) return;
-
-        LOG(LogInfo) << "Hashing games from XML file \"" << xmlpath.ToString() << "\"...";
-
-        pugi::xml_document doc;
-        pugi::xml_parse_result result = doc.load_file(xmlpath.ToChars());
-
-        if(!result)
-        {
-            LOG(LogError) << "Error parsing XML file \"" << xmlpath.ToString() << "\"!\n	" << result.description();
-            return;
-        }
-
-        pugi::xml_node root = doc.child("gameList");
-        if(!root)
-        {
-            LOG(LogError) << "Could not find <gameList> node in gamelist \"" << xmlpath.ToString() << "\"!";
-            return;
-        }
-
-        const Path& relativeTo = system->getStartPath();
-
-        const char* tag = "game";
-
-        int totalRoms = 0;
-
-	    for (pugi::xml_node fileNode = root.child(tag); fileNode != nullptr; fileNode = fileNode.next_sibling(tag))
-	    {
-	    	totalRoms++;
-	    }
-
-	    int currentRom = 0;
-
-	    std::string busyText;
-	    const std::string& systemName = system->getFullName();
-
-        for (pugi::xml_node fileNode = root.child(tag); fileNode != nullptr; fileNode = fileNode.next_sibling(tag))
-        {
-			currentRom++;
-
-            Path path = Path(fileNode.child("path").text().get()).ToAbsolute(relativeTo);
-
-            if(!path.Exists())
-            {
-                LOG(LogWarning) << "File \"" << path.ToString() << "\" does not exist! Ignoring.";
-                continue;
-            }
-
-	        busyText = systemName + " " + std::to_string(currentRom) + " / " + std::to_string(totalRoms);
-
-	        mBusyAnim.setText(busyText);
-
-	        // if missing only, don't bother calculating hash if tag is present
-	        if ((fileNode.child("hash") != nullptr) && mFilter->getSelected() != "all") {
-	        	continue;
-	        }
-
-            std::string cmd = "/recalbox/scripts/recalbox-hash.sh -f \"" + path.ToString() + "\"";
-
-	        auto hashResult = RecalboxSystem::execute(cmd);
-
-	        std::string hashString = hashResult.first;
-
-	        //script output an endline after hash, need to remove it
-	        hashString.erase(std::remove(hashString.begin(), hashString.end(), '\n'), hashString.end());
-
-	        //if tag exist, update if not, add it
-            if ((fileNode.child("hash") != nullptr) && mFilter->getSelected() == "all") {
-	            fileNode.child("hash").text().set(hashString.c_str());
-            } else {
-                pugi::xml_node hash = fileNode.append_child("hash");
-                hash.append_child(pugi::node_pcdata).set_value(hashString.c_str());
-            }
-        }
-        doc.save_file(xmlpath.ToChars());
-
+  // Run through systems...
+  for (auto system : mSystems->getSelectedObjects())
+  {
+    // Check if the system should be hashed
+    auto cmdResult = RecalboxSystem::execute("/recalbox/scripts/recalbox-hash.sh -s \"" + system->getName() + "\" -t");
+    if (cmdResult.second == 1)
+    {
+      LOG(LogInfo) << "system \"" << system->getName() << "\"  can't be hashed";
+      continue;
     }
-    mLoading = false;
-    mState = -1;
+    LOG(LogInfo) << "Hashing games from system \"" << system->getName() << "\"...";
+
+    // Run through games
+    int romCount = 0;
+    FileData::List games = system->getRootFolder()->getAllItems(true);
+    for (FileData* game : games)
+    {
+      // Check path
+      const Path& path = Path(game->getPath());
+      if (!path.Exists())
+      {
+        LOG(LogWarning) << "File \"" << path.ToString() << "\" does not exist! Ignoring.";
+        continue;
+      }
+
+      // Update GUI text
+      std::string busyText = system->getFullName() + " " + std::to_string(++romCount) + " / " + std::to_string(games.size());
+      mMutex.Lock();
+      mBusyAnim.setText(busyText);
+      mMutex.UnLock();
+
+      // Hash already exists?
+      if (game->Metadata().RomCrc32() == 0 && !forceAll)
+        continue;
+
+      // Get hash
+      auto hashResult = RecalboxSystem::execute("/recalbox/scripts/recalbox-hash.sh -f \"" + path.ToString() + "\"");
+      game->Metadata().SetRomCrc32AsString(hashResult.first);
+    }
+    system->UpdateGamelistXml();
+  }
+  mState = State::Exit;
 }
 
 bool GuiHashStart::ProcessInput(const InputCompactEvent& event)
 {
-	if (mLoading)
-		return false;
+  if (mState == State::Hashing)
+    return false;
 
-	if (event.APressed())
-	{
-		delete this;
-	}
-	return GuiComponent::ProcessInput(event);
+  if (event.APressed())
+    Close();
+
+  return GuiComponent::ProcessInput(event);
 }
 
-void GuiHashStart::update(int deltaTime) {
-    GuiComponent::update(deltaTime);
-    mBusyAnim.update(deltaTime);
+void GuiHashStart::update(int deltaTime)
+{
+  GuiComponent::update(deltaTime);
+  mMutex.Lock();
+  mBusyAnim.update(deltaTime);
+  mMutex.UnLock();
 
-	if (mState == 1) {
-		mWindow->pushGui(
-				new GuiMsgBox(mWindow, _("THIS COULD TAKE A WHILE, CONFIRM?"), _("YES"),
-				              [this] {
-					              this->mLoading = true;
-					              mHandle = new boost::thread(boost::bind(&GuiHashStart::start, this));
-					              mState = 0;
-
-				              }, _("NO"), [this] {
-							mState = -1;
-						})
-
-		);
-		mState = 0;
-	}
-	if (mState == -1) {
-		delete this;
-	}
+  if (mState == State::Exit)
+    Close();
 }
 
 void GuiHashStart::render(const Transform4x4f& parentTrans)
 {
-    Transform4x4f trans = parentTrans * getTransform();
+  Transform4x4f trans = parentTrans * getTransform();
 
-    if (!mLoading)
-        renderChildren(trans);
+  if (mState != State::Hashing)
+    renderChildren(trans);
 
-    Renderer::setMatrix(trans);
-    Renderer::drawRect(0.f, 0.f, mSize.x(), mSize.y(), 0x00000011);
+  Renderer::setMatrix(trans);
+  Renderer::drawRect(0.f, 0.f, mSize.x(), mSize.y(), 0x00000011);
 
-    if (mLoading)
-        mBusyAnim.render(trans);
-
+  if (mState == State::Hashing)
+    mBusyAnim.render(trans);
 }
 
 
 std::vector<HelpPrompt> GuiHashStart::getHelpPrompts()
 {
-    std::vector<HelpPrompt> prompts = mMenu.getHelpPrompts();
-    prompts.push_back(HelpPrompt("a", _("BACK")));
-    return prompts;
+  std::vector<HelpPrompt> prompts = mMenu.getHelpPrompts();
+  prompts.push_back(HelpPrompt("a", _("BACK")));
+  return prompts;
 }
--- projects/frontend/es-app/src/guis/GuiHashStart.h
+++ projects/frontend/es-app/src/guis/GuiHashStart.h
@@ -3,7 +3,6 @@
 //
 #pragma once
 
-#include <boost/thread.hpp>
 #include "GuiComponent.h"
 #include "systems/SystemData.h"
 #include "components/MenuComponent.h"
@@ -12,34 +11,73 @@
 template<typename T>
 class OptionListComponent;
 
-
-class GuiHashStart : public GuiComponent
+class GuiHashStart : public GuiComponent, private Thread
 {
-public:
+  private:
+    //! UI State
+    enum class State
+    {
+      Wait,    //!< Waiting for user input
+      Hashing, //!< Running hash on selected systems
+      Exit,    //!< Close this UI
+    };
+
+    //! Busy animation
+    BusyComponent mBusyAnim;
+    //! Selected systems
+    std::shared_ptr< OptionListComponent<SystemData*> > mSystems;
+    //! Filters
+    std::shared_ptr< OptionListComponent<std::string> > mFilter;
+    //! Menu
+    MenuComponent mMenu;
+    //! GUI Global state
+    State mState;
+    //! Protext mBysuAnim component access from both main & hash threads
+    Mutex mMutex;
+
+    /*
+     * Thread implementation
+     */
+
+    /*!
+     * @brief Main hash thread method
+     */
+    void Run() override;
+
+  public:
+    /*!
+     * @brief Constructor
+     * @param window main ui window
+     */
     explicit GuiHashStart(Window* window);
 
+    /*!
+     * @brief Destructor
+     */
+    ~GuiHashStart() override { Thread::Stop(); }
+
+    /*!
+     * @brief Process input event
+     * @param event Input event
+     * @return True if the method has processed the inpur event
+     */
     bool ProcessInput(const InputCompactEvent& event) override;
 
+    /*!
+     * @brief Get the help system
+     * @return Help system
+     */
     std::vector<HelpPrompt> getHelpPrompts() override;
 
+    /*!
+     * @brief Update method, called periodically
+     * @param deltaTime Elapsed milliseconds since the last call
+     */
     void update(int deltaTime) override;
 
+    /*!
+     * @brief Render UI
+     * @param parentTrans Parent transformation
+     */
     void render(const Transform4x4f &parentTrans) override;
-
-private:
-    void start();
-
-    BusyComponent mBusyAnim;
-
-    std::shared_ptr< OptionListComponent<SystemData*> > mSystems;
-
-    std::shared_ptr< OptionListComponent<std::string> > mFilter;
-
-    MenuComponent mMenu;
-
-    bool mLoading;
-
-	boost::thread *mHandle;
-
-	int mState;
 };
