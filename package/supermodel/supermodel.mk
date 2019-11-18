b253fdd8bd7460f174814ef1dba23f868652f2ee
cpasjuste <cpasjuste@gmail.com>
2019-11-18 20:03:05 +0000

    chore(supermodel): fix compilation

--- package/supermodel/supermodel.mk
+++ package/supermodel/supermodel.mk
@@ -4,32 +4,25 @@
 #
 ################################################################################
 
-SUPERMODEL_VERSION = r777
+SUPERMODEL_VERSION = r782
 SUPERMODEL_SITE = svn://svn.code.sf.net/p/model3emu/code/trunk
 SUPERMODEL_SITE_METHOD = svn
 SUPERMODEL_LICENSE = GPL2
 SUPERMODEL_DEPENDENCIES = sdl2 zlib libpng libogg libvorbis sdl_sound sdl2_net
 
-define SUPERMODEL_FIX_SDL_CONFIG_PATH
-	$(SED) "s+sdl-config+$(STAGING_DIR)/usr/bin/sdl-config+g" $(@D)/Makefiles/Makefile.UNIX
-		$(SED) "s+(SDL_LIBS)+(STAGING_DIR)/bin/+g" $(@D)/Makefiles/Makefile.UNIX
-endef
-
-define SUPERMODEL_CONFIGURE_CMDS
-	ln -s $(@D)/Makefiles/Makefile.UNIX $(@D)/Makefile; \
-	$(SED) "s|-O2|-O3|g" $(@D)/Makefile
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LDFLAGS="$(TARGET_LDFLAGS)" LD="$(TARGET_CC)" $(@D)/ NET_BOARD=1
+define SUPERMODEL_BUILD_CMDS
+	cp $(@D)/Makefiles/Makefile.UNIX $(@D)/Makefile
+	$(SED) "s|CC = gcc|CC = $(TARGET_CC)|g" $(@D)/Makefile
+	$(SED) "s|CXX = g++|CXX = $(TARGET_CXX)|g" $(@D)/Makefile
+	$(SED) "s|LD = gcc|LD = $(TARGET_CC)|g" $(@D)/Makefile
+	$(SED) "s|sdl-config|$(STAGING_DIR)/usr/bin/sdl-config|g" $(@D)/Makefile
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) -f Makefile
 endef
 
 define SUPERMODEL_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/bin/supermodel \
-		$(TARGET_DIR)/usr/bin/supermodel
-	$(INSTALL) -D $(@D)/Configs/Games.xml \
-		$(TARGET_DIR)/recalbox/share_init/roms/model3/Games.xml
-	$(INSTALL) -D $(@D)/Configs/Supermodel.ini \
-		$(TARGET_DIR)/recalbox/share_init/configs/model3/Supermodel.ini
+	$(INSTALL) -D -m 0755 $(@D)/bin/supermodel $(TARGET_DIR)/usr/bin/supermodel
+	$(INSTALL) -D -m 0755 $(@D)/Configs/Games.xml $(TARGET_DIR)/recalbox/share_init/roms/model3/Games.xml
+	$(INSTALL) -D -m 0755 $(@D)/Configs/Supermodel.ini $(TARGET_DIR)/recalbox/share_init/configs/model3/Supermodel.ini
 endef
 
-SUPERMODEL_PRE_CONFIGURE_HOOKS += SUPERMODEL_FIX_SDL_CONFIG_PATH
-
-$(eval $(autotools-package))
+$(eval $(generic-package))
