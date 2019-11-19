f661a98c3d512c419ab9843e2a4d8090a801b2cb
littlebalup <littlebalup@gmail.com>
2019-11-19 01:59:13 +0100

    bump uboot-xu4 : odroidxu4-v2017.05

--- package/uboot-xu4/uboot-xu4.mk
+++ package/uboot-xu4/uboot-xu4.mk
@@ -4,16 +4,16 @@
 #
 ################################################################################
 
-UBOOT_XU4_VERSION = odroidxu3-v2012.07
+UBOOT_XU4_VERSION = odroidxu4-v2017.05
 UBOOT_XU4_SITE = $(call github,hardkernel,u-boot,$(UBOOT_XU4_VERSION))
 UBOOT_XU4_INSTALL_TARGET = NO
 UBOOT_XU4_INSTALL_IMAGES = YES
 
 define UBOOT_XU4_INSTALL_IMAGES_CMDS
-	cp -dpf $(@D)/sd_fuse/hardkernel/u-boot.bin.hardkernel \
-		$(@D)/sd_fuse/hardkernel/bl1.bin.hardkernel \
-		$(@D)/sd_fuse/hardkernel/bl2.bin.hardkernel \
-		$(@D)/sd_fuse/hardkernel/tzsw.bin.hardkernel \
+	cp -dpf $(@D)/sd_fuse/u-boot.bin.hardkernel \
+		$(@D)/sd_fuse/bl1.bin.hardkernel \
+		$(@D)/sd_fuse/bl2.bin.hardkernel.720k_uboot \
+		$(@D)/sd_fuse/tzsw.bin.hardkernel \
 		$(BINARIES_DIR)
 endef
 $(eval $(generic-package))
