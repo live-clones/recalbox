deef98b04038cb02786e6e37bdcd30b1f6045503
lekeux kevin <strodown@gmail.com>
2020-02-13 21:40:35 +0100

    chore(retroarch): bump libretro gearsystem

--- package/libretro-gearsystem/libretro-gearsystem.mk
+++ package/libretro-gearsystem/libretro-gearsystem.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRETRO_GEARSYSTEM_VERSION = 5becd908e48f57a6c1ec23b2f28dbdb2164f1b0a
+LIBRETRO_GEARSYSTEM_VERSION = 7e2536fafd4ab0e35efdc8570c807635b2fc4679
 LIBRETRO_GEARSYSTEM_SITE = $(call github,drhelius,Gearsystem,$(LIBRETRO_GEARSYSTEM_VERSION))
 
 define LIBRETRO_GEARSYSTEM_BUILD_CMDS
