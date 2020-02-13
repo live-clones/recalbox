379da07dd86d1e707242e441787853558fdb09b8
lekeux kevin <strodown@gmail.com>
2020-02-13 21:51:43 +0100

    chore(retroarch): bump libretro-gpsp

--- package/libretro-gpsp/libretro-gpsp.mk
+++ package/libretro-gpsp/libretro-gpsp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRETRO_GPSP_VERSION = 24af89596e6484ff5a7a08efecfa8288cfbc02f3
+LIBRETRO_GPSP_VERSION = 4d6846b4b72f0f5bbfeb684106cbf5fca8d4743a
 LIBRETRO_GPSP_SITE = $(call github,libretro,gpsp,$(LIBRETRO_GPSP_VERSION))
 
 define LIBRETRO_GPSP_BUILD_CMDS
