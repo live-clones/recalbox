0d5523f76b8c0472c0d85c642b43c5cf650f8801
lekeux kevin <strodown@gmail.com>
2020-02-16 13:02:17 +0100

    chore(retroarch): bump libretro cheats to 1.8.2 version

--- package/libretro-cheats/libretro-cheats.mk
+++ package/libretro-cheats/libretro-cheats.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRETRO_CHEATS_VERSION = 69e12b1bec0633a7b63e83506c0b04d5a658e660
+LIBRETRO_CHEATS_VERSION = v1.8.2
 LIBRETRO_CHEATS_SITE = $(call github,libretro,libretro-database,$(LIBRETRO_CHEATS_VERSION))
 
 define LIBRETRO_CHEATS_INSTALL_TARGET_CMDS
