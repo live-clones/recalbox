51164389af44fa454186a64c5bcd45cbfecd0582
LionSquall <lionsquall25@gmail.com>
2020-02-19 20:01:52 +0100

    chore(retroarch): bump libretro mame 2003 plus

--- package/libretro-mame2003-plus/libretro-mame2003-plus.mk
+++ package/libretro-mame2003-plus/libretro-mame2003-plus.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRETRO_MAME2003_PLUS_VERSION = e8b7131141a6667104185081db38b52afde0d16b
+LIBRETRO_MAME2003_PLUS_VERSION = 0134c428b75882aa474f78dbbf2c6ecde49b97b7
 LIBRETRO_MAME2003_PLUS_SITE = $(call github,libretro,mame2003-plus-libretro,$(LIBRETRO_MAME2003_PLUS_VERSION))
 
 define LIBRETRO_MAME2003_PLUS_BUILD_CMDS
