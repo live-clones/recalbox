afbf349736b9af34756f7303e206eea8289b36ca
LionSquall <lionsquall25@gmail.com>
2020-02-19 18:04:17 +0100

    chore(retroarch): bump libretro beetle saturn

--- package/libretro-beetle-saturn/libretro-beetle-saturn.mk
+++ package/libretro-beetle-saturn/libretro-beetle-saturn.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRETRO_BEETLE_SATURN_VERSION = 3313cc6760c14cffa9226e0cfd41debc11df8bdd
+LIBRETRO_BEETLE_SATURN_VERSION = 8a65943bb7bbc3183eeb0d57c4ac3e663f1bcc11
 LIBRETRO_BEETLE_SATURN_SITE = $(call github,libretro,beetle-saturn-libretro,$(LIBRETRO_BEETLE_SATURN_VERSION))
 
 define LIBRETRO_BEETLE_SATURN_BUILD_CMDS
