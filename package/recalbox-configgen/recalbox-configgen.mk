2f20b36373d9f2206085e821421907e6583696bf
Bkg2k <bkg2k9@gmail.com>
2020-03-06 12:47:50 +0100

    chore: bump configgen

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = f0959c1f3a16406ad02ef96b8ad04d8729dd651d
+RECALBOX_CONFIGGEN_VERSION = 54cb4d50e10d28066af7a2add08aa2b28807c3b8
 
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
