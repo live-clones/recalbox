e14e6cccd08cca91eb42bf700de078376fa69c95
paradadf <paradad@outlook.com>
2020-03-27 00:14:22 +0000

    chore(quake): add disabled packages

---
+++ package/libretro-vitavoyager/libretro-vitavoyager.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# VITAVOYAGER
+#
+################################################################################
+
+LIBRETRO_VITAVOYAGER_VERSION = 58b70dd7ea891c2e8ddc37d03a5d58116b0ea118
+LIBRETRO_VITAVOYAGER_SITE = $(call github,libretro,vitaVoyager,$(LIBRETRO_VITAVOYAGER_VERSION))
+
+define LIBRETRO_VITAVOYAGER_BUILD_CMDS
+	$(SED) "s|-O2|-O3|g" $(@D)/Makefile
+	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
+		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
+		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
+		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile platform="$(RETROARCH_LIBRETRO_PLATFORM)"
+endef
+
+define LIBRETRO_VITAVOYAGER_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/vitavoyager_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/vitavoyager_libretro.so
+endef
+
+$(eval $(generic-package))
---
+++ package/recalbox-romfs/recalbox-romfs-quake/roms/quake/readme.txt
@@ -0,0 +1,5 @@
+## RECALBOX - SYSTEM QUAKE ##
+
+Put your QUAKE 1, 2 and 3 games (or Star Trek Voyager: Elite Force Holomatch) in this directory.
+
+QUAKE games must have a ".pak/.pk3" extension.
