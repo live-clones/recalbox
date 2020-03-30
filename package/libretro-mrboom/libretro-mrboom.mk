cfbecc064c5170c2646f96cd6331c0d2d0cadf18
paradadf <paradad@outlook.com>
2020-03-30 01:05:36 +0000

    chore(mrboom): fix build and add launcher

--- package/libretro-mrboom/libretro-mrboom.mk
+++ package/libretro-mrboom/libretro-mrboom.mk
@@ -7,19 +7,22 @@
 LIBRETRO_MRBOOM_VERSION = a47e81ce1c4126a977780f6b0d797990161ddd20
 LIBRETRO_MRBOOM_SITE = $(call github,libretro,mrboom-libretro,$(LIBRETRO_MRBOOM_VERSION))
 
+# Flag to fix build on arm platforms
+ifeq ($(BR2_ARM_CPU_HAS_NEON),y)
+LIBRETRO_MRBOOM_OPTIONS += "HAVE_NEON=1"
+endif
+
 define LIBRETRO_MRBOOM_BUILD_CMDS
 	$(SED) "s|-O2|-O3|g" $(@D)/Makefile
-	$(SED) "s|LDFLAGS :=|LDFLAGS := $(COMPILER_COMMONS_LDFLAGS_SO)|g" $(@D)/Makefile
 	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
 		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
 		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(RETROARCH_LIBRETRO_PLATFORM)"
+		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(RETROARCH_LIBRETRO_PLATFORM)" $(LIBRETRO_MRBOOM_OPTIONS)
 endef
 
 define LIBRETRO_MRBOOM_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/mrboom_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/mrboom_libretro.so
-	mkdir -p $(TARGET_DIR)/recalbox/share_init/roms/ports/mrboom
 endef
 
 $(eval $(generic-package))
