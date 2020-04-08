1bb6302acbe79387f698186dbdd74ea20ccdd544
paradadf <paradad@outlook.com>
2020-04-08 23:03:47 +0000

    chore(np2kai): cleanup mk

--- package/libretro-np2kai/libretro-np2kai.mk
+++ package/libretro-np2kai/libretro-np2kai.mk
@@ -4,26 +4,16 @@
 #
 ################################################################################
 
-LIBRETRO_NP2KAI_VERSION = 6c1b6304d0a3882eb05d9b97d8bc33700d58051c
+LIBRETRO_NP2KAI_VERSION = f7e6013cf3b90ac919bb113b564fdded797b561f
 LIBRETRO_NP2KAI_SITE = $(call github,AZO234,NP2kai,$(LIBRETRO_NP2KAI_VERSION))
 LIBRETRO_NP2KAI_LICENSE = MIT
-
-ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_RPI3),y)
-LIBRETRO_NP2KAI_PLATFORM=rpi3-embedded
-else ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_RPI2),y)
-LIBRETRO_NP2KAI_PLATFORM=rpi2-embedded
-else ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_XU4),y)
-LIBRETRO_NP2KAI_PLATFORM=xu4-embedded
-else
-LIBRETRO_NP2KAI_PLATFORM=unix
-endif
 
 define LIBRETRO_NP2KAI_BUILD_CMDS
 	$(SED) "s|-O2|-O3|g" $(@D)/sdl2/Makefile
 	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
 		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
 		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/sdl2 -f Makefile.libretro platform="$(LIBRETRO_NP2KAI_PLATFORM)"
+		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/sdl2 -f Makefile.libretro platform="$(RETROARCH_LIBRETRO_BOARD)"
 endef
 
 define LIBRETRO_NP2KAI_INSTALL_TARGET_CMDS
@@ -31,12 +21,5 @@ define LIBRETRO_NP2KAI_INSTALL_TARGET_CMDS
 		$(TARGET_DIR)/usr/lib/libretro/np2kai_libretro.so
 	mkdir -p $(TARGET_DIR)/recalbox/share_init/bios/np2kai
 endef
-
-define LIBRETRO_NP2KAI_LINE_ENDINGS_FIXUP
-	# DOS2UNIX Makefile.libretro - patch system does not support different line endings
-	sed -i -E -e "s|\r$$||g" $(@D)/sdl2/Makefile.libretro
-endef
-
-LIBRETRO_NP2KAI_PRE_PATCH_HOOKS += LIBRETRO_NP2KAI_LINE_ENDINGS_FIXUP
 
 $(eval $(generic-package))
