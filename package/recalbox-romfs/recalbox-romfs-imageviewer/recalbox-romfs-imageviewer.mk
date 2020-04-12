537ccc5c58c41626db8833274e3edae5dbbdff66
Bkg2k <bkg2k9@gmail.com>
2020-04-12 15:35:39 +0200

    fix: fix extra "

--- package/recalbox-romfs/recalbox-romfs-imageviewer/recalbox-romfs-imageviewer.mk
+++ package/recalbox-romfs/recalbox-romfs-imageviewer/recalbox-romfs-imageviewer.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # Package generated with :
-# ./scripts/linux/empack.py --force --system imageviewer --extension '.jpg .jpeg .png .bmp .psd .tga .gif .hdr .pic .ppm .pgm .JPG .JPEG .PNG .BMP .PSD .TGA .GIF .HDR .PIC .PPM .PGM"' --fullname 'Screenshots' --platform ignore --theme imageviewer 1:imageviewer:imageviewer:BR2_PACKAGE_RETROARCH
+# ./scripts/linux/empack.py --force --system imageviewer --extension '.jpg .jpeg .png .bmp .psd .tga .gif .hdr .pic .ppm .pgm .JPG .JPEG .PNG .BMP .PSD .TGA .GIF .HDR .PIC .PPM .PGM' --fullname 'Screenshots' --platform ignore --theme imageviewer 1:imageviewer:imageviewer:BR2_PACKAGE_RETROARCH
 
 # Name the 3 vars as the package requires
 RECALBOX_ROMFS_IMAGEVIEWER_SOURCE = 
@@ -23,7 +23,7 @@ SOURCE_ROMDIR_IMAGEVIEWER = $(RECALBOX_ROMFS_IMAGEVIEWER_PKGDIR)/roms
 
 ifneq ($(BR2_PACKAGE_RETROARCH),)
 define CONFIGURE_MAIN_IMAGEVIEWER_START
-	$(call RECALBOX_ROMFS_CALL_ADD_SYSTEM,$(SYSTEM_XML_IMAGEVIEWER),Screenshots,$(SYSTEM_NAME_IMAGEVIEWER),.jpg .jpeg .png .bmp .psd .tga .gif .hdr .pic .ppm .pgm .JPG .JPEG .PNG .BMP .PSD .TGA .GIF .HDR .PIC .PPM .PGM",ignore,imageviewer)
+	$(call RECALBOX_ROMFS_CALL_ADD_SYSTEM,$(SYSTEM_XML_IMAGEVIEWER),Screenshots,$(SYSTEM_NAME_IMAGEVIEWER),.jpg .jpeg .png .bmp .psd .tga .gif .hdr .pic .ppm .pgm .JPG .JPEG .PNG .BMP .PSD .TGA .GIF .HDR .PIC .PPM .PGM,ignore,imageviewer)
 endef
 
 ifneq ($(BR2_PACKAGE_RETROARCH),)
