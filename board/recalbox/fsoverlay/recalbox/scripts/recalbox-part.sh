3a237f3f6310e519a90628d0ff862c61b4d291c6
Bkg2k <bkg2k9@gmail.com>
2020-04-13 21:25:29 +0200

    fix(squashfs): fix share creation

--- board/recalbox/fsoverlay/recalbox/scripts/recalbox-part.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/recalbox-part.sh
@@ -5,36 +5,24 @@
 # by convention, the share partition is the next partition on the same disk
 # it cannot work if the root is an nfs device or an initramfs device, which is not supported by recalbox
 
-# recalbox has 3 partitions
-# 1 : the boot partition
-# 2 : the system partition
-# 3 : the share partition (user data)
+# recalbox has 2 partitions
+# 1 : the boot partition (contains system in a squashfs file)
+# 2 : the share partition (user data)
 #
 # from the root device partition the partitions can be determined
 # the root partition is not always /dev/mmcblk0p1, mainly in case you boot from an usb stick or a hard drive
 
 determine_boot_part() {
-    ROOTPART=$(rdev | sed -e s+' /$'++)
-    XROOT=$(echo "${ROOTPART}" | sed -e s+'^.*\([0-9]\)$'+'\1'+)
-
-    # check that it is a number
-    if ! echo "${XROOT}" | grep -qE '^[0-9]$'
-    then
-	return 1
-    fi
-
-    XBOOT=$(expr ${XROOT} - 1)
-    echo "${ROOTPART}" | sed -e s+"${XROOT}$"+"${XBOOT}"+
+    df | grep -E '^.*? /boot' | cut -d ' ' -f 1
 }
 
 determine_default_share_part() {
-    ROOTPART=$(rdev | sed -e s+' /$'++)
+    ROOTPART=$(determine_boot_part)
     XROOT=$(echo "${ROOTPART}" | sed -e s+'^.*\([0-9]\)$'+'\1'+)
 
     # check that it is a number
-    if ! echo "${XROOT}" | grep -qE '^[0-9]$'
-    then
-	return 1
+    if ! echo "${XROOT}" | grep -qE '^[0-9]$' ; then
+        return 1
     fi
 
     XSHARE=$(expr ${XROOT} + 1)
@@ -46,10 +34,9 @@ determine_part_prefix() {
     # /dev/sda1      => /dev/sda
 
     # sometimes, it's pX, sometimes just an X : http://www.tldp.org/HOWTO/Partition-Mass-Storage-Definitions-Naming-HOWTO/x160.html
-    if echo "${1}" | grep -qE 'p[0-9]$'
-    then
-	echo "$1" | sed -e s+'p[0-9]$'+''+
-	return 0
+    if echo "${1}" | grep -qE 'p[0-9]$' ; then
+        echo "$1" | sed -e s+'p[0-9]$'+''+
+        return 0
     fi
     echo "$1" | sed -e s+'[0-9]$'++
 }
@@ -59,9 +46,8 @@ determine_previous_part() {
     XPART=$(echo "${1}" | sed -e s+'^.*\([0-9]\)$'+'\1'+)
 
     # check that it is a number
-    if ! echo "${XPART}" | grep -qE '^[0-9]$'
-    then
-	return 1
+    if ! echo "${XPART}" | grep -qE '^[0-9]$' ; then
+       return 1
     fi
 
     XPREVPART=$(expr ${XPART} - 1)
@@ -72,33 +58,29 @@ PARTNAME=$1
 
 case "${PARTNAME}" in
     "boot")
-	determine_boot_part
-	;;
-
-    "system")
-	rdev | sed -e s+' /$'++
-	;;
+    determine_boot_part
+    ;;
 
     "share_internal")
-	determine_default_share_part
-	;;
+    determine_default_share_part
+    ;;
 
     "prefix")
-	determine_part_prefix "$2"
-	;;
+    determine_part_prefix "$2"
+    ;;
 
     "previous")
-	determine_previous_part "$2"
-	;;
+    determine_previous_part "$2"
+    ;;
 
     *)
-	echo "${0} <boot|system|share_internal|prefix x|previous x>" >&2
-	echo "  boot: the recalbox boot partition" >&2
-	echo "  system: the recalbox system partition" >&2
-	echo "  share_internal: the recalbox share internal partition " >&2
-	echo "  prefix x: the disk of the partition x (without the partition number)" >&2
-	echo "  previous x: the partition before x on the same disk" >&2
-	exit 1
+    echo "${0} <boot|system|share_internal|prefix x|previous x>" >&2
+    echo "  boot: the recalbox boot partition" >&2
+    echo "  system: the recalbox system partition" >&2
+    echo "  share_internal: the recalbox share internal partition " >&2
+    echo "  prefix x: the disk of the partition x (without the partition number)" >&2
+    echo "  previous x: the partition before x on the same disk" >&2
+    exit 1
     ;;
 esac
 
