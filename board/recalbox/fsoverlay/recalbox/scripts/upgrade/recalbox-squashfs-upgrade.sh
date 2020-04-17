a46672c7db35edd81afb8a116ab009ea59474e2a
OyyoDams <oyyodams@recalbox.com>
2020-04-17 14:02:29 +0200

    chore: update directory

--- board/recalbox/fsoverlay/recalbox/scripts/upgrade/recalbox-squashfs-upgrade.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/upgrade/recalbox-squashfs-upgrade.sh
@@ -1,51 +1,66 @@
 #!/bin/bash
 
-# Stop ES
-/etc/init.d/S31emulationstation stop
-dd if=/dev/zero of=/dev/fb0 > /dev/null 2>&1
-fbv -f -i /recalbox/system/resources/offline-install-0.jpg &
-sleep 1
+UPDATEARCH=`cat /recalbox/recalbox.arch`
+UPDATEFILE="recalbox-${UPDATEARCH}.img.xz"
+UPDATEFILE2="recalbox-${UPDATEARCH}.img"
 
-# Uncompress img
-fbv -f -i /recalbox/system/resources/offline-install-1.jpg &
-sleep 1
-if test -e "/recalbox/share/system/upgrade/recalbox-rpi3.img.xz"; then
-  unxz /recalbox/share/system/upgrade/recalbox-rpi3.img.xz || exit 1
+function do_update() {
+
+echo "go"
+  # Stop ES
+  /etc/init.d/S31emulationstation stop
+  dd if=/dev/zero of=/dev/fb0 > /dev/null 2>&1
+  fbv -f -i /recalbox/system/resources/offline-install-0.jpg &
+  sleep 1
+
+  # Uncompress img
+  fbv -f -i /recalbox/system/resources/offline-install-1.jpg &
+  sleep 1
+  if test -e "/recalbox/share/system/upgrade/${UPDATEFILE}"; then
+    unxz "/recalbox/share/system/upgrade/${UPDATEFILE}" || return 1
+  else
+    return 1
+  fi
+
+  # Mount
+  fbv -f -i /recalbox/system/resources/offline-install-2.jpg &
+  sleep 1
+  mount -o loop,offset=512 "/recalbox/share/system/upgrade/${UPDATEFILE2}" /mnt/ || return 1
+
+  fbv -f -i /recalbox/system/resources/offline-install-3.jpg &
+  sleep 1
+  mount -o remount,rw /boot/ || return 1
+
+  # Files copy
+  fbv -f -i /recalbox/system/resources/offline-install-4.jpg &
+  sleep 1
+  cp /mnt/boot/recalbox /boot/update/recalbox || return 1
+  fbv -f -i /recalbox/system/resources/offline-install-5.jpg &
+  sleep 1
+  cp /mnt/boot/linux /boot/update/linux || return 1
+  fbv -f -i /recalbox/system/resources/offline-install-6.jpg &
+  sleep 1
+  cp /mnt/boot/initrd.gz /boot/update/initrd.gz || return 1
+
+  # Umount
+  umount /mnt/ || return 1
+
+  # Clean up
+  rm "/recalbox/share/system/upgrade/${UPDATEFILE2}" || return 1
+
+  return 0
+
+}
+
+if do_update; then
+  # Reboot
+  fbv -f -i /recalbox/system/resources/offline-install-7.jpg &
+  sleep 1
+  reboot
 else
-  exit 1
+  fbv -f -i /recalbox/system/resources/offline-install-error.jpg &
+  sleep 10
+  reboot
 fi
 
-# Mount
-fbv -f -i /recalbox/system/resources/offline-install-2.jpg &
-sleep 1
-mount -o loop,offset=512 /recalbox/share/system/upgrade/recalbox-rpi3.img /mnt/ || exit 1
-
-fbv -f -i /recalbox/system/resources/offline-install-3.jpg &
-sleep 1
-mount -o remount,rw /boot/ || exit 1
-
-# Delete old backup
-rm -rf /boot/boot/.old          
-
-# Files copy
-fbv -f -i /recalbox/system/resources/offline-install-4.jpg &
-sleep 1
-cp /mnt/boot/recalbox /boot/boot/recalbox.update || exit 1
-fbv -f -i /recalbox/system/resources/offline-install-5.jpg &
-sleep 1
-cp /mnt/boot/linux /boot/boot/linux.update || exit 1
-fbv -f -i /recalbox/system/resources/offline-install-6.jpg &
-sleep 1
-cp /mnt/boot/initrd.gz /boot/boot/initrd.gz.update || exit 1
-
-# Umount
-umount /mnt/
-
-# Clean up
-rm /recalbox/share/system/upgrade/recalbox-rpi3.img
-
-# Reboot
-fbv -f -i /recalbox/system/resources/offline-install-7.jpg &
-sleep 1
-reboot
-
+exit 0
