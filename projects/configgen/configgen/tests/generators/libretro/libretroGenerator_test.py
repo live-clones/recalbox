fe5954e4e35afb1f3505869630565b6d4ffc42c1
Bkg2k <bkg2k9@gmail.com>
2020-04-20 13:03:31 +0200

    fix: fix tests setup

--- projects/configgen/configgen/tests/generators/libretro/libretroGenerator_test.py
+++ projects/configgen/configgen/tests/generators/libretro/libretroGenerator_test.py
@@ -23,23 +23,6 @@ RETROARCH_OVERRIDE_CFG_FILE = os.path.abspath(os.path.join(os.path.dirname(__fil
 RECALBOX_CFG_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__), 'tmp/recalbox.conf'))
 RETROARCH_CORE_CONFIG = os.path.abspath(os.path.join(os.path.dirname(__file__), "tmp/retroarchcorecustom.cfg"))
 
-# Cloning config files
-shutil.copyfile(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../resources/retroarchcustom.cfg.origin')),
-                RETROARCH_CUSTOM_CFG_FILE)
-shutil.copyfile(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../resources/retroarchcustom.cfg.origin')),
-                RETROARCH_ORIGIN_CFG_FILE)
-shutil.copyfile(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../resources/recalbox.conf.origin')),
-                RECALBOX_CFG_FILE)
-shutil.copyfile(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../resources/retroarchcores.cfg")),
-                RETROARCH_CORE_CONFIG)
-
-# Injecting test files
-libretroGenerator.recalboxFiles.retroarchCustom = RETROARCH_CUSTOM_CFG_FILE
-libretroGenerator.recalboxFiles.retroarchCustomOrigin = RETROARCH_ORIGIN_CFG_FILE
-libretroConfigurations.coreSettings = unixSettings.UnixSettings(RETROARCH_CORE_CONFIG, separator=' ')
-
-libretroConfigurations.libretroSettings = unixSettings.UnixSettings(RETROARCH_CUSTOM_CFG_FILE, separator=' ')
-
 PS3UUID = "060000004c0500006802000000010000"
 
 rom = "MyRom.nes"
@@ -72,27 +55,42 @@ class TestLibretroGenerator(unittest.TestCase):
         self.sdl2controllers['1'] = self.basicController1
         self.sdl2controllers['2'] = self.sdl2controler
 
+        shutil.copyfile(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../resources/retroarchcustom.cfg.origin')), RETROARCH_CUSTOM_CFG_FILE)
+        shutil.copyfile(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../resources/retroarchcustom.cfg.origin')), RETROARCH_ORIGIN_CFG_FILE)
+        shutil.copyfile(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../resources/recalbox.conf.origin')), RECALBOX_CFG_FILE)
+        shutil.copyfile(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../resources/retroarchcores.cfg")), RETROARCH_CORE_CONFIG)
+
+        libretroGenerator.recalboxFiles.retroarchCustom = RETROARCH_CUSTOM_CFG_FILE
+        libretroGenerator.recalboxFiles.retroarchCustomOrigin = RETROARCH_ORIGIN_CFG_FILE
+        libretroConfigurations.coreSettings = unixSettings.UnixSettings(RETROARCH_CORE_CONFIG, separator=' ')
+        libretroConfigurations.libretroSettings = unixSettings.UnixSettings(RETROARCH_CUSTOM_CFG_FILE, separator=' ')
+
     def test_generate_system_no_custom_settings(self):
         command = libretroGen.generate(self.snes, rom, dict(), False, keyValueSettings("", False))
         self.assertEquals(command.videomode, '4')
         self.assertEquals(command.array,
                           [recalboxFiles.recalboxBins["libretro"], '-L', '/usr/lib/libretro/snes9x2002_libretro.so', '--config',
                            RETROARCH_CUSTOM_CFG_FILE, '--appendconfig', RETROARCH_OVERRIDE_CFG_FILE,
-                           '--set-shader', 'myshaders.gpslp', 'MyRom.nes'])
+                           #'--set-shader', 'myshaders.gpslp',
+                           'MyRom.nes'])
 
     def test_generate_system_custom_settings(self):
         command = libretroGen.generate(self.nes, rom, dict(), False, keyValueSettings("", False))
         self.assertEquals(command.videomode, '6')
         self.assertEquals(command.array,
                           [recalboxFiles.recalboxBins["libretro"], '-L', '/usr/lib/libretro/snes9x2005_libretro.so', '--config', '/myconfigfile.cfg',
-                           '--appendconfig', RETROARCH_OVERRIDE_CFG_FILE, '--set-shader', 'myshaders.gpslp', 'MyRom.nes'])
+                           '--appendconfig', RETROARCH_OVERRIDE_CFG_FILE,
+                           #'--set-shader', 'myshaders.gpslp',
+                           'MyRom.nes'])
 
     def test_generate_forced_input_config(self):
         command = libretroGen.generate(self.nes, rom, dict(), False, keyValueSettings("", False))
         self.assertEquals(command.videomode, '6')
         self.assertEquals(command.array,
                           [recalboxFiles.recalboxBins["libretro"], '-L', '/usr/lib/libretro/snes9x2005_libretro.so', '--config', '/myconfigfile.cfg',
-                           '--appendconfig', RETROARCH_OVERRIDE_CFG_FILE, '--set-shader', 'myshaders.gpslp', 'MyRom.nes'])
+                           '--appendconfig', RETROARCH_OVERRIDE_CFG_FILE,
+                           #'--set-shader', 'myshaders.gpslp',
+                           'MyRom.nes'])
 
     def test_custom_inputdriver_override_choice(self):
         self.snes.config['inputdriver'] = 'sdl2'
