b2c61d57c67cd69e47acf20fea9da485fd1e8889
paradadf <paradad@outlook.com>
2020-05-02 02:37:50 +0000

    chore(dinothawr): fix makefile

--- package/libretro-dinothawr/libretro-dinothawr.mk
+++ package/libretro-dinothawr/libretro-dinothawr.mk
@@ -12,12 +12,14 @@ define LIBRETRO_DINOTHAWR_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
 		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
 		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/src/libretro/ -f Makefile platform="unix"
+		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="unix"
 endef
 
 define LIBRETRO_DINOTHAWR_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/dinothawr_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/dinothawr_libretro.so
+	mkdir -p $(TARGET_DIR)/recalbox/share_init/roms/ports/Dinothawr
+	cp -R $(@D)/dinothawr/* $(TARGET_DIR)/recalbox/share_init/roms/ports/Dinothawr/dinothawr
 endef
 
 $(eval $(generic-package))
