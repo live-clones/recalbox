072d7b14406a16498858ca84296a693cb82db739
paradadf <paradad@outlook.com>
2020-05-03 16:26:04 +0000

    chore: bumps part 3

--- package/libretro-stella/libretro-stella.mk
+++ package/libretro-stella/libretro-stella.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRETRO_STELLA_VERSION = 6739ada4f2b51e2b2532fbdaf1670539449df664
+LIBRETRO_STELLA_VERSION = 9685887833244a8ab3e64e3bd213122491c6f21b
 LIBRETRO_STELLA_SITE = $(call github,stella-emu,stella,$(LIBRETRO_STELLA_VERSION))
 
 define LIBRETRO_STELLA_BUILD_CMDS
