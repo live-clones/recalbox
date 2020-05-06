25e7526fecdd3caf7a0559befd7597db7d98910a
OyyoDams <oyyodams@recalbox.com>
2020-05-06 14:37:04 +0200

    fix(squashfs): x86 upgrade

--- board/recalbox/fsoverlay/recalbox/scripts/upgrade/recalbox-squashfs-upgrade.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/upgrade/recalbox-squashfs-upgrade.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-UPDATEARCH=`cat /recalbox/recalbox.arch`
-UPDATEFILE="recalbox-${UPDATEARCH}.img.xz"
-UPDATEFILE2="recalbox-${UPDATEARCH}.img"
-
 function do_update() {
 
-echo "go"
+  UPDATEFILE="rbx_no_upgrade"
+  UPDATEFILE2="rbx_no_upgrade"
+
+  for file in /recalbox/share/system/upgrade/recalbox-*.img.xz; do
+    if [[ -f $file ]]; then
+      UPDATEFILE=${file}
+      UPDATEFILE2=${file::-3}
+      break
+    fi
+  done
+
   # Stop ES
   /etc/init.d/S31emulationstation stop
   dd if=/dev/zero of=/dev/fb0 > /dev/null 2>&1
@@ -16,8 +22,8 @@ echo "go"
   # Uncompress img
   fbv -f -i /recalbox/system/resources/offline-install-1.jpg &
   sleep 1
-  if test -e "/recalbox/share/system/upgrade/${UPDATEFILE}"; then
-    unxz "/recalbox/share/system/upgrade/${UPDATEFILE}" || return 1
+  if test -e "${UPDATEFILE}"; then
+    unxz "${UPDATEFILE}"
   else
     return 1
   fi
@@ -25,7 +31,12 @@ echo "go"
   # Mount
   fbv -f -i /recalbox/system/resources/offline-install-2.jpg &
   sleep 1
-  mount -o loop,offset=1048576 "/recalbox/share/system/upgrade/${UPDATEFILE2}" /mnt/ || return 1
+  losetup /dev/loop1 "${UPDATEFILE2}" -o 1048576
+  RESULT=$?
+  if [ $RESULT -ne 0 ]; then
+    losetup /dev/loop1 "${UPDATEFILE2}" -o $((2048 * 512)) || return 1
+  fi
+  mount /dev/loop1 /mnt || return 1
 
   fbv -f -i /recalbox/system/resources/offline-install-3.jpg &
   sleep 1
@@ -46,7 +57,7 @@ echo "go"
   umount /mnt/ || return 1
 
   # Clean up
-  rm "/recalbox/share/system/upgrade/${UPDATEFILE2}" || return 1
+  rm "${UPDATEFILE2}" || return 1
 
   return 0
 
@@ -64,3 +75,4 @@ else
 fi
 
 exit 0
+
