2e15b4ab899380fff2a2d16d7061c02cb594fb3d
David Barbion <davidb@230ruedubac.fr>
2020-05-06 21:18:16 +0200

    fix: set correct path of recalbox-x86_64_efi for BR2020.02

--- package/recalbox-x86_64_efi/recalbox-x86_64_efi.mk
+++ package/recalbox-x86_64_efi/recalbox-x86_64_efi.mk
@@ -9,7 +9,7 @@ RECALBOX_X86_64_EFI_SOURCE = bootx64.efi.gz
 RECALBOX_X86_64_EFI_SITE = https://github.com/recalbox/recalbox-x86_64_efi/releases/download/$(RECALBOX_X86_64_EFI_VERSION)
 
 define RECALBOX_X86_64_EFI_EXTRACT_CMDS
-	cp $(DL_DIR)/$(RECALBOX_X86_64_EFI_SOURCE) $(@D)
+	cp $(DL_DIR)/recalbox-x86_64_efi/$(RECALBOX_X86_64_EFI_SOURCE) $(@D)
 	gunzip $(@D)/$(RECALBOX_X86_64_EFI_SOURCE)
 endef
 
