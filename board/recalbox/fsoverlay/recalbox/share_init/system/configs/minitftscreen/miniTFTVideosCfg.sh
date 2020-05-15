a607682d649ec7b1742c3d1c89e176d04c4d469c
Bkg2k <bkg2k9@gmail.com>
2020-05-15 11:01:11 +0200

    chore: minitftsupport - add loop configuration to system video

--- board/recalbox/fsoverlay/recalbox/share_init/system/configs/minitftscreen/miniTFTVideosCfg.sh
+++ board/recalbox/fsoverlay/recalbox/share_init/system/configs/minitftscreen/miniTFTVideosCfg.sh
@@ -10,9 +10,11 @@ DefaultStartLogo='RBvideos/recalboxHologram.mp4'
 DefaultStopLogo='RBvideos/recalboxIntroExplode.mp4'
 DefaultImage='noimage.png'
 LogoFile=""
-Loop=false
-#defined but not actually used
-DefaultLogoLoop=false
+
+#loop system video : -1 = loop forever, 0 = no loop, 1 = loop once, 2 = loop twice, etc...
+Loop=-1
+#loop default system video
+DefaultLogoLoop=-1
 
 #change to your own videos by modifying each "LogoFile" variable.
 
@@ -22,97 +24,98 @@ selectSystemVideo()
 case "${TFTESSystem}" in
     default)        LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}"  ;;
     # Nintendo
-    snes)           LogoFile='RBvideos/introRecalboxSNES.mp4' && Loop=false  ;;
-    nes)            LogoFile='NES/VideoSnaps/logo.mp4' && Loop=true  ;;
-    #n64)            LogoFile='N64/VideoSnaps/logo.mp4' && Loop=true  ;;
-    n64)            LogoFile='RBvideos/introRecalboxN64.mp4' && Loop=true  ;;
-    gba)            LogoFile='GBA/VideoSnaps/logo.mp4' && Loop=true  ;;
-    gb)             LogoFile='GBx/VideoSnaps/logo.mp4' && Loop=false  ;;
-    gbc)            LogoFile='GBx/VideoSnaps/logo.mp4' && Loop=false  ;;
-    fds)            LogoFile='FDS/VideoSnaps/logo.mp4' && Loop=true  ;;
-    virtualboy)     LogoFile='VB/VideoSnaps/logo.mp4' && Loop=true  ;;
-    gamecube)       LogoFile='GC/VideoSnaps/logo.mp4' && Loop=true  ;;
-    wii)            LogoFile='Wii/VideoSnaps/logo.mp4' && Loop=false  ;;
-    nds)            LogoFile='NDS/VideoSnaps/logo.mp4' && Loop=false  ;;
+    snes)           LogoFile='RBvideos/introRecalboxSNES.mp4' && Loop=-1  ;;
+    nes)            LogoFile='NES/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    #n64)            LogoFile='N64/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    n64)            LogoFile='RBvideos/introRecalboxN64.mp4' && Loop=-1  ;;
+    gba)            LogoFile='GBA/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    gb)             LogoFile='GBx/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    gbc)            LogoFile='GBx/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    fds)            LogoFile='FDS/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    virtualboy)     LogoFile='VB/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    gamecube)       LogoFile='GC/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    wii)            LogoFile='Wii/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    nds)            LogoFile='NDS/VideoSnaps/logo.mp4' && Loop=-1  ;;
     pokemini)       LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}" ;; # tbd
     satellaview)    LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}"  ;; # tbd
     sufami)         LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}"  ;; # tbd
-    gw)             LogoFile='GW/VideoSnaps/logo.mp4' && Loop=true  ;;
+    gw)             LogoFile='GW/VideoSnaps/logo.mp4' && Loop=-1  ;;
     # Sega
-    sg1000)          LogoFile='SEGA/SG-1000/VideoSnaps/logo.mp4' && Loop=true  ;;
-    #mastersystem)    LogoFile='SEGA/MasterSystem/VideoSnaps/logo.mp4' && Loop=false  ;;
-    mastersystem)    LogoFile='RBvideos/introRecalboxSMS.mp4' && Loop=false  ;;
-    megadrive)       LogoFile='RBvideos/introRecalboxMegadrive.mp4' && Loop=true  ;;
-    gamegear)        LogoFile='SEGA/Gamegear/VideoSnaps/logo.mp4' && Loop=true  ;;
-    sega32x)         LogoFile='SEGA/Sega32X/VideoSnaps/logo.mp4' && Loop=true  ;;
-    segacd)          LogoFile='SEGA/SegaCD/VideoSnaps/logo.mp4' && Loop=true  ;;
-    #dreamcast)       LogoFile='SEGA/Dreamcast/VideoSnaps/logo.mp4' && Loop=true  ;;
-    dreamcast)       LogoFile='RBvideos/introRecalboxDCBlue.mp4' && Loop=true  ;;
+    sg1000)          LogoFile='SEGA/SG-1000/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    #mastersystem)    LogoFile='SEGA/MasterSystem/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    mastersystem)    LogoFile='RBvideos/introRecalboxSMS.mp4' && Loop=-1  ;;
+    megadrive)       LogoFile='RBvideos/introRecalboxMegadrive.mp4' && Loop=-1  ;;
+    gamegear)        LogoFile='SEGA/Gamegear/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    sega32x)         LogoFile='SEGA/Sega32X/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    segacd)          LogoFile='SEGA/SegaCD/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    #dreamcast)       LogoFile='SEGA/Dreamcast/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    dreamcast)       LogoFile='RBvideos/introRecalboxDCBlue.mp4' && Loop=-1  ;;
     # Sony
-    #psx)             LogoFile='PSX/VideoSnaps/logo.mp4' && Loop=false  ;;
-    psx)             LogoFile='RBvideos/introRecalboxPS1.mp4' && Loop=false  ;;
-    psp)             LogoFile='PSP/VideoSnaps/logo.mp4' && Loop=true  ;;
+    #psx)             LogoFile='PSX/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    psx)             LogoFile='RBvideos/introRecalboxPS1.mp4' && Loop=-1  ;;
+    psp)             LogoFile='PSP/VideoSnaps/logo.mp4' && Loop=-1  ;;
     # Atari
-    atarist)         LogoFile='ATARI/AtariST/VideoSnaps/logo.mp4' && Loop=true  ;;
-    atari800)        LogoFile='ATARI/Atari800/VideoSnaps/logo.mp4' && Loop=true  ;;
-    lynx)            LogoFile='ATARI/Lynx/VideoSnaps/logo.mp4' && Loop=true  ;;
-    jaguar)          LogoFile='ATARI/Jaguar/VideoSnaps/logo.mp4' && Loop=true  ;;
-    atari2600)       LogoFile='ATARI/A2600/VideoSnaps/logo.mp4' && Loop=true  ;;
-    atari5200)       LogoFile='ATARI/A5200/VideoSnaps/logo.mp4' && Loop=true  ;;
-    atari7800)       LogoFile='ATARI/A7800/VideoSnaps/logo.mp4' && Loop=true  ;;
+    atarist)         LogoFile='ATARI/AtariST/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    atari800)        LogoFile='ATARI/Atari800/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    lynx)            LogoFile='ATARI/Lynx/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    jaguar)          LogoFile='ATARI/Jaguar/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    atari2600)       LogoFile='ATARI/A2600/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    atari5200)       LogoFile='ATARI/A5200/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    atari7800)       LogoFile='ATARI/A7800/VideoSnaps/logo.mp4' && Loop=-1  ;;
     # Amiga
-    amiga)           LogoFile='AMIGA/VideoSnaps/logo.mp4' && Loop=false  ;;
+    amiga)           LogoFile='AMIGA/VideoSnaps/logo.mp4' && Loop=-1  ;;
     amiga600)        LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}"  ;; # tbd
-    amiga1200)       LogoFile='AMIGA1200/VideoSnaps/logo.mp4' && Loop=true  ;;
-    amigacd32)       LogoFile='AMIGACD32/VideoSnaps/logo.mp4' && Loop=true  ;;
-    amigacdtv)       LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}" && Loop=false  ;; # tbd
+    amiga1200)       LogoFile='AMIGA1200/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    amigacd32)       LogoFile='AMIGACD32/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    amigacdtv)       LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}" && Loop=-1  ;; # tbd
     # NEC
-    #pcengine)        LogoFile='PCE/VideoSnaps/logo.mp4' && Loop=true  ;;
-    #pcenginecd)      LogoFile='PCE/VideoSnaps/logo.mp4' && Loop=false  ;;
-    pcengine|pcenginecd)        LogoFile='RBvideos/introRecalboxPCEngine.mp4' && Loop=true  ;;
-    supergrafx)      LogoFile='SuperGrafx/VideoSnaps/logo.mp4' && Loop=true  ;;
-    pcfx)            LogoFile='PCFX/VideoSnaps/logo.mp4' && Loop=true  ;;
+    #pcengine)        LogoFile='PCE/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    #pcenginecd)      LogoFile='PCE/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    pcengine|pcenginecd)        LogoFile='RBvideos/introRecalboxPCEngine.mp4' && Loop=-1  ;;
+    supergrafx)      LogoFile='SuperGrafx/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    pcfx)            LogoFile='PCFX/VideoSnaps/logo.mp4' && Loop=-1  ;;
     # SNK
-    #neogeo)          LogoFile='NeoGeo/VideoSnaps/logo.mp4' && Loop=false  ;;
-    neogeo|neogeocd) LogoFile='RBvideos/introRecalboxNeoGeo.mp4' && Loop=false  ;;
-    #neogeocd)        LogoFile='NeoGeo/VideoSnaps/logo.mp4' && Loop=false  ;;
-    ngp)             LogoFile='NeoGeoPocket/VideoSnaps/logo.mp4' && Loop=false ;;
-    ngpc)            LogoFile='NeoGeoPocket/VideoSnaps/logo.mp4' && Loop=false ;;
+    #neogeo)          LogoFile='NeoGeo/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    neogeo|neogeocd) LogoFile='RBvideos/introRecalboxNeoGeo.mp4' && Loop=-1  ;;
+    #neogeocd)        LogoFile='NeoGeo/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    ngp)             LogoFile='NeoGeoPocket/VideoSnaps/logo.mp4' && Loop=-1 ;;
+    ngpc)            LogoFile='NeoGeoPocket/VideoSnaps/logo.mp4' && Loop=-1 ;;
     # Arcade             
-    mame)            LogoFile='RBvideos/introRecalboxMame.mp4' && Loop=true  ;;
-    advancemame)     LogoFile='MAME/VideoSnaps/logo.mp4' && Loop=true  ;;
-    #fba)             LogoFile='FBA/VideoSnaps/logo.mp4' && Loop=false  ;;
-    fba)             LogoFile='RBvideos/introRBCapcom.mp4' && Loop=false  ;;
-    #fba_libretro)    LogoFile='FBA/VideoSnaps/logo.mp4' && Loop=false  ;;
-    fba_libretro)    LogoFile='RBvideos/introRBCapcom.mp4' && Loop=false  ;;
+    mame)            LogoFile='RBvideos/introRecalboxMame.mp4' && Loop=-1  ;;
+    advancemame)     LogoFile='MAME/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    #fba)             LogoFile='FBA/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    fba)             LogoFile='RBvideos/introRBCapcom.mp4' && Loop=-1  ;;
+    #fba_libretro)    LogoFile='FBA/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    fba_libretro)    LogoFile='RBvideos/introRBCapcom.mp4' && Loop=-1  ;;
     daphne)          LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}"  ;; # tbd
     # Various computers  
-    amstradcpc)      LogoFile='CPC/VideoSnaps/logo.mp4' && Loop=false  ;;
-    msx|msx1|msx2)   LogoFile='MSX/VideoSnaps/logo.mp4' && Loop=true  ;;
+    amstradcpc)      LogoFile='CPC/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    msx|msx1|msx2)   LogoFile='MSX/VideoSnaps/logo.mp4' && Loop=-1  ;;
     apple2)          LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}"  ;; # tbd
-    zxspectrum)      LogoFile='ZXSpectr/VideoSnaps/logo.mp4' && Loop=true   ;;
-    o2em|odyssey2)        LogoFile='O2EM/VideoSnaps/logo.mp4' && Loop=true  ;;
-    zx81)            LogoFile='ZX81/VideoSnaps/logo.mp4' && Loop=true  ;;
+    zxspectrum)      LogoFile='ZXSpectr/VideoSnaps/logo.mp4' && Loop=-1   ;;
+    o2em|odyssey2)        LogoFile='O2EM/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    zx81)            LogoFile='ZX81/VideoSnaps/logo.mp4' && Loop=-1  ;;
     dos)             LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}" ;; # tbd
-    c64)             LogoFile='C64/VideoSnaps/logo.mp4' && Loop=false  ;;
+    c64)             LogoFile='C64/VideoSnaps/logo.mp4' && Loop=-1  ;;
     x68000)          LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}" ;; # tbd
     thomson)         LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}"  ;; # tbd
     # Various consoles   
-    vectrex)         LogoFile='Vectrex/VideoSnaps/logo.mp4' && Loop=true  ;;
-    wswan)           LogoFile='WS/VideoSnaps/logo.mp4' && Loop=true  ;;
-    wswanc)          LogoFile='WS/VideoSnaps/logo.mp4' && Loop=true  ;;
+    vectrex)         LogoFile='Vectrex/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    wswan)           LogoFile='WS/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    wswanc)          LogoFile='WS/VideoSnaps/logo.mp4' && Loop=-1  ;;
     intellivision)   LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}"  ;; # tbd
     channelf)        LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}"  ;; # tbd
-    colecovision)    LogoFile='ColecoVision/VideoSnaps/logo.mp4' && Loop=false  ;;
-    3do)             LogoFile='3DO/VideoSnaps/logo.mp4' && Loop=true  ;;
+    colecovision)    LogoFile='ColecoVision/VideoSnaps/logo.mp4' && Loop=-1  ;;
+    3do)             LogoFile='3DO/VideoSnaps/logo.mp4' && Loop=-1  ;;
     # Misc               
-    doom)             LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}" ;;
+    doom)            LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}" ;;
+    #doom)            LogoFile="recalbox.png" && Loop=0 ;;
     cavestory)       LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}" ;; # tbd
     lutro)           LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}" ;; # tbd
     imageviewer)     LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}" ;; # tbd
-    scummvm)         LogoFile='SCUMMVM/VideoSnaps/logo.mp4' && Loop=false  ;;
+    scummvm)         LogoFile='SCUMMVM/VideoSnaps/logo.mp4' && Loop=-1  ;;
     moonlight)       LogoFile="${DefaultLogo}" && Loop="${DefaultLogoLoop}"  ;; # tbd
-    kodi)            LogoFile='Kodi/logo.png' && Loop=false  ;;
+    kodi)            LogoFile='Kodi/logo.png' && Loop=-1  ;;
     *)                   
 esac
 }
