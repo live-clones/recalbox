3e34b57259cf8a2c7f673dd5f47d0a13a181a297
paradadf <paradad@outlook.com>
2020-05-28 11:45:05 +0000

    chore(cheats): use version label

--- package/libretro-cheats/libretro-cheats.mk
+++ package/libretro-cheats/libretro-cheats.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRETRO_CHEATS_VERSION = 53ab2a9bdd5376d33ae1e732eb3cdc70611bc987
+LIBRETRO_CHEATS_VERSION = v1.8.8
 LIBRETRO_CHEATS_SITE = $(call github,libretro,libretro-database,$(LIBRETRO_CHEATS_VERSION))
 
 define LIBRETRO_CHEATS_INSTALL_TARGET_CMDS
