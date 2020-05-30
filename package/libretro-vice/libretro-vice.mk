a30ef16f10038336b5d0763fd496bb66e392e953
paradadf <paradad@outlook.com>
2020-05-30 14:08:43 +0000

    chore(vice): bump

--- package/libretro-vice/libretro-vice.mk
+++ package/libretro-vice/libretro-vice.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRETRO_VICE_VERSION = 461527f0ba52439223f1439ecb4a2006d6560735
+LIBRETRO_VICE_VERSION = 0156e9537a4799a6b43d549ff3ac961b09fc88b4
 LIBRETRO_VICE_SITE = $(call github,libretro,vice-libretro,$(LIBRETRO_VICE_VERSION))
 
 LIBRETRO_VICE_SUBEMULATORS = x64 x64sc x128 xpet xplus4 xvic xcbm2 xscpu64 xcbm5x0
