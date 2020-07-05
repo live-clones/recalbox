9bd49ed003975ebc0756e35c5e050bda72951201
Bkg2k <bkg2k9@gmail.com>
2020-07-05 15:38:21 +0200

    feat(assets): move tft assets into their own package

---
+++ package/recalbox-tft-assets/recalbox-tft-assets.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# Recalbox TFT assets
+#
+################################################################################
+
+RECALBOX_TFT_ASSETS_SOURCE=
+RECALBOX_TFT_ASSETS_VERSION=1.0
+
+define RECALBOX_TFT_ASSETS_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/recalbox/share_init/system
+	cp -r $(RECALBOX_TFT_ASSETS_PKGDIR)/tft_logos $(TARGET_DIR)/recalbox/share_init/system/
+endef
+
+$(eval $(generic-package))
