43c4e585ddca91e19717e78dded7126deb652462
David Barbion <davidb@230ruedubac.fr>
2020-07-05 23:45:51 +0200

    chore(kodi-scripts): reorganize kodi addons, 2nd part

---
+++ package/kodi-script-module-future/kodi_script_module_future.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# kodi future script
+#
+################################################################################
+
+KODI_SCRIPT_MODULE_FUTURE_VERSION = 0.17.1
+KODI_SCRIPT_MODULE_FUTURE_SOURCE = script.module.future-$(KODI_SCRIPT_MODULE_FUTURE_VERSION).zip
+KODI_SCRIPT_MODULE_FUTURE_SITE = https://mirrors.kodi.tv/addons/krypton/script.module.future
+KODI_SCRIPT_MODULE_FUTURE_PLUGIN_NAME = script.module.future
+
+KODI_SCRIPT_MODULE_FUTURE_TARGET_DIR=$(TARGET_DIR)/usr/share/kodi/addons
+
+define KODI_SCRIPT_MODULE_FUTURE_EXTRACT_CMDS
+	@unzip -q -o $(DL_DIR)/kodi-script-module-future/$(KODI_SCRIPT_MODULE_FUTURE_SOURCE) -d $(@D)
+endef
+
+define KODI_SCRIPT_MODULE_FUTURE_INSTALL_TARGET_CMDS
+	@mkdir -p $(KODI_SCRIPT_MODULE_FUTURE_TARGET_DIR)
+	@cp -r $(@D)/$(KODI_SCRIPT_MODULE_FUTURE_PLUGIN_NAME) $(KODI_SCRIPT_MODULE_FUTURE_TARGET_DIR)/
+endef
+
+$(eval $(generic-package))
---
+++ package/kodi-script-module-inputstreamhelper/kodi_script_module_inputstreamhelper.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# kodi inputstreamhelper script
+#
+################################################################################
+
+KODI_SCRIPT_MODULE_INPUTSTREAMHELPER_VERSION = 0.5.0
+KODI_SCRIPT_MODULE_INPUTSTREAMHELPER_SOURCE = script.module.inputstreamhelper-$(KODI_SCRIPT_MODULE_INPUTSTREAMHELPER_VERSION).zip
+KODI_SCRIPT_MODULE_INPUTSTREAMHELPER_SITE = https://mirrors.kodi.tv/addons/krypton/script.module.inputstreamhelper
+KODI_SCRIPT_MODULE_INPUTSTREAMHELPER_PLUGIN_NAME = script.module.inputstreamhelper
+
+KODI_SCRIPT_MODULE_INPUTSTREAMHELPER_TARGET_DIR=$(TARGET_DIR)/usr/share/kodi/addons
+
+define KODI_SCRIPT_MODULE_INPUTSTREAMHELPER_EXTRACT_CMDS
+	@unzip -q -o $(DL_DIR)/kodi-script-module-inputstreamhelper/$(KODI_SCRIPT_MODULE_INPUTSTREAMHELPER_SOURCE) -d $(@D)
+endef
+
+define KODI_SCRIPT_MODULE_INPUTSTREAMHELPER_INSTALL_TARGET_CMDS
+	@mkdir -p $(KODI_SCRIPT_MODULE_INPUTSTREAMHELPER_TARGET_DIR)
+	@cp -r $(@D)/$(KODI_SCRIPT_MODULE_INPUTSTREAMHELPER_PLUGIN_NAME) $(KODI_SCRIPT_MODULE_INPUTSTREAMHELPER_TARGET_DIR)/
+endef
+
+$(eval $(generic-package))
