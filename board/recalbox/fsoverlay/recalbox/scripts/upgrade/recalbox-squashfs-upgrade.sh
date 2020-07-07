8f4d0cf6277f09a9ec98b2a788af33b140ed353a
David Barbion <davidb@230ruedubac.fr>
2020-07-08 00:21:56 +0200

    chore(upgrade): S11upgrade unarchive on the fly to share

--- board/recalbox/fsoverlay/recalbox/scripts/upgrade/recalbox-squashfs-upgrade.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/upgrade/recalbox-squashfs-upgrade.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-function do_update() {
+do_update() {
 
   UPDATEFILE="rbx_no_upgrade"
-  UPDATEFILE2="rbx_no_upgrade"
 
-  for file in /recalbox/share/system/upgrade/recalbox-*.img.xz; do
+  for file in /recalbox/share/system/upgrade/recalbox-*.img; do
     if [[ -f $file ]]; then
       UPDATEFILE=${file}
-      UPDATEFILE2=${file::-3}
       break
     fi
   done
@@ -17,26 +15,17 @@ function do_update() {
   echo "stopping emulationstation"
   /etc/init.d/S31emulationstation stop
   dd if=/dev/zero of=/dev/fb0 > /dev/null 2>&1
-  fbv -f -i /recalbox/system/resources/offline-install-0.jpg &
-  sleep 1
-
-  # Uncompress img
   fbv -f -i /recalbox/system/resources/offline-install-1.jpg &
   sleep 1
-  if test -e "${UPDATEFILE}"; then
-    unxz "${UPDATEFILE}"
-  else
-    return 1
-  fi
 
   # Mount
   echo "mount image"
   fbv -f -i /recalbox/system/resources/offline-install-2.jpg &
   sleep 1
-  losetup /dev/loop1 "${UPDATEFILE2}" -o 1048576
+  losetup /dev/loop1 "${UPDATEFILE}" -o 1048576
   RESULT=$?
   if [ $RESULT -ne 0 ]; then
-    losetup /dev/loop1 "${UPDATEFILE2}" -o $((2048 * 512)) || return 1
+    losetup /dev/loop1 "${UPDATEFILE}" -o $((2048 * 512)) || return 2
   fi
   mount /dev/loop1 /mnt || return 1
 
@@ -65,7 +54,8 @@ function do_update() {
   umount /mnt/ || return 1
 
   # Clean up
-  rm "${UPDATEFILE2}" || return 1
+  echo "cleanup"
+  rm "${UPDATEFILE}" || return 9
   fbv -f -i /recalbox/system/resources/offline-install-6.jpg &
   sleep 1
 
