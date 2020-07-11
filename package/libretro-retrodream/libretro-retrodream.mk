eca8aa5e3d8442f39efe55b3e717e3802ffaecde
Bkg2k <bkg2k9@gmail.com>
2020-07-11 18:49:27 +0200

    fix(retrodream): fix compilation parameters

--- package/libretro-retrodream/libretro-retrodream.mk
+++ package/libretro-retrodream/libretro-retrodream.mk
@@ -20,10 +20,14 @@ endif
 
 define LIBRETRO_RETRODREAM_BUILD_CMDS
 	$(SED) "s|-O2|-O3|g" $(@D)/deps/libretro/Makefile
+	# Kill residual X86 flags...
+	$(SED) "s|CAPSTONE_HAS_X86|CAPSTONE_HAS_ARM|g" $(@D)/deps/libretro/Makefile
+	# Kill shared memory (cause memory allocation failure)
+	$(SED) "s|-DHAVE_FASTMEM||g" $(@D)/deps/libretro/Makefile.common
 	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
 		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
 		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" AR="$(TARGET_AR)" -C $(@D)/deps/libretro -f Makefile platform="$(RETROARCH_LIBRETRO_PLATFORM)" $(LIBRETRO_RETRODREAM_OPTS)
+		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" AR="$(TARGET_AR)" -C $(@D)/deps/libretro -f Makefile platform="unix-$(RETROARCH_LIBRETRO_PLATFORM)" $(LIBRETRO_RETRODREAM_OPTS)
 endef
 
 define LIBRETRO_RETRODREAM_INSTALL_TARGET_CMDS
