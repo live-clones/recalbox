19013a193c00dbc65bb74288682813e788b77fad
Bkg2k <bkg2k9@gmail.com>
2020-07-24 09:13:50 +0200

    chore(emulationstation): bump

--- package/recalbox-emulationstation2/recalbox-emulationstation2.mk
+++ package/recalbox-emulationstation2/recalbox-emulationstation2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_EMULATIONSTATION2_VERSION = 248cbcc68d175c771e42a4e8612e5c7d88ddd5f7
+RECALBOX_EMULATIONSTATION2_VERSION = 52e36d8deb3db7214c99b33f4ca192ecbdf39786
 RECALBOX_EMULATIONSTATION2_SITE = https://gitlab.com/recalbox/recalbox-emulationstation.git
 RECALBOX_EMULATIONSTATION2_SITE_METHOD = git
 RECALBOX_EMULATIONSTATION2_LICENSE = MIT
