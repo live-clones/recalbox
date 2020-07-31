d112c090962b597125fdce4d46d7f416fc5df25b
Bkg2k <bkg2k9@gmail.com>
2020-07-31 14:22:05 +0200

    fix(sound): changed pi4 mixer name to hdmi.

--- projects/frontend/es-app/src/VolumeControl.h
+++ projects/frontend/es-app/src/VolumeControl.h
@@ -1,55 +1,36 @@
 #pragma once
 
-#include <memory>
-
-#if defined (__APPLE__)
-    #warning TODO: Not implemented for MacOS yet!!!
-#elif defined(__linux__)
-	#include <alsa/asoundlib.h>
-#elif defined(WIN32) || defined(_WIN32)
-	#include <Windows.h>
-	#include <MMSystem.h>
-	#include <mmdeviceapi.h>
-	#include <endpointvolume.h>
-#endif
+#include <alsa/asoundlib.h>
 
 /*!
 Singleton pattern. Call getInstance() to get an object.
 */
 class VolumeControl
 {
-		int originalVolume;
-		int internalVolume;
+    int originalVolume;
+    int internalVolume;
 
-#if defined (__APPLE__)
-    #warning TODO: Not implemented for MacOS yet!!!
-#elif defined(__linux__)
-    static const char * mixerName;
-    static const char * mixerCard;
     int mixerIndex;
-	  snd_mixer_t* mixerHandle;
+    snd_mixer_t* mixerHandle;
     snd_mixer_elem_t* mixerElem;
     snd_mixer_selem_id_t* mixerSelemId;
-#elif defined(WIN32) || defined(_WIN32)
-	HMIXER mixerHandle;
-	MIXERCONTROL mixerControl;
-	IAudioEndpointVolume * endpointVolume;
-#endif
 
-	static std::weak_ptr<VolumeControl> sInstance;
+    VolumeControl();
 
-	VolumeControl();
-	//VolumeControl(const VolumeControl & right);
-  //VolumeControl & operator=(const VolumeControl & right);
+    static const char* getMixerName();
 
-public:
-	static std::shared_ptr<VolumeControl> & getInstance();
+    static const char* getMixerCard();
 
-	void init();
-	void deinit();
+  public:
+    static VolumeControl* getInstance();
 
-	int getVolume() const;
-	void setVolume(int volume);
+    void init();
 
-	~VolumeControl();
+    void deinit();
+
+    int getVolume() const;
+
+    void setVolume(int volume);
+
+    ~VolumeControl();
 };
