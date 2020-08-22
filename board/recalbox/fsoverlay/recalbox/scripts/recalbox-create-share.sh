a851a2cefde69a9b1a81c2d7fc6d4d110d1f497f
Bkg2k <bkg2k9@gmail.com>
2020-08-22 11:00:54 +0200

    fix(boot-video): fix mpv options for x86 in recalbox-creating-share.sh

--- board/recalbox/fsoverlay/recalbox/scripts/recalbox-create-share.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/recalbox-create-share.sh
@@ -11,7 +11,32 @@ if [ "$1" = "stop" ]; then
 	exit 0
 fi
 
+machineArch=$(arch)
+
+# findConnectedConnectors
+#   returns a list of connected connectors (using mpv)
+findConnectedConnectors() {
+  grep -l ^connected /sys/class/drm/card*/status | sed 's/.*card\([0-9]\+\)-\([^\/]\+\).*/\1.\2/'
+}
+
+# playVideo
+#   select all screen and run playVideoOnOneScreen for each connected screen
+# intput:
+# * filename to play
+playVideo() {
+  local conn
+  if [[ "$machineArch" =~ x86 ]]; then
+    # on x86 and x86_64, find connected monitors and play video on all of them
+    findConnectedConnectors | while read -r conn; do
+      mpv --loop --fs --really-quiet --fps 5 --no-correct-pts -ao=alsa --vo=drm --drm-connector=$conn /recalbox/system/resources/creating-share.mp4 &
+    done
+  else
+    # on rpi, xu4, simply play
+    mpv --loop --fs --really-quiet --fps 5 --no-correct-pts /recalbox/system/resources/creating-share.mp4 &
+  fi
+}
+
 # Start
 touch /tmp/newshare
-mpv --loop --fs --really-quiet --fps 5 --no-correct-pts /recalbox/system/resources/creating-share.mp4 &
+playVideo
 exit 0
