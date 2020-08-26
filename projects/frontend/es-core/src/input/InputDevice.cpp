362e09ed01d0ab8072ddb09dd2dcdd30b8338fad
Bkg2k <bkg2k9@gmail.com>
2020-08-26 19:29:52 +0200

    fix(input): fix patched hk in xbox360 compatible pads

--- projects/frontend/es-core/src/input/InputDevice.cpp
+++ projects/frontend/es-core/src/input/InputDevice.cpp
@@ -228,7 +228,7 @@ bool InputDevice::LoadAutoConfiguration(const std::string& originalConfiguration
   // Hard-patch for XBox360 & clone mapping
   if (GUID() == "030000005e0400008e02000014010000")
   {
-    configuration = "a:b0,b:b1,x:b2,y:b3,back:b8,start:b9,guide:b10," \
+    configuration = "a:b0,b:b1,x:b2,y:b3,back:b8,start:b9,guide:b8," \
                     "dpdown:h0.4,dpleft:h0.8,dpright:h0.2,dpup:h0.1," \
                     "leftshoulder:b4,rightshoulder:b5,lefttrigger:b6,righttrigger:b7,leftstick:b11,rightstick:b12," \
                     "leftx:a0,lefty:a1,rightx:a2,righty:a3,";
