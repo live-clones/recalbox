cde0ebe7a853693421f396853fcf34ffb11a6e8f
David Barbion <davidb@230ruedubac.fr>
2020-09-23 21:23:09 +0200

    fix: simplify module build

--- package/db9_gpio_rpi/db9_gpio_rpi.mk
+++ package/db9_gpio_rpi/db9_gpio_rpi.mk
@@ -4,25 +4,11 @@
 #
 ################################################################################
 
-DB9_GPIO_RPI_VERSION = 6f257ff226b866a37b0ea6d6f0a7f91b5b73c421
+DB9_GPIO_RPI_VERSION = c21faee29e42696722a953a8eee9093a223f1408
 DB9_GPIO_RPI_SITE = $(call github,marqs85,db9_gpio_rpi,$(DB9_GPIO_RPI_VERSION))
 
-DB9_GPIO_RPI_DEPENDENCIES = linux
-DB9_GPIO_RPI_STRIP_COMPONENTS = 1
-
-define DB9_GPIO_RPI_COPY_CMDS
-	cp -r $(@D)/db9_gpio_rpi-1.2/* $(@D)/.
-endef
-
-DB9_GPIO_RPI_CONFIGURE_CMDS += $(DB9_GPIO_RPI_COPY_CMDS)
-
-define DB9_GPIO_RPI_BUILD_CMDS
-	$(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR)
-endef
-
-define DB9_GPIO_RPI_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR) modules_install
-endef
+DB9_GPIO_RPI_MODULE_SUBDIRS = db9_gpio_rpi-1.2
+DB9_GPIO_RPI_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
