43dce4355407511ba3b0a9df90a4748803eebec4
David Barbion <davidb@230ruedubac.fr>
2020-09-24 09:24:37 +0200

    feat: no db9 code for rpi4 compatibility

--- package/db9_gpio_rpi/db9_gpio_rpi.mk
+++ package/db9_gpio_rpi/db9_gpio_rpi.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-DB9_GPIO_RPI_VERSION = c21faee29e42696722a953a8eee9093a223f1408
-DB9_GPIO_RPI_SITE = $(call github,marqs85,db9_gpio_rpi,$(DB9_GPIO_RPI_VERSION))
+DB9_GPIO_RPI_VERSION = v1.3.1
+DB9_GPIO_RPI_SITE = https://gitlab.com/recalbox/db9_gpio_rpi.git
+DB9_GPIO_RPI_SITE_METHOD = git
 
 DB9_GPIO_RPI_MODULE_SUBDIRS = db9_gpio_rpi-1.2
 DB9_GPIO_RPI_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)
