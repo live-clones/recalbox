2aa5c1a6474552b78d5934fc5608b5dc65464bc4
David Barbion <davidb@230ruedubac.fr>
2020-09-28 21:56:32 +0200

    feat: add youtube key (this is insecure)

--- package/kodi-plugin-video-youtube/kodi_plugin_video_youtube.mk
+++ package/kodi-plugin-video-youtube/kodi_plugin_video_youtube.mk
@@ -18,6 +18,7 @@ endef
 define KODI_PLUGIN_VIDEO_YOUTUBE_INSTALL_TARGET_CMDS
 	@mkdir -p $(KODI_PLUGIN_VIDEO_YOUTUBE_TARGET_DIR)
 	@cp -r $(@D)/$(KODI_PLUGIN_VIDEO_YOUTUBE_PLUGINNAME) $(KODI_PLUGIN_VIDEO_YOUTUBE_TARGET_DIR)
+	$(INSTALL) -D -m 0644 $(KODI_PLUGIN_VIDEO_YOUTUBE_PKGDIR)/api_keys.json $(TARGET_DIR)/recalbox/share_init/system/.kodi/userdata/addon_data/plugin.video.youtube/api_keys.json
 endef
 
 $(eval $(generic-package))
