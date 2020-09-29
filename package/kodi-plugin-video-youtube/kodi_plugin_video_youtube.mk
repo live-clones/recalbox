73996f197a0c5bb9f37dd1be4d9ed24c423382da
OyyoDams <oyyodams@recalbox.com>
2020-09-29 10:19:09 +0200

    chore: kodi video plugin api update

--- package/kodi-plugin-video-youtube/kodi_plugin_video_youtube.mk
+++ package/kodi-plugin-video-youtube/kodi_plugin_video_youtube.mk
@@ -18,7 +18,7 @@ endef
 define KODI_PLUGIN_VIDEO_YOUTUBE_INSTALL_TARGET_CMDS
 	@mkdir -p $(KODI_PLUGIN_VIDEO_YOUTUBE_TARGET_DIR)
 	@cp -r $(@D)/$(KODI_PLUGIN_VIDEO_YOUTUBE_PLUGINNAME) $(KODI_PLUGIN_VIDEO_YOUTUBE_TARGET_DIR)
-	$(INSTALL) -D -m 0644 $(KODI_PLUGIN_VIDEO_YOUTUBE_PKGDIR)/api_keys.json $(TARGET_DIR)/recalbox/share_init/system/.kodi/userdata/addon_data/plugin.video.youtube/api_keys.json
+	[ -f $(BR2_EXTERNAL_RECALBOX_PATH)/api_keys.json ] && $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_RECALBOX_PATH)/api_keys.json $(TARGET_DIR)/recalbox/share_init/system/.kodi/userdata/addon_data/plugin.video.youtube/api_keys.json
 endef
 
 $(eval $(generic-package))
