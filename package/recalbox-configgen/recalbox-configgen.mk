700a63d8226531d7a0d475e17730efbadf400b24
Bkg2k <bkg2k9@gmail.com>
2020-10-05 10:34:17 +0200

    chore(configgen): bump configgen to fix dosbox generator & mupen64 l1/r1

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = 31de94f0e91bbd499edc4a6654ea0fc633ba6a40
+RECALBOX_CONFIGGEN_VERSION = e36c49fbd947f3a6d375134b54b882b89810e1fe
 
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
