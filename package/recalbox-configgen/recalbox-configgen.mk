86c0b76693e6a6c7dd1f6941d8b3b8e89d802594
Bkg2k <bkg2k9@gmail.com>
2020-10-09 09:26:39 +0200

    fix(config): revert mupen64 l/r fix in configgen

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = e36c49fbd947f3a6d375134b54b882b89810e1fe
+RECALBOX_CONFIGGEN_VERSION = 11f17851e253aeb30dcf558943718a09d1ddae3b
 
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
