57a8835886ac240441d3493518955edfce290919
lekeux kevin <strodown@gmail.com>
2020-10-14 21:00:46 +0200

    chore(citra): change to citra qt and change repository
    - patch for disable popup message on first boot 
    
    
    chore(citra): add netplay config in recalbox conf
    
    - add birthday in recalbox conf month day  
    chore(citra): update recalbox romfs 
    
    
    chore(citra): fix romfs bump to nightly-1549 and add hotkey patch 
    
    
    
    
    fix hotkey

--- package/citra-emu/citra-emu.mk
+++ package/citra-emu/citra-emu.mk
@@ -4,18 +4,20 @@
 #
 ################################################################################
 
-CITRA_EMU_VERSION = nightly-1399
-CITRA_EMU_SITE = https://github.com/citra-emu/citra-nightly.git
+##on version "nightly-1549"
+CITRA_EMU_VERSION = c4ba7b362696ff424044856974d9ae8e2263dd73
+CITRA_EMU_SITE = https://github.com/citra-emu/citra.git
 CITRA_EMU_SITE_METHOD = git
 CITRA_EMU_GIT_SUBMODULES = YES
 CITRA_EMU_LICENSE = GPLv2
-CITRA_EMU_DEPENDENCIES = sdl2 ffmpeg boost
+CITRA_EMU_DEPENDENCIES = fmt ffmpeg boost qt5base qt5tools qt5multimedia
 CITRA_EMU_SUPPORTS_IN_SOURCE_BUILD = NO
 
-CITRA_EMU_CONF_OPTS += -DENABLE_QT=OFF
+CITRA_EMU_CONF_OPTS += -DENABLE_QT=ON
 CITRA_EMU_CONF_OPTS += -DENABLE_CUBEB=OFF
 CITRA_EMU_CONF_OPTS += -DENABLE_SDL2=ON
 CITRA_EMU_CONF_OPTS += -DENABLE_WEB_SERVICE=ON
+CITRA_EMU_CONF_OPTS += -DENABLE_QT_TRANSLATION=ON
 CITRA_EMU_CONF_OPTS += -DENABLE_FFMPEG_AUDIO_DECODER=ON
 CITRA_EMU_CONF_OPTS += -DENABLE_FFMPEG_VIDEO_DUMPER=ON
 CITRA_EMU_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
@@ -24,27 +26,27 @@ CITRA_EMU_CONF_ENV += LDFLAGS=-lpthread
 define CITRA_EMU_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/bin
 	mkdir -p $(TARGET_DIR)/usr/lib
-	$(INSTALL) -D $(@D)/buildroot-build/bin/citra \
+	$(INSTALL) -D $(@D)/buildroot-build/bin/citra-qt \
 		$(TARGET_DIR)/usr/bin/citra
 	$(INSTALL) -D $(@D)/buildroot-build/bin/citra-room \
 		$(TARGET_DIR)/usr/bin/citra-room
-	cp -pr $(@D)/buildroot-build/externals/inih/lib* \
+	cp -pr $(@D)/buildroot-build/externals/inih/*.so \
 		$(TARGET_DIR)/usr/lib/
-	cp -pr $(@D)/buildroot-build/externals/dynarmic/src/lib* \
+	cp -pr $(@D)/buildroot-build/externals/dynarmic/src/*.so \
 		$(TARGET_DIR)/usr/lib/
-	cp -pr $(@D)/buildroot-build/externals/teakra/src/lib* \
+	cp -pr $(@D)/buildroot-build/externals/teakra/src/*.so \
 		$(TARGET_DIR)/usr/lib/
-	cp -pr $(@D)/buildroot-build/externals/libressl/tls/lib* \
+	cp -pr $(@D)/buildroot-build/externals/libressl/tls/*.so \
 		$(TARGET_DIR)/usr/lib/
-	cp -pr $(@D)/buildroot-build/externals/libressl/ssl/lib* \
+	cp -pr $(@D)/buildroot-build/externals/libressl/ssl/*.so \
 		$(TARGET_DIR)/usr/lib/
-	cp -pr $(@D)/buildroot-build/externals/libressl/crypto/lib* \
+	cp -pr $(@D)/buildroot-build/externals/libressl/crypto/*.so \
 		$(TARGET_DIR)/usr/lib/
-	cp -pr $(@D)/buildroot-build/externals/lurlparser/lib*\
+	cp -pr $(@D)/buildroot-build/externals/lurlparser/*.so \
 		$(TARGET_DIR)/usr/lib/
-	cp -pr $(@D)/buildroot-build/externals/fmt/lib* \
+	cp -pr $(@D)/buildroot-build/externals/fmt/*.so \
 		$(TARGET_DIR)/usr/lib/
-	cp -pr $(@D)/buildroot-build/externals/lodepng/lib* \
+	cp -pr $(@D)/buildroot-build/externals/lodepng/*.so \
 		$(TARGET_DIR)/usr/lib/
 endef
 
--- package/recalbox-romfs/recalbox-romfs-3ds/recalbox-romfs-3ds.mk
+++ package/recalbox-romfs/recalbox-romfs-3ds/recalbox-romfs-3ds.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # Package generated with :
-# ./scripts/linux/empack.py --system 3ds --extension '.3ds .3DS' --fullname 'Nintendo 3DS' --platform 3ds --theme 3ds BR2_PACKAGE_CITRA_EMU
+# ./scripts/linux/empack.py --force --system 3ds --extension '.3ds .3DS' --fullname 'Nintendo 3DS' --platform 3ds --theme 3ds 1:citra:citra:BR2_PACKAGE_CITRA_EMU
 
 # Name the 3 vars as the package requires
 RECALBOX_ROMFS_3DS_SOURCE = 
@@ -21,11 +21,39 @@ SOURCE_ROMDIR_3DS = $(RECALBOX_ROMFS_3DS_PKGDIR)/roms
 # variables are global across buildroot
 
 
-ifeq ($(BR2_PACKAGE_CITRA_EMU),y)
-define CONFIGURE_3DS
-	$(call RECALBOX_ROMFS_CALL_ADD_STANDALONE_SYSTEM,$(SYSTEM_XML_3DS),Nintendo 3DS,$(SYSTEM_NAME_3DS),.3ds .3DS,3ds,3ds,$(SOURCE_ROMDIR_3DS),$(@D))
+ifneq ($(BR2_PACKAGE_CITRA_EMU),)
+define CONFIGURE_MAIN_3DS_START
+	$(call RECALBOX_ROMFS_CALL_ADD_SYSTEM,$(SYSTEM_XML_3DS),Nintendo 3DS,$(SYSTEM_NAME_3DS),.3ds .3DS,3ds,3ds)
 endef
-RECALBOX_ROMFS_3DS_CONFIGURE_CMDS += $(CONFIGURE_3DS)
+
+ifneq ($(BR2_PACKAGE_CITRA_EMU),)
+define CONFIGURE_3DS_CITRA_START
+	$(call RECALBOX_ROMFS_CALL_START_EMULATOR,$(SYSTEM_XML_3DS),citra)
+endef
+ifeq ($(BR2_PACKAGE_CITRA_EMU),y)
+define CONFIGURE_3DS_CITRA_CITRA_DEF
+	$(call RECALBOX_ROMFS_CALL_ADD_CORE,$(SYSTEM_XML_3DS),citra,1)
+endef
 endif
+
+define CONFIGURE_3DS_CITRA_END
+	$(call RECALBOX_ROMFS_CALL_END_EMULATOR,$(SYSTEM_XML_3DS))
+endef
+endif
+
+
+
+define CONFIGURE_MAIN_3DS_END
+	$(call RECALBOX_ROMFS_CALL_END_SYSTEM,$(SYSTEM_XML_3DS),$(SOURCE_ROMDIR_3DS),$(@D))
+endef
+endif
+
+define RECALBOX_ROMFS_3DS_CONFIGURE_CMDS
+	$(CONFIGURE_MAIN_3DS_START)
+	$(CONFIGURE_3DS_CITRA_START)
+	$(CONFIGURE_3DS_CITRA_CITRA_DEF)
+	$(CONFIGURE_3DS_CITRA_END)
+	$(CONFIGURE_MAIN_3DS_END)
+endef
 
 $(eval $(generic-package))
