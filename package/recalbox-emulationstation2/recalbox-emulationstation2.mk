6490f534f1d93851e19fc11a849458e7301c7bec
Bkg2k <bkg2k9@gmail.com>
2020-10-16 17:20:27 +0200

    fix(update): fix xu4 downloaded file

--- package/recalbox-emulationstation2/recalbox-emulationstation2.mk
+++ package/recalbox-emulationstation2/recalbox-emulationstation2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_EMULATIONSTATION2_VERSION = 6aa397740b7bf64a6324fbaf4d3bb2cc5cc9b574
+RECALBOX_EMULATIONSTATION2_VERSION = e29785804fa523e93faad8d719bf8819a05e9424
 RECALBOX_EMULATIONSTATION2_SITE = https://gitlab.com/recalbox/recalbox-emulationstation.git
 RECALBOX_EMULATIONSTATION2_SITE_METHOD = git
 RECALBOX_EMULATIONSTATION2_LICENSE = MIT
