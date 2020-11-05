2f305b42f24c235f16a9a0f65dca8c8b05321cf0
Bkg2k <bkg2k9@gmail.com>
2020-11-05 08:35:02 +0100

    fix(cores): back to x64 as primary x64 emulator. x64sc is too slow

--- package/recalbox-romfs/recalbox-romfs-c64/recalbox-romfs-c64.mk
+++ package/recalbox-romfs/recalbox-romfs-c64/recalbox-romfs-c64.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # Package generated with :
-# ./scripts/linux/empack.py --force --system c64 --extension '.20 .40 .60 .a0 .b0 .bin .cmd .crt .d64 .d6z .d71 .d7z .d80 .d81 .d82 .d8z .g41 .g4z .g64 .g6z .gz .m3u .nbz .nib .p00 .prg .t64 .tap .vfl .vsf .x64 .x6z .zip .7z' --fullname 'Commodore 64' --platform c64 --theme c64 1:libretro:vice_x64sc:BR2_PACKAGE_LIBRETRO_VICE 2:libretro:vice_x64:BR2_PACKAGE_LIBRETRO_VICE 3:libretro:vice_xplus4:BR2_PACKAGE_LIBRETRO_VICE 4:libretro:vice_xpet:BR2_PACKAGE_LIBRETRO_VICE 5:libretro:vice_xvic:BR2_PACKAGE_LIBRETRO_VICE 6:libretro:vice_x128:BR2_PACKAGE_LIBRETRO_VICE 7:libretro:vice_xcbm2:BR2_PACKAGE_LIBRETRO_VICE 8:libretro:vice_xscpu64:BR2_PACKAGE_LIBRETRO_VICE 9:libretro:vice_xcbm5x0:BR2_PACKAGE_LIBRETRO_VICE
+# ./scripts/linux/empack.py --force --system c64 --extension '.20 .40 .60 .a0 .b0 .bin .cmd .crt .d64 .d6z .d71 .d7z .d80 .d81 .d82 .d8z .g41 .g4z .g64 .g6z .gz .m3u .nbz .nib .p00 .prg .t64 .tap .vfl .vsf .x64 .x6z .zip .7z' --fullname 'Commodore 64' --platform c64 --theme c64 2:libretro:vice_x64sc:BR2_PACKAGE_LIBRETRO_VICE 1:libretro:vice_x64:BR2_PACKAGE_LIBRETRO_VICE 3:libretro:vice_xplus4:BR2_PACKAGE_LIBRETRO_VICE 4:libretro:vice_xpet:BR2_PACKAGE_LIBRETRO_VICE 5:libretro:vice_xvic:BR2_PACKAGE_LIBRETRO_VICE 6:libretro:vice_x128:BR2_PACKAGE_LIBRETRO_VICE 7:libretro:vice_xcbm2:BR2_PACKAGE_LIBRETRO_VICE 8:libretro:vice_xscpu64:BR2_PACKAGE_LIBRETRO_VICE 9:libretro:vice_xcbm5x0:BR2_PACKAGE_LIBRETRO_VICE
 
 # Name the 3 vars as the package requires
 RECALBOX_ROMFS_C64_SOURCE = 
@@ -50,7 +50,7 @@ endif
 
 ifeq ($(BR2_PACKAGE_LIBRETRO_VICE),y)
 define CONFIGURE_C64_LIBRETRO_VICE_X64_DEF
-	$(call RECALBOX_ROMFS_CALL_ADD_CORE,$(SYSTEM_XML_C64),vice_x64,2)
+	$(call RECALBOX_ROMFS_CALL_ADD_CORE,$(SYSTEM_XML_C64),vice_x64,1)
 endef
 endif
 
@@ -62,7 +62,7 @@ endif
 
 ifeq ($(BR2_PACKAGE_LIBRETRO_VICE),y)
 define CONFIGURE_C64_LIBRETRO_VICE_X64SC_DEF
-	$(call RECALBOX_ROMFS_CALL_ADD_CORE,$(SYSTEM_XML_C64),vice_x64sc,1)
+	$(call RECALBOX_ROMFS_CALL_ADD_CORE,$(SYSTEM_XML_C64),vice_x64sc,2)
 endef
 endif
 
