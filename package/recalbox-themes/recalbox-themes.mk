919189c3bcc5d007f038b5f3997335b7d5960d23
Bkg2k <bkg2k9@gmail.com>
2020-11-06 19:55:22 +0100

    chore(themes): add odroid go advance 2 theme

--- package/recalbox-themes/recalbox-themes.mk
+++ package/recalbox-themes/recalbox-themes.mk
@@ -4,13 +4,15 @@
 #
 ################################################################################
 
-RECALBOX_THEMES_VERSION = a6af2ceae029b3a3ddc21ee4d1e16e5276777132
+RECALBOX_THEMES_VERSION = 90155611053daecc00474ae3c58b30109539adc0
 RECALBOX_THEMES_SITE = https://gitlab.com/recalbox/recalbox-themes
 RECALBOX_THEMES_SITE_METHOD = git
 
 define RECALBOX_THEMES_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/recalbox/share_init/system/.emulationstation/themes/
 	cp -r $(@D)/themes/recalbox-next \
+		$(TARGET_DIR)/recalbox/share_init/system/.emulationstation/themes/
+	cp -r $(@D)/themes/recalbox-goa2 \
 		$(TARGET_DIR)/recalbox/share_init/system/.emulationstation/themes/
 endef
 
