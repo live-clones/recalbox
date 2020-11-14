cd45b55841df1518c73936d90178147ccf99cdb2
David Barbion <davidb@230ruedubac.fr>
2020-11-14 23:22:17 +0100

    chore(share): force INTERNAL share if ANYEXTERNAL or DEV is selected
    This is needed as the odroid go2 does not work well with
    USB keys: the USB host gets shutdown on suspend but not
    powered up at wake-up.
    To workaround this behavior, the dwc kernel module is unloaded
    right before suspend and load at wake-up. This makes USB great again
    but USB keys appears with a new kernel device. This could lead to
    filesystem corruption.
    This commit addresses this possible data corruption

---
+++ board/recalbox/fsoverlay/recalbox/scripts/recalbox-utils.sh
@@ -0,0 +1,42 @@
+# utilities
+# how to use this file:
+# in your bash script:
+#
+# source /recalbox/scripts/recalbox-utils.sh
+#
+
+RECALBOX_ARCH_FILE="/recalbox/recalbox.arch"
+
+# Return the current running arch (or board)
+# sanitized
+# returns 0 on success and the name of the board on stdout
+# returns 1 on error and unknown on stdout
+getArchName() {
+  local ARCH
+  if [ -f "$RECALBOX_ARCH_FILE" ]; then
+    ARCH=$(head -n 1 "$RECALBOX_ARCH_FILE")
+    if [ -n "$ARCH" ]; then
+      echo "$ARCH"
+      return 0
+    else
+      echo "unknown"
+      return 1
+    fi
+  else
+    echo "unknown"
+    return 1
+  fi
+}
+
+# Test if the current board allows mounting external share (USB, not network)
+# i.e sharedevice=ANYEXTERNAL or sharedevice=DEV fs-uuid from /boot/recalbox-boot.conf
+# currently, odroidgo2 does not support mounting USB share as
+# it becomes unavailable after power suspend
+# return 0 if the current board support mounting an external share
+# return 1 if the current board DOES NOT support mounting an external share
+doesBoardSupportExternalShare() {
+  if [ "$(getArchName)" = 'odroidgo2' ]; then
+    return 1
+  fi
+  return 0
+}
