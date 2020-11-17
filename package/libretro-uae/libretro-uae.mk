aa89a521380afa80e20d5fdbf77d09e0c333869d
Bkg2k <bkg2k9@gmail.com>
2020-11-17 13:00:31 +0100

    fix(puae): fix patches & fix kickstart names in whdl amiga script

--- package/libretro-uae/libretro-uae.mk
+++ package/libretro-uae/libretro-uae.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LIBRETRO_UAE_VERSION = 6bcad0b3f3fd19b4c4d35a4befd7bc9535f7410b
-LIBRETRO_UAE_SITE = $(call github,libretro,libretro-uae,$(LIBRETRO_UAE_VERSION))
+LIBRETRO_UAE_VERSION = e187a61d53caf2214c3e76cc0f4625f66d338212
+LIBRETRO_UAE_SITE = $(call github,recalbox,packages/libretro/libretro-uae,$(LIBRETRO_UAE_VERSION))
 
 ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
 UAEPLATFORM=rpi
@@ -33,21 +33,6 @@ endif
 define LIBRETRO_UAE_BUILD_CMDS
 	$(SED) "s|-O2|-O3|g" $(@D)/Makefile
 	$(SED) "s|^PLATFLAGS :=|PLATFLAGS := $(UAEPLATFLAGS)|g" $(@D)/Makefile
-	$(SED) "s+defined(WIIU)+defined(WIIU) || defined(AARCH64)+g" $(@D)/retrodep/machdep/maccess.h
-	$(SED) "s+defined(WIIU)+defined(WIIU) || defined(AARCH64)+g" $(@D)/retrodep/machdep/m68kops.h
-	# Rename bios to match amiberry
-	$(SED) 's/"kick34005.A500"/"kick34005.A500.rom"/g' $(@D)/libretro/libretro-glue.h
-	$(SED) 's/"kick37175.A500"/"kick37175.A500.rom"/g' $(@D)/libretro/libretro-glue.h
-	$(SED) 's/"kick40063.A600"/"kick40063.A600.rom"/g' $(@D)/libretro/libretro-glue.h
-	$(SED) 's/"kick40068.A1200"/"kick40068.A1200.rom"/g' $(@D)/libretro/libretro-glue.h
-	$(SED) 's/"kick40068.A4000"/"kick40068.A4000.rom"/g' $(@D)/libretro/libretro-glue.h
-	$(SED) 's/"kick34005.CDTV"/"kick34005.CDTV.rom"/g' $(@D)/libretro/libretro-glue.h
-	$(SED) 's/"kick40060.CD32"/"kick40060.CD32.rom"/g' $(@D)/libretro/libretro-glue.h
-	$(SED) 's/"kick40060.CD32.ext"/"kick40060.CD32.ext.rom"/g' $(@D)/libretro/libretro-glue.h
-	$(SED) 's/kick33180.A500/kick33180.A500.rom/g' $(@D)/whdload/WHDLoad_files/S/KSCopy
-	$(SED) 's/kick34005.A500/kick34005.A500.rom/g' $(@D)/whdload/WHDLoad_files/S/KSCopy
-	$(SED) 's/kick40063.A600/kick40063.A600.rom/g' $(@D)/whdload/WHDLoad_files/S/KSCopy
-	$(SED) 's/kick40068.A1200/kick40068.A1200.rom/g' $(@D)/whdload/WHDLoad_files/S/KSCopy
 	CFLAGS="$(TARGET_CFLAGS) $(LIBRETRO_UAE_COMPILER_COMMONS_CFLAGS)" \
 		CXXFLAGS="$(TARGET_CXXFLAGS) $(LIBRETRO_UAE_COMPILER_COMMONS_CXXFLAGS)" \
 		LDFLAGS="$(TARGET_LDFLAGS) $(LIBRETRO_UAE_COMPILER_COMMONS_LDFLAGS)" \
