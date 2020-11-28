23485cb109d3f0a5308dd44cf36928b06e00a889
Bkg2k <bkg2k9@gmail.com>
2020-11-28 10:24:46 +0100

    fix(ui): fix popup delay not saved

--- projects/frontend/es-app/src/guis/menus/GuiMenuPopupSettings.cpp
+++ projects/frontend/es-app/src/guis/menus/GuiMenuPopupSettings.cpp
@@ -14,34 +14,46 @@ GuiMenuPopupSettings::GuiMenuPopupSettings(Window& window)
   //help popup time 0=no popup
   mHelp = std::make_shared<SliderComponent>(mWindow, 0.f, 10.f, 1.f, "s");
   mHelp->setSlider((float) RecalboxConf::Instance().GetPopupHelp());
+  mHelp->setSelectedChangedCallback(SetHelpPopupTime);
   mMenu.addWithLabel(mHelp, _("HELP POPUP DURATION"), _(MENUMESSAGE_UI_HELP_POPUP_DURATION_HELP_MSG));
 
   //music popup time 0=no popup
   mMusic = std::make_shared<SliderComponent>(mWindow, 0.f, 10.f, 1.f, "s");
   mMusic->setSlider((float) RecalboxConf::Instance().GetPopupMusic());
+  mMusic->setSelectedChangedCallback(SetMusicPopupTime);
   mMenu.addWithLabel(mMusic, _("MUSIC POPUP DURATION"), _(MENUMESSAGE_UI_MUSIC_POPUP_DURATION_HELP_MSG));
 
   //netplay popup time 0=no popup
   mNetplay = std::make_shared<SliderComponent>(mWindow, 0.f, 10.f, 1.f, "s");
   mNetplay->setSlider((float) RecalboxConf::Instance().GetPopupNetplay());
+  mNetplay->setSelectedChangedCallback(SetNetplayPopupTime);
   mMenu.addWithLabel(mNetplay, _("NETPLAY POPUP DURATION"), _(MENUMESSAGE_UI_NETPLAY_POPUP_DURATION_HELP_MSG));
 }
 
 void GuiMenuPopupSettings::SetHelpPopupTime(float time)
 {
-  RecalboxConf::Instance().SetPopupHelp((int)time);
-  RecalboxConf::Instance().Save();
+  if (time != RecalboxConf::Instance().GetPopupHelp())
+  {
+    RecalboxConf::Instance().SetPopupHelp((int) time);
+    RecalboxConf::Instance().Save();
+  }
 }
 
 void GuiMenuPopupSettings::SetMusicPopupTime(float time)
 {
-  RecalboxConf::Instance().SetPopupMusic((int)time);
-  RecalboxConf::Instance().Save();
+  if (time != RecalboxConf::Instance().GetPopupMusic())
+  {
+    RecalboxConf::Instance().SetPopupMusic((int) time);
+    RecalboxConf::Instance().Save();
+  }
 }
 
 void GuiMenuPopupSettings::SetNetplayPopupTime(float time)
 {
-  RecalboxConf::Instance().SetPopupNetplay((int)time);
-  RecalboxConf::Instance().Save();
+  if (time != RecalboxConf::Instance().GetPopupNetplay())
+  {
+    RecalboxConf::Instance().SetPopupNetplay((int) time);
+    RecalboxConf::Instance().Save();
+  }
 }
 
