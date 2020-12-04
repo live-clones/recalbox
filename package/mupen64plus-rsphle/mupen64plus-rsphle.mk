c2e3bd6b49244ca2525425e6d48406551d32a1b2
David Barbion <davidb@230ruedubac.fr>
2020-12-05 00:18:38 +0100

    feat(rsphle): bump mupen64plus rsphle plugin

--- package/mupen64plus-rsphle/mupen64plus-rsphle.mk
+++ package/mupen64plus-rsphle/mupen64plus-rsphle.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MUPEN64PLUS_RSPHLE_VERSION = 2df8038d5f8fb722326c98d717b2d571a6d716ed
+MUPEN64PLUS_RSPHLE_VERSION = bb5698577052f6b8913d77ee591e4a2b0a921b4c
 MUPEN64PLUS_RSPHLE_SITE = $(call github,mupen64plus,mupen64plus-rsp-hle,$(MUPEN64PLUS_RSPHLE_VERSION))
 MUPEN64PLUS_RSPHLE_LICENSE = MIT
 MUPEN64PLUS_RSPHLE_DEPENDENCIES = sdl2 alsa-lib mupen64plus-core
