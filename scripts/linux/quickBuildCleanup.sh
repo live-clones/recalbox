bcad24f06c8af7fb63fd83f28976f8efe672704d
digitalLumberjack <digitallumberjack@gmail.com>
2020-12-18 09:01:52 +0100

    feat(quickbuild): remove packages and romfs when needed

---
+++ scripts/linux/quickBuildCleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -euxo pipefail
+
+
+git fetch origin "${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}"
+
+# Removing modified packages
+for package in $(git diff --name-status HEAD..origin/master| grep package | awk -F'/' '{ print $2 }');do 
+    echo rm -rf output/build/${package}*
+done
+
+
+# Removing romfs when board or config modified
+if git diff --name-status HEAD..origin/master | grep -e "board\|config"; then
+    echo rm output/build/recalbox-romfs*
+fi
