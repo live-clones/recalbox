e72de8a0402151e5c8a65ab036f50d534cb4dd1d
David Barbion <davidb@230ruedubac.fr>
2021-02-21 20:48:34 +0100

    chore(odroidgo2): handle boot.ini headers for old uboot (before 3/12/2020) when upgrading

---
+++ package/recalbox-system/odroidgo2/pre-upgrade.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This part will fetch the boot.ini header
+# and will replace in boot.ini that comes with the update.
+# This is done as for uboot from 3/12/2020 looks for
+#   odroidgoa-uboot-config
+OLDBOOT=$(head -n 1 /boot/boot.ini)
+NEWBOOT=$(head -n 1 /boot/update/boot.ini)
+
+if [ "$NEWBOOT" != "$OLDBOOT" ]; then
+  mount -o remount,rw /boot
+  sed -i -E '1 s/^.*$/'$OLDBOOT'/' /boot/update/boot.ini
+  mount -o remount,ro /boot
+fi
+
+# remove old es_input.cfg as the joypad name has changed upstrem
+rm /recalbox/share/system/.emulationstation/es_input.cfg
+
+# disable visibility of pre-installed games to avoid double display of those games
+grep -Eq '^\s*global.hidepreinstalledgames=' /recalbox/share/system/recalbox.conf || \
+  echo -e '\nglobal.hidepreinstalledgames=1' >>/recalbox/share/system/recalbox.conf
