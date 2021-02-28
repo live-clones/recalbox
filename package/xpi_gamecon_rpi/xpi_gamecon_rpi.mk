95a3f6e78ee635ff29097fe37f4d2d3fabdb4b57
David Barbion <davidb@230ruedubac.fr>
2021-02-28 20:59:05 +0100

    fix: use original xpi source code and add recalbox patches

--- package/xpi_gamecon_rpi/xpi_gamecon_rpi.mk
+++ package/xpi_gamecon_rpi/xpi_gamecon_rpi.mk
@@ -4,21 +4,11 @@
 #
 ################################################################################
 
-XPI_GAMECON_RPI_VERSION = v0.1
+XPI_GAMECON_RPI_VERSION = v0.2
 XPI_GAMECON_RPI_SITE = https://gitlab.com/recalbox/packages/hardware/xpi_gamecon_rpi.git
 XPI_GAMECON_RPI_SITE_METHOD = git
 XPI_GAMECON_RPI_DEPENDENCIES = linux
 XPI_GAMECON_RPI_LICENSE = GPL-3.0
-
-define XPI_GAMECON_RPI_BUILD_CMDS
-	$(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR)
-endef
-
-define XPI_GAMECON_RPI_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR) modules_install
-	$(INSTALL) -d -m 0755 $(TARGET_DIR)/home/pi
-	cp -r $(XPI_GAMECON_RPI_PKGDIR)/ressources/osd $(TARGET_DIR)/home/pi
-endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
