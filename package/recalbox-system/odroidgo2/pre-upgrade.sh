a14ecf6bc776d6fb4faf05f03dfb25f42651e715
David Barbion <davidb@230ruedubac.fr>
2021-03-14 00:50:08 +0100

    fix: use old magic string for odroidgo2 and odroidgo3 boards

--- package/recalbox-system/odroidgo2/pre-upgrade.sh
+++ package/recalbox-system/odroidgo2/pre-upgrade.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# This part will fetch the boot.ini header
-# and will replace in boot.ini that comes with the update.
-# This is done as for uboot from 3/12/2020 looks for
-#   odroidgoa-uboot-config
-OLDBOOT=$(head -n 1 /boot/boot.ini)
-NEWBOOT=$(head -n 1 /boot/update/boot.ini)
-
-if [ "$NEWBOOT" != "$OLDBOOT" ]; then
-  mount -o remount,rw /boot
-  sed -i -E '1 s/^.*$/'$OLDBOOT'/' /boot/update/boot.ini
-  mount -o remount,ro /boot
-fi
-
 # remove old es_input.cfg as the joypad name has changed upstrem
 rm /recalbox/share/system/.emulationstation/es_input.cfg
 
