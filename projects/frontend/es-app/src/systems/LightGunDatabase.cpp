2eea8acb4e5e8952304f4f62e40b071a050886ee
Bkg2k <bkg2k9@gmail.com>
2021-03-17 13:20:36 +0100

    chore(lightgun): improve game name simplification

--- projects/frontend/es-app/src/systems/LightGunDatabase.cpp
+++ projects/frontend/es-app/src/systems/LightGunDatabase.cpp
@@ -37,16 +37,23 @@ std::string LightGunDatabase::GetSimplifiedName(const std::string& name)
 {
   std::string result(name);
 
+  // Kill decorations
+  size_t pos1 = result.find('(');
+  size_t pos2 = result.find('[');
+  if ((pos2 != std::string::npos) && (pos2 < pos1)) pos1 = pos2;
+  if (pos1 != std::string::npos) result = result.substr(0, pos1);
+
+  // Crunch non a-z,0-9,! characters
   int writeIndex = 0;
   for(int i = -1; ++i < (int)result.size(); )
   {
     unsigned int c = result[i] | 0x20; // upper => lowercase letters, no effect on digits
-    if (((unsigned int)(c - 0x30) <= 9) || ((unsigned int)(c - 0x61) <= 25))
+    if (((unsigned int)(c - 0x30) <= 9) || ((unsigned int)(c - 0x61) <= 25) || (c == ('!' | 0x20)))
       result[writeIndex++] = (char)c;
   }
   result.resize(writeIndex);
 
-  //{ LOG(LogDebug) << "[LightGun] " << name << " = " << result; }
+  { LOG(LogDebug) << "[LightGun] " << name << " = " << result; }
 
   return result;
 }
