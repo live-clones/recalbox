c67c6db9fa8588aae40a84d32a1bfb6cdb702f01
paradadf <paradad@outlook.com>
2021-03-23 10:33:36 +0000

    chore(wsv): rename final part

--- package/recalbox-romfs/recalbox-romfs-supervision/recalbox-romfs-supervision.mk
+++ package/recalbox-romfs/recalbox-romfs-supervision/recalbox-romfs-supervision.mk
@@ -1,59 +1,59 @@
 ################################################################################
 #
-# recalbox-romfs-wsv
+# recalbox-romfs-supervision
 #
 ################################################################################
 
 # Package generated with :
-# ./scripts/linux/empack.py --force --system wsv --extension '.sv .zip .7z' --fullname 'Watara Supervision' --platform wsv --theme wsv 1:libretro:potator:BR2_PACKAGE_LIBRETRO_POTATOR
+# ./scripts/linux/empack.py --force --system supervision --extension '.sv .zip .7z' --fullname 'Watara Supervision' --platform wsv --theme supervision 1:libretro:potator:BR2_PACKAGE_LIBRETRO_POTATOR
 
 # Name the 3 vars as the package requires
-RECALBOX_ROMFS_WSV_SOURCE = 
-RECALBOX_ROMFS_WSV_SITE = 
-RECALBOX_ROMFS_WSV_INSTALL_STAGING = NO
+RECALBOX_ROMFS_SUPERVISION_SOURCE = 
+RECALBOX_ROMFS_SUPERVISION_SITE = 
+RECALBOX_ROMFS_SUPERVISION_INSTALL_STAGING = NO
 # Set the system name
-SYSTEM_NAME_WSV = wsv
-SYSTEM_XML_WSV = $(@D)/$(SYSTEM_NAME_WSV).xml
+SYSTEM_NAME_SUPERVISION = supervision
+SYSTEM_XML_SUPERVISION = $(@D)/$(SYSTEM_NAME_SUPERVISION).xml
 # System rom path
-SOURCE_ROMDIR_WSV = $(RECALBOX_ROMFS_WSV_PKGDIR)/roms
+SOURCE_ROMDIR_SUPERVISION = $(RECALBOX_ROMFS_SUPERVISION_PKGDIR)/roms
 
 # CONFIGGEN_STD_CMD is defined in recalbox-romfs, so take good care that
 # variables are global across buildroot
 
 
 ifneq ($(BR2_PACKAGE_LIBRETRO_POTATOR),)
-define CONFIGURE_MAIN_WSV_START
-	$(call RECALBOX_ROMFS_CALL_ADD_SYSTEM,$(SYSTEM_XML_WSV),Watara Supervision,$(SYSTEM_NAME_WSV),.sv .zip .7z,wsv,wsv)
+define CONFIGURE_MAIN_SUPERVISION_START
+	$(call RECALBOX_ROMFS_CALL_ADD_SYSTEM,$(SYSTEM_XML_SUPERVISION),Watara Supervision,$(SYSTEM_NAME_SUPERVISION),.sv .zip .7z,wsv,supervision)
 endef
 
 ifneq ($(BR2_PACKAGE_LIBRETRO_POTATOR),)
-define CONFIGURE_WSV_LIBRETRO_START
-	$(call RECALBOX_ROMFS_CALL_START_EMULATOR,$(SYSTEM_XML_WSV),libretro)
+define CONFIGURE_SUPERVISION_LIBRETRO_START
+	$(call RECALBOX_ROMFS_CALL_START_EMULATOR,$(SYSTEM_XML_SUPERVISION),libretro)
 endef
 ifeq ($(BR2_PACKAGE_LIBRETRO_POTATOR),y)
-define CONFIGURE_WSV_LIBRETRO_POTATOR_DEF
-	$(call RECALBOX_ROMFS_CALL_ADD_CORE,$(SYSTEM_XML_WSV),potator,1)
+define CONFIGURE_SUPERVISION_LIBRETRO_POTATOR_DEF
+	$(call RECALBOX_ROMFS_CALL_ADD_CORE,$(SYSTEM_XML_SUPERVISION),potator,1)
 endef
 endif
 
-define CONFIGURE_WSV_LIBRETRO_END
-	$(call RECALBOX_ROMFS_CALL_END_EMULATOR,$(SYSTEM_XML_WSV))
+define CONFIGURE_SUPERVISION_LIBRETRO_END
+	$(call RECALBOX_ROMFS_CALL_END_EMULATOR,$(SYSTEM_XML_SUPERVISION))
 endef
 endif
 
 
 
-define CONFIGURE_MAIN_WSV_END
-	$(call RECALBOX_ROMFS_CALL_END_SYSTEM,$(SYSTEM_XML_WSV),$(SOURCE_ROMDIR_WSV),$(@D))
+define CONFIGURE_MAIN_SUPERVISION_END
+	$(call RECALBOX_ROMFS_CALL_END_SYSTEM,$(SYSTEM_XML_SUPERVISION),$(SOURCE_ROMDIR_SUPERVISION),$(@D))
 endef
 endif
 
-define RECALBOX_ROMFS_WSV_CONFIGURE_CMDS
-	$(CONFIGURE_MAIN_WSV_START)
-	$(CONFIGURE_WSV_LIBRETRO_START)
-	$(CONFIGURE_WSV_LIBRETRO_POTATOR_DEF)
-	$(CONFIGURE_WSV_LIBRETRO_END)
-	$(CONFIGURE_MAIN_WSV_END)
+define RECALBOX_ROMFS_SUPERVISION_CONFIGURE_CMDS
+	$(CONFIGURE_MAIN_SUPERVISION_START)
+	$(CONFIGURE_SUPERVISION_LIBRETRO_START)
+	$(CONFIGURE_SUPERVISION_LIBRETRO_POTATOR_DEF)
+	$(CONFIGURE_SUPERVISION_LIBRETRO_END)
+	$(CONFIGURE_MAIN_SUPERVISION_END)
 endef
 
 $(eval $(generic-package))
