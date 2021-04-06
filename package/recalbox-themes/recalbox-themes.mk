56b9fc2fc269d28915010ec60189cc6e8e063a9b
Bkg2k <bkg2k9@gmail.com>
2021-04-06 11:53:32 +0200

    chore: bump theme

--- package/recalbox-themes/recalbox-themes.mk
+++ package/recalbox-themes/recalbox-themes.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 ifneq ($(GITLAB_TOKEN_THEMES),)
-RECALBOX_THEMES_VERSION = 0b5711bf77e444c5dd288715f5260d17216899f9
+RECALBOX_THEMES_VERSION = d9aeb82e8bce2d46c6e926fb4064ecfd38d9b1f5
 RECALBOX_THEMES_SITE = https://gitlab-ci-token:$(GITLAB_TOKEN_THEMES)@gitlab.com/recalbox/recalbox-themes-prime
 else
 RECALBOX_THEMES_VERSION = dd10b43c5bf48861274a77af9e604b1fa5bcc2ae
