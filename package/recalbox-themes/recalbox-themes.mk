22bb50af6135a56ae5940863e6f1f505b8d07343
Bkg2k <bkg2k9@gmail.com>
2021-04-13 07:36:48 +0200

    chore: bump all subprojects

--- package/recalbox-themes/recalbox-themes.mk
+++ package/recalbox-themes/recalbox-themes.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 ifneq ($(GITLAB_TOKEN_THEMES),)
-RECALBOX_THEMES_VERSION = 6fbee47ef2a10b6c45738f21458e584390f64021
+RECALBOX_THEMES_VERSION = 0ba3ba6d3986f386aaaf8d73b719114c9ff782d0
 RECALBOX_THEMES_SITE = https://gitlab-ci-token:$(GITLAB_TOKEN_THEMES)@gitlab.com/recalbox/recalbox-themes-prime
 else
 RECALBOX_THEMES_VERSION = dd10b43c5bf48861274a77af9e604b1fa5bcc2ae
