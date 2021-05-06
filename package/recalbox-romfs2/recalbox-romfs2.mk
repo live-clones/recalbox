0933825f853f1974836f332656059de52203aca2
Bkg2k <bkg2k9@gmail.com>
2021-05-06 17:15:15 +0200

    chore(romfs): allow merging with older es_systems.cfg

--- package/recalbox-romfs2/recalbox-romfs2.mk
+++ package/recalbox-romfs2/recalbox-romfs2.mk
@@ -5,15 +5,15 @@
 ################################################################################
 
 RECALBOX_ROMFS2_LICENSE = MIT
-RECALBOX_ROMFS2_DEPENDECIES = host-python3
+RECALBOX_ROMFS2_DEPENDECIES = host-python3 recalbox-romfs
 
 define RECALBOX_ROMFS2_BUILD_CMDS
-	python3 $(RECALBOX_ROMFS2_PKGDIR)/scripts/romfs.py -buildsystems $(@D)/es_systems.cfg
+	$(HOST_DIR)/usr/bin/python3 $(RECALBOX_ROMFS2_PKGDIR)/scripts/romfs.py -systems $(RECALBOX_ROMFS2_PKGDIR)/systems -buildsystems $(@D)/es_systems.cfg
 endef
 
 define RECALBOX_ROMFS2_INSTALL_TARGET_CMDS
 	cp $(@D)/es_systems.cfg $(TARGET_DIR)/recalbox/share_init/system/.emulationstation/
-	python3 $(@D)/romfs.py -installroms $(TARGET_DIR)/recalbox/share_init/roms/
+	$(HOST_DIR)/usr/bin/python3 $(RECALBOX_ROMFS2_PKGDIR)/scripts/romfs.py -systems $(RECALBOX_ROMFS2_PKGDIR)/systems -installroms $(TARGET_DIR)/recalbox/share_init/roms/
 endef
 
 $(eval $(generic-package))
