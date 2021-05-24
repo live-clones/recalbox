8a67b7edb5b2c7cd8f3a6d65b0137b4374ec8a49
David Barbion <davidb@230ruedubac.fr>
2021-05-24 21:37:35 +0200

    feat: replace Moonlight embedded with Moonlight QT
    The moonlight-qt patch disables the prompt in the moonlight-qt UI. So, when
    pairing is complete moonlight-qt will quit. The pairing process can continue
    
    The Q&D part in Moonlight.sh is about exporting the cert and key from
    moonlight-qt config file to files expected by recalbox scripts.

---
+++ package/moonlight-qt/moonlight-qt.mk
@@ -0,0 +1,48 @@
+################################################################################
+#
+# moonlight-qt
+#
+################################################################################
+
+MOONLIGHT_QT_VERSION = v4.2.1
+MOONLIGHT_QT_SITE = https://github.com/moonlight-stream/moonlight-qt.git
+MOONLIGHT_QT_SITE_METHOD = git
+MOONLIGHT_QT_GIT_SUBMODULES=y
+MOONLIGHT_QT_DEPENDENCIES = opus expat libevdev avahi alsa-lib udev libcurl libcec ffmpeg sdl2 libenet \
+			qt5quickcontrols2 qt5svg pulseaudio openssl sdl2_ttf qt5base
+MOONLIGHT_QT_LICENSE = GPL-3.0
+MOONLIGHT_QT_LICENSE_FILES = LICENSE
+
+MOONLIGHT_QT_CONF_OPTS = CONFIG+=embedded CONFIG+=release PREFIX=$(STAGING_DIR)/usr
+
+ifeq ($(BR2_PACKAGE_XORG7),y)
+MOONLIGHT_QT_DEPENDENCIES += xlib_libX11
+endif
+
+ifeq ($(BR2_PACKAGE_HAS_LIBEGL),y)
+MOONLIGHT_QT_DEPENDENCIES += libegl
+endif
+
+ifeq ($(BR2_PACKAGE_HAS_GLES),y)
+MOONLIGHT_QT_DEPENDENCIES += libgles
+endif
+
+ifeq ($(BR2_PACKAGE_HAS_GL),y)
+MOONLIGHT_QT_DEPENDENCIES += libgl
+endif
+
+ifeq ($(BR2_PACKAGE_LIBAMCODEC),y)
+MOONLIGHT_QT_DEPENDENCIES += libamcodec
+endif
+
+ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
+MOONLIGHT_QT_DEPENDENCIES += rpi-userland
+endif
+
+define MOONLIGHT_QT_SCRIPT_INSTALL
+	$(INSTALL) -D -m 0755 $(MOONLIGHT_QT_PKGDIR)/Moonlight.sh $(TARGET_DIR)/recalbox/scripts/moonlight/Moonlight.sh
+endef
+
+MOONLIGHT_QT_POST_INSTALL_TARGET_HOOKS += MOONLIGHT_QT_SCRIPT_INSTALL
+
+$(eval $(qmake-package))
