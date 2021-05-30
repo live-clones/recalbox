79f4ccf760a1fe44a2f950d702ceedb29ad3d37d
David Barbion <davidb@230ruedubac.fr>
2021-05-30 21:19:15 +0200

    fix: change compilation options for pcsx_rearmed

--- package/pcsx_rearmed/pcsx_rearmed.mk
+++ package/pcsx_rearmed/pcsx_rearmed.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PCSX_REARMED_VERSION = b085fc5ae83c7e0f91cb3a8dc00bd14d2b8f9c1c
+PCSX_REARMED_VERSION = fbf19ce6b6c02da47a67f58e23d8294cfcd18f55
 PCSX_REARMED_SITE = git://github.com/notaz/pcsx_rearmed.git
-PCSX_REARMED_DEPENDENCIES = sdl libpng zlib
+PCSX_REARMED_DEPENDENCIES = sdl sdl2 libpng zlib pulseaudio
 PCSX_REARMED_GIT_SUBMODULES = YES
 PCSX_REARMED_LICENSE = GPL-2.0
 PCSX_REARMED_LICENSE_FILES = COPYING
@@ -25,17 +25,33 @@ PCSX_REARMED_SDL_INCLUDE = -I$(STAGING_DIR)/usr/include/SDL
 PCSX_REARMED_SDL_LIBRARY = -L$(STAGING_DIR)/usr/lib
 PCSX_REARMED_ALL_LIBRARIES = $(PCSX_REARMED_SDL_LIBRARY) -ldl -lpthread -lSDL -lpng -lz
 
-define PCSX_REARMED_BUILD_CMDS
-	$(SED) "s|-O2|-O3|g" $(@D)/Makefile
-	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_NOLTO) $(PCSX_REARMED_SDL_INCLUDE)" \
-		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_NOLTO) $(PCSX_REARMED_SDL_INCLUDE)" \
-		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_NOLTO) $(PCSX_REARMED_ALL_LIBRARIES)" \
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" AR="$(TARGET_AR)" -C $(@D) -f Makefile $(PCSX_REARMED_MAKEFILE_CONFIGURATION)
-endef
+PCSX_REARMED_CONF_OPTS += --sound-drivers=pulseaudio
+PCSX_REARMED_CONF_ENV += PATH=$(STAGING_DIR)/usr/bin:$$PATH
+
+ifeq ($(BR2_arm1176jzf_s),y)
+PCSX_REARMED_CONF_ENV += CFLAGS=-march=armv6
+endif
+
+ifeq ($(BR2_ARM_CPU_HAS_NEON),y)
+PCSX_REARMED_CONF_OPTS += --enable-neon
+else
+PCSX_REARMED_CONF_OPTS += --disable-neon
+endif
+
+#define PCSX_REARMED_BUILD_CMDS
+#	$(SED) "s|-O2|-O3|g" $(@D)/Makefile
+#	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_NOLTO) $(PCSX_REARMED_SDL_INCLUDE)" \
+#		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_NOLTO) $(PCSX_REARMED_SDL_INCLUDE)" \
+#		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_NOLTO) $(PCSX_REARMED_ALL_LIBRARIES)" \
+#		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" AR="$(TARGET_AR)" -C $(@D) -f Makefile $(PCSX_REARMED_MAKEFILE_CONFIGURATION)
+#endef
 
 define PCSX_REARMED_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/pcsx_rearmed \
+	$(INSTALL) -D $(@D)/pcsx \
 		$(TARGET_DIR)/usr/bin/pcsx_rearmed
+	$(INSTALL) -D $(@D)/plugins/dfxvideo/gpu_peops.so $(TARGET_DIR)/recalbox/share/system/configs/pcsx/plugins/gpu_peops.so
+	$(INSTALL) -D $(@D)/plugins/gpu_unai/gpu_unai.so $(TARGET_DIR)/recalbox/share/system/configs/pcsx/plugins/gpu_unai.so
+	$(INSTALL) -D $(@D)/plugins/spunull/spunull.so $(TARGET_DIR)/recalbox/share/system/configs/pcsx/plugins/spunull.so
 endef
 
 define PCSX_REARMED_PRE_PATCH_FIXUP
@@ -45,4 +61,4 @@ endef
 
 PCSX_REARMED_PRE_PATCH_HOOKS += PCSX_REARMED_PRE_PATCH_FIXUP
 
-$(eval $(generic-package))
+$(eval $(autotools-package))
