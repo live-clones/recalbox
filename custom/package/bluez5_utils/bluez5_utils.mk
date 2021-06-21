587cfd098cda20e19087698081b910f0e74068e8
David Barbion <davidb@230ruedubac.fr>
2021-06-21 14:06:24 +0200

    chore(bluez5): update patch for 5.58

--- custom/package/bluez5_utils/bluez5_utils.mk
+++ custom/package/bluez5_utils/bluez5_utils.mk
@@ -5,12 +5,14 @@
 ################################################################################
 
 # Keep the version and patches in sync with bluez5_utils-headers
-BLUEZ5_UTILS_VERSION = 5.55
+BLUEZ5_UTILS_VERSION = 5.58
 BLUEZ5_UTILS_SOURCE = bluez-$(BLUEZ5_UTILS_VERSION).tar.xz
 BLUEZ5_UTILS_SITE = $(BR2_KERNEL_MIRROR)/linux/bluetooth
 BLUEZ5_UTILS_INSTALL_STAGING = YES
 BLUEZ5_UTILS_LICENSE = GPL-2.0+, LGPL-2.1+
 BLUEZ5_UTILS_LICENSE_FILES = COPYING COPYING.LIB
+BLUEZ5_UTILS_CPE_ID_VENDOR = bluez
+BLUEZ5_UTILS_CPE_ID_PRODUCT = bluez
 BLUEZ5_UTILS_AUTORECONF = YES
 
 BLUEZ5_UTILS_DEPENDENCIES = \
@@ -134,6 +136,11 @@ BLUEZ5_UTILS_DEPENDENCIES += systemd
 else
 BLUEZ5_UTILS_CONF_OPTS += --disable-systemd
 endif
+
+define BLUEZ5_UTILS_INSTALL_INIT_SYSV
+	$(INSTALL) -m 0755 -D package/bluez5_utils/S40bluetooth \
+		$(TARGET_DIR)/etc/init.d/S40bluetooth
+endef
 
 define BLUEZ5_UTILS_INSTALL_CONF_FILES
 	mkdir -p $(TARGET_DIR)/etc/bluetooth
