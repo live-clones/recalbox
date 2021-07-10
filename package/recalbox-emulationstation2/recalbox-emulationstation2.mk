905a41cebaa80500d1884e5606e8419595e9e470
digitalLumberjack <digitallumberjack@gmail.com>
2021-07-10 20:28:00 +0200

    fix(themerandomepath): fix theme random path - bumping es
    - related to #1793

--- package/recalbox-emulationstation2/recalbox-emulationstation2.mk
+++ package/recalbox-emulationstation2/recalbox-emulationstation2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_EMULATIONSTATION2_VERSION = 2e5324aa8d02c72775d32128dd879ee43584872d
+RECALBOX_EMULATIONSTATION2_VERSION = f3504fcdbf0c100c57cd8ef7d532b05a1a11fd81
 RECALBOX_EMULATIONSTATION2_SITE = https://gitlab.com/recalbox/recalbox-emulationstation.git
 RECALBOX_EMULATIONSTATION2_SITE_METHOD = git
 RECALBOX_EMULATIONSTATION2_LICENSE = MIT
