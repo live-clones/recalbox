5b80e7dac6749307ac9db811c387c50b37295a7c
David Barbion <davidb@230ruedubac.fr>
2021-07-24 13:58:12 +0200

    feat: bump recalbox-hardware for piboy dmg

--- package/recalbox-hardware/recalbox-hardware.mk
+++ package/recalbox-hardware/recalbox-hardware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_HARDWARE_VERSION = 25b41d28aaa65991f7539d27e5509460793cb54c
+RECALBOX_HARDWARE_VERSION = 2102fc40fdc364193572a8b63fb2d549690146aa
 RECALBOX_HARDWARE_SITE = https://gitlab.com/recalbox/recalbox-hardware
 RECALBOX_HARDWARE_LICENSE = MIT
 RECALBOX_HARDWARE_LICENSE_FILES = LICENSE.md
