79dc12920edb68b732b6285cf760329af61980a6
David Barbion <davidb@230ruedubac.fr>
2021-08-03 22:29:29 +0200

    feat: use PID for controlling fan speed

--- projects/wpaf/src/fan_manager.c
+++ projects/wpaf/src/fan_manager.c
@@ -1,3 +1,4 @@
+#include <search.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -9,42 +10,82 @@
 
 #include "cpu_controller.h"
 #include "fan_driver.h"
+#include "fan_manager.h"
+#include "log.h"
+#include "pid_controller.h"
 
-#define HYST_LOW  40 
-#define HYST_HIGH 50
-
-extern int verbose_flag;
+#define HYST_LOW  55
+#define HYST_HIGH 60
+#define HYSTERESIS 2
+#define FAN_END_OF_LIST -1
 
 /*
- * This is sample fan manager
- * works for CC fans (aka on/off)
- * need to implement need functions for PWM (modulated speed fan)
+ * This is fan manager
+ * can handle both CC fans and PWM fans
  */ 
 void start_fan_manager(fan_interface * fan, fan_handler * handler) {
-
-  float temp;
-  bool fan_status = false;
-
   // initialize
   fan->off(handler) ;
+
+  switch (fan->fan_capabilities()) {
+    case FAN_CAP_SIMPLE:
+      log_debug("Fan is simple");
+      manage_simple_fan(fan, handler);
+      break;
+    case FAN_CAP_PWM:
+      log_debug("Fan is PWM");
+      manage_pwm_fan(fan, handler);
+      break;
+  }
+}
+
+/* handle PWM fan
+ */
+void manage_pwm_fan(fan_interface * fan, fan_handler * handler) {
+  float temp;
+  float PID;
+  struct pid * _pid = pid_controller_init(1, 3, -3, 0.01, 0.001, 0.01);
 
   // cpu fan controller main loop
   while(true) {
     temp = get_cpu_temp();
-    if (temp > HYST_HIGH && !fan_status) {
+    PID = -pid_controller_calculate(_pid, 52, temp); // 52 should be read from config file
+    PID = PID >=0 ? PID : 0 ;
+    fan->set_pwm(handler, (uint8_t)PID);
+
+    log_debug("cpu: %.2f", temp);
+
+    log_debug("with pid_controller: %0.10f", PID);
+
+    sleep(1);
+  }
+}
+
+/* start/stop CC fan
+ */
+void manage_simple_fan(fan_interface * fan, fan_handler * handler) {
+  float temp;
+  float PID;
+  struct pid * _pid = pid_controller_init(1, 1, -1, 0.1, 0.01, 0.1); // PID parameters, config file ?
+
+  fan->off(handler);
+  // cpu fan controller main loop
+  while(true) {
+    temp = get_cpu_temp();
+    PID = pid_controller_calculate(_pid, 55, temp) ; // 55 should be read from config file
+    if (PID == _pid->min) {
       fan->on(handler);
-      if (verbose_flag)
-        printf("fan on\n");
-      fan_status = true;
+      log_debug("fan on");
     }
-    if (temp < HYST_LOW && fan_status) {
+    if (PID == _pid->max) {
       fan->off(handler);
-      if (verbose_flag)
-        printf("fan off\n");
-      fan_status = false;
+      log_debug("fan off");
     }
-    if (verbose_flag)
-      printf("cpu: %.2f\n", temp);
+
+    log_debug("cpu: %.2f", temp);
+
+    log_debug("with pid_controller: %0.2f fan_status: %x", PID, fan->status(handler));
+
     sleep(1);
   }
 }
--- projects/wpaf/src/fan_manager.h
+++ projects/wpaf/src/fan_manager.h
@@ -1,8 +1,13 @@
 #ifndef __FAN_MANAGER_H
 #define __FAN_MANAGER_H
 
+#include <stdbool.h>
+#include <stdint.h>
+
 #include "fan_driver.h"
 
-void start_fan_manager(fan_interface *, fan_handler *) ;
+void start_fan_manager(fan_interface *, fan_handler *);
+void manage_simple_fan(fan_interface * fan, fan_handler * handler);
+void manage_pwm_fan(fan_interface * fan, fan_handler * handler);
 
 #endif // __FAN_MANAGER_H
