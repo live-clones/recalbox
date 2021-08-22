cd0171f1159fc5f4e5b215446ae08287d1b9ecc7
digitalLumberjack <digitallumberjack@gmail.com>
2021-08-22 17:37:39 +0200

    feat(recalbox-crt): added recalbox-crt package for CRT config
    - related to #1805

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = 878c6f22a1eb6a68caeb642adabe1d6536f65f1f
+RECALBOX_CONFIGGEN_VERSION = e54bf6737ed2488206f41bea583db15bb6be08f7
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
 
