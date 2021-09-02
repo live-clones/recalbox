7ab6588442a9dfd338dba815da8030ca91417e96
Bkg2k <bkg2k9@gmail.com>
2021-09-02 23:49:54 +0200

    refactor: make simcoupe configuration all lowercase

--- projects/configgen/configgen/generators/simcoupe/simcoupeGenerator.py
+++ projects/configgen/configgen/generators/simcoupe/simcoupeGenerator.py
@@ -26,38 +26,38 @@ class SimCoupeGenerator(Generator):
         settings = keyValueSettings(recalboxFiles.simcoupeConfig)
         settings.loadFile(True)
 
-        settings.setOption("FilterGUI", "1")                                    # Nice looking GUI
-        settings.setOption("InPath", recalboxFiles.ROMS + "/samcoupe")          # default input path
-        settings.setOption("OutPath", recalboxFiles.SAVES + "/samcoupe")        # default output path
-        settings.setOption("Disk1", args.rom)                                   # rom
-        settings.setOption("Fullscreen", "Yes")                                 # Fullscreen
-        settings.setOption("FirstRun", "No")                                    # no first-launch nag-screen #1
-        settings.setOption("CfgVersion", "4")                                   # no first launch nag-screen #2
-        smooth = "Yes" if self.enabled('smooth', system.config) else "No"
-        settings.setOption("Filter", smooth)                                    # no first launch nag-screen #2
+        settings.setOption("filtergui", "1")                                    # Nice looking GUI
+        settings.setOption("inpath", recalboxFiles.ROMS + "/samcoupe")          # default input path
+        settings.setOption("outpath", recalboxFiles.SAVES + "/samcoupe")        # default output path
+        settings.setOption("disk1", args.rom)                                   # rom
+        settings.setOption("fullscreen", "yes")                                 # Fullscreen
+        settings.setOption("firstrun", "no")                                    # no first-launch nag-screen #1
+        settings.setOption("cfgversion", "4")                                   # no first launch nag-screen #2
+        smooth = "yes" if self.enabled('smooth', system.config) else "no"
+        settings.setOption("filter", smooth)                                    # no first launch nag-screen #2
 
         settings.setOption("scanlines", "no")
         if self.defined('shaderset', system.config) and system.config['shaderset'] == 'scanlines':
-            settings.setOption("Scanlines", "Yes")
+            settings.setOption("scanlines", "yes")
 
         for index in playersControllers:
             controller = playersControllers[index]
             if controller.player == "1":
-                settings.setOption("JoyDev1", controller.configName)
-                settings.setOption("JoyHKey1", controller.inputs["hotkey"].id)
-                settings.setOption("JoyStart1", controller.inputs["start"].id)
-                settings.setOption("JoyA1", controller.inputs["a"].id)
-                settings.setOption("JoyB1", controller.inputs["b"].id)
-                settings.setOption("JoyX1", controller.inputs["x"].id)
-                settings.setOption("JoyY1", controller.inputs["y"].id)
+                settings.setOption("joydev1", controller.configName)
+                settings.setOption("joyhkey1", controller.inputs["hotkey"].id)
+                settings.setOption("joystart1", controller.inputs["start"].id)
+                settings.setOption("joya1", controller.inputs["a"].id)
+                settings.setOption("joyb1", controller.inputs["b"].id)
+                settings.setOption("joyx1", controller.inputs["x"].id)
+                settings.setOption("joyy1", controller.inputs["y"].id)
             if controller.player == "2":
-                settings.setOption("JoyDev2", controller.configName)
-                settings.setOption("JoyHKey2", controller.inputs["hotkey"].id)
-                settings.setOption("JoyStart2", controller.inputs["start"].id)
-                settings.setOption("JoyA2", controller.inputs["a"].id)
-                settings.setOption("JoyB2", controller.inputs["b"].id)
-                settings.setOption("JoyX2", controller.inputs["x"].id)
-                settings.setOption("JoyY2", controller.inputs["y"].id)
+                settings.setOption("joydev2", controller.configName)
+                settings.setOption("joyhkey2", controller.inputs["hotkey"].id)
+                settings.setOption("joystart2", controller.inputs["start"].id)
+                settings.setOption("joya2", controller.inputs["a"].id)
+                settings.setOption("joyb2", controller.inputs["b"].id)
+                settings.setOption("joyx2", controller.inputs["x"].id)
+                settings.setOption("joyy2", controller.inputs["y"].id)
 
         settings.saveFile()
 
