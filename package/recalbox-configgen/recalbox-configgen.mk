eb1994061162721602e737a518b31b49a06397dd
Bkg2k <bkg2k9@gmail.com>
2021-09-07 18:04:58 +0200

    feat(emulator): add hatari standalone emulator

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = 54f007c38f7ac3cfecfa4f72f56451af0c6cb943
+RECALBOX_CONFIGGEN_VERSION = b12650f03fb0dbf92d5dce31e27df8c693a8bb35
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
 
