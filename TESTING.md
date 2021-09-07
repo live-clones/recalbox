dbcada0705537a3e8584c1745e0f5160a85fd165
Bkg2k <bkg2k9@gmail.com>
2021-09-07 22:56:09 +0200

    feat(emulator): add duckstation standalone

--- TESTING.md
+++ TESTING.md
@@ -109,6 +109,8 @@ Pour les pending features, elles ne seront activées que si les beta testeurs le
   - [ ] Check the presence of the dats in .zip format (instead of .dat)
 
 - [ ] Test Beebem
+[ ] Check the theme of trs80coco (#1945)
+- [ ] Test Duckstation
 
 ## [7.2.2]
 - [X] Test ppsspp saves are copied in share on upgrade (#1773)
--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = 878c6f22a1eb6a68caeb642adabe1d6536f65f1f
+RECALBOX_CONFIGGEN_VERSION = 616196621bb6aac4d04d5b495aee748575819feb
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
 
