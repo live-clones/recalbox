b080855ffaabf9acbb6e003565ebe5c00635c0f4
Bkg2k <bkg2k9@gmail.com>
2021-09-15 12:43:08 +0200

    feat(emulator): add recalbox version of beebem

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = 46a6255286b0a8802a8031323d24c50ba0f1cec8
+RECALBOX_CONFIGGEN_VERSION = 878c6f22a1eb6a68caeb642adabe1d6536f65f1f
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
 
