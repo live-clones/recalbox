47773c3c60ca3dce00361876ff33e952db393195
Akkeoss <akeos@laposte.net>
2021-09-23 21:40:47 +0200

    feat(theme): add theme for lowres nx

--- package/recalbox-themes/recalbox-themes.mk
+++ package/recalbox-themes/recalbox-themes.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 ifneq ($(GITLAB_TOKEN_THEMES),)
-RECALBOX_THEMES_VERSION = d617b7f826009832ae1a905e1a32f40bbc289e7a
+RECALBOX_THEMES_VERSION = e3366f293216d3dee25deea6e5f041c53e6787dc
 RECALBOX_THEMES_SITE = https://gitlab-ci-token:$(GITLAB_TOKEN_THEMES)@gitlab.com/recalbox/recalbox-themes-prime
 else
 RECALBOX_THEMES_VERSION = dd10b43c5bf48861274a77af9e604b1fa5bcc2ae
