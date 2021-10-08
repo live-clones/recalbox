46c8fce7063e69e44b68c55c49c14e1161046d06
Pit64 <yrigaud@icloud.com>
2021-10-08 18:26:28 +0200

    chore(kodi): update plugins
    - Fix Addon Signals build

--- package/kodi-plugin-video-netflix/kodi_plugin_video_netflix.mk
+++ package/kodi-plugin-video-netflix/kodi_plugin_video_netflix.mk
@@ -6,7 +6,7 @@
 
 KODI_PLUGIN_VIDEO_NETFLIX_VERSION = 1.17.0
 KODI_PLUGIN_VIDEO_NETFLIX_SOURCE = plugin.video.netflix-$(KODI_PLUGIN_VIDEO_NETFLIX_VERSION)+matrix.1.zip
-KODI_PLUGIN_VIDEO_NETFLIX_SITE = https://github.com/CastagnaIT/plugin.video.netflix/releases/download/v$(KODI_PLUGIN_VIDEO_NETFLIX_VERSION)
+KODI_PLUGIN_VIDEO_NETFLIX_SITE = https://castagnait.github.io/repository.castagnait/zip/plugin.video.netflix
 KODI_PLUGIN_VIDEO_NETFLIX_PLUGINNAME = plugin.video.netflix
 KODI_PLUGIN_VIDEO_NETFLIX_DEPENDENCIES = python-pycryptodomex
 KODI_PLUGIN_VIDEO_NETFLIX_LICENSE = MIT
