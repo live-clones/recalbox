49189ab3845bd112d223da17f18c0b126e561b11
digitalLumberjack <digitallumberjack@gmail.com>
2021-10-09 00:45:49 +0200

    feat(recalbox-crt): add 240ptestsuite system
    - built only when recalbox-crt is selected (rpi4 for now)
    - related to #1805

--- package/recalbox-themes/recalbox-themes.mk
+++ package/recalbox-themes/recalbox-themes.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 ifneq ($(GITLAB_TOKEN_THEMES),)
-RECALBOX_THEMES_VERSION = 2b714324bd11eec2454ddb509928f095d9ec804c
+RECALBOX_THEMES_VERSION = e13ac25497c8ad7d1190da9cdf5f0211315df54e
 RECALBOX_THEMES_SITE = https://gitlab-ci-token:$(GITLAB_TOKEN_THEMES)@gitlab.com/recalbox/recalbox-themes-prime
 else
 RECALBOX_THEMES_VERSION = dd10b43c5bf48861274a77af9e604b1fa5bcc2ae
