744c18a495e0b1b240b1b02e20d6a2e294301363
digitalLumberjack <digitallumberjack@gmail.com>
2021-11-04 07:22:54 +0100

    fix(es): fix kms mode change when reinit sdl
    - related to #1991

--- package/recalbox-emulationstation2/recalbox-emulationstation2.mk
+++ package/recalbox-emulationstation2/recalbox-emulationstation2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_EMULATIONSTATION2_VERSION = 063e805f4163e673af39a461f6295860076b98f7
+RECALBOX_EMULATIONSTATION2_VERSION = 1e260ef845aec3be20e92fe0ebe3df8fb0915186
 RECALBOX_EMULATIONSTATION2_SITE = https://gitlab.com/recalbox/recalbox-emulationstation.git
 RECALBOX_EMULATIONSTATION2_SITE_METHOD = git
 RECALBOX_EMULATIONSTATION2_LICENSE = MIT
