1291875a1e70b73e09cb9bdc0d6bdca59c572325
Bkg2k <bkg2k9@gmail.com>
2021-11-12 08:40:44 +0100

    chore(ps2): add system ps2 with pcsx2 emulator

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = c4cfc63e109751561a192039d802b57d29448764
+RECALBOX_CONFIGGEN_VERSION = 5fd562e845bb69355487c61201be99c2b760ae6f
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
 
