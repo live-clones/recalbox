e588147e62400a098ec6a876ad15c12985cff6af
digitalLumberjack <digitallumberjack@gmail.com>
2021-11-21 00:52:03 +0100

    ci(beta): bumping subprojects to last beta version

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = 5fd562e845bb69355487c61201be99c2b760ae6f
+RECALBOX_CONFIGGEN_VERSION = 4e8ddfae68e2ee03c0a445b27aad4de9e53f02cb
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
 
