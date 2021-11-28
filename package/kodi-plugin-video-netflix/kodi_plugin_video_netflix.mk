6a21332e2201eff12874d04263bee850c9cb3cb4
Yves Rigaud <webmaster@zelda-series.net>
2021-11-28 13:14:34 +0100

    chore(kodi): update plugins
    - Updated Netflix from 1.17.0 to 1.18.1

--- package/kodi-plugin-video-netflix/kodi_plugin_video_netflix.mk
+++ package/kodi-plugin-video-netflix/kodi_plugin_video_netflix.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PLUGIN_VIDEO_NETFLIX_VERSION = 1.17.0
+KODI_PLUGIN_VIDEO_NETFLIX_VERSION = 1.18.1
 KODI_PLUGIN_VIDEO_NETFLIX_SOURCE = plugin.video.netflix-$(KODI_PLUGIN_VIDEO_NETFLIX_VERSION)+matrix.1.zip
 KODI_PLUGIN_VIDEO_NETFLIX_SITE = https://castagnait.github.io/repository.castagnait/zip/plugin.video.netflix
 KODI_PLUGIN_VIDEO_NETFLIX_PLUGINNAME = plugin.video.netflix
