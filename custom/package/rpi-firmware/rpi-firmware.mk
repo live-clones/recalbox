e0fbeb314d1357f3b92ace99a7f8459f6120f110
David Barbion <davidb@230ruedubac.fr>
2021-12-09 23:41:36 +0100

    feat: bump rpi kernel to 5.10.83
    This one brings vc4-kms-dpi-generic overlay

--- custom/package/rpi-firmware/rpi-firmware.mk
+++ custom/package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 12bc6e3677348adaffd155e7a04761e2661d4bff
+RPI_FIRMWARE_VERSION = 303d0012cd73da63e21133dadd90532d8328f5c2
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
