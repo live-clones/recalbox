7a234ad0d1a5588523ed6705723fbe23a5f3fb5a
digitalLumberjack <digitallumberjack@gmail.com>
2022-01-04 00:54:02 +0100

    fix(recalbox-crt): fix mode offsetter when values are too high or low

--- projects/configgen/configgen/crt/CRTModeOffsetter.py
+++ projects/configgen/configgen/crt/CRTModeOffsetter.py
@@ -5,6 +5,14 @@ from configgen.utils.recallog import recallog
 class CRTModeOffsetter:
 
     def processMode(self, mode: Mode, horizontal_offset: int, vertical_offset: int) -> Mode:
+        if mode.h_front_porch - horizontal_offset < 1:
+            horizontal_offset = mode.h_front_porch - 1
+        if mode.h_front_porch - horizontal_offset > mode.h_back_porch + horizontal_offset:
+            horizontal_offset = -(mode.h_back_porch - mode.h_front_porch) // 2
+        if mode.v_front_porch - vertical_offset < 1:
+            vertical_offset = mode.v_front_porch - 1
+        if mode.v_front_porch - vertical_offset > mode.v_back_porch + vertical_offset:
+            vertical_offset = -(mode.v_back_porch - mode.v_front_porch) // 2
         mode.h_front_porch -= horizontal_offset
         mode.h_back_porch += horizontal_offset
         mode.v_front_porch -= vertical_offset
--- projects/configgen/tests/crt/CRTModeOffsetter_test.py
+++ projects/configgen/tests/crt/CRTModeOffsetter_test.py
@@ -6,14 +6,34 @@ def test_given_mode_list_and_offset_zero_nothing_move(mocker):
     mode = Mode("1920 1 80 184 312 240 1 2 3 16 0 0 0 60 0 39087360 1")
     assert CRTModeOffsetter().processMode(mode, 0, 0) is mode
 
+
 def test_given_mode_and_offset_10_will_move_horizontally(mocker):
     mode = Mode("1920 1 80 184 312 240 1 2 3 16 0 0 0 60 0 39087360 1")
     assert CRTModeOffsetter().processMode(mode, -10, 0) == Mode("1920 1 90 184 302 240 1 2 3 16 0 0 0 60 0 39087360 1")
+
 
 def test_given_mode_and_offset_1_will_move_vertically(mocker):
     mode = Mode("1920 1 80 184 312 240 1 2 3 16 0 0 0 60 0 39087360 1")
     assert CRTModeOffsetter().processMode(mode, 0, -1) == Mode("1920 1 80 184 312 240 1 3 3 15 0 0 0 60 0 39087360 1")
 
-def test_given_offset_too_low_will_not_make_porch_under_minimal_value(mocker):
+
+def test_given_vertical_offset_too_high_will_not_make_porch_under_minimal_value(mocker):
     mode = Mode("1920 1 80 184 312 240 1 2 3 16 0 0 0 60 0 39087360 1")
-    assert CRTModeOffsetter().processMode(mode, 0, -1) == Mode("1920 1 80 184 312 240 1 3 3 15 0 0 0 60 0 39087360 1")
+    assert CRTModeOffsetter().processMode(mode, 0, 20) == Mode("1920 1 80 184 312 240 1 1 3 17 0 0 0 60 0 39087360 1")
+
+
+def test_given_vertical_offset_too_low_will_make_maximum_porch_value(mocker):
+    mode = Mode("1920 1 80 184 312 240 1 2 3 16 0 0 0 60 0 39087360 1")
+    assert CRTModeOffsetter().processMode(mode, 0, -10) == Mode(
+        "1920 1 80 184 312 240 1 9 3 9 0 0 0 60 0 39087360 1")
+
+
+def test_given_horizontal_offset_too_high_will_not_make_porch_under_minimal_value(mocker):
+    mode = Mode("1920 1 80 184 312 240 1 2 3 16 0 0 0 60 0 39087360 1")
+    assert CRTModeOffsetter().processMode(mode, 200, 0) == Mode("1920 1 1 184 391 240 1 2 3 16 0 0 0 60 0 39087360 1")
+
+
+def test_given_horizontal_offset_too_low_will_make_maximum_porch_value(mocker):
+    mode = Mode("1920 1 80 184 312 240 1 2 3 16 0 0 0 60 0 39087360 1")
+    assert CRTModeOffsetter().processMode(mode, -200, 0) == Mode(
+        "1920 1 196 184 196 240 1 2 3 16 0 0 0 60 0 39087360 1")
