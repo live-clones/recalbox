8a93279f783934c3429ce782e6f4c9a1085a0299
digitalLumberjack <digitallumberjack@gmail.com>
2022-01-11 14:06:38 +0100

    feat(recalbox-crt): add thirdparty dac support
    recalboxrgbdual will be the only module, but two dts are available:
    one for the Recalbox RGB Dual board, and the other for third parties

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = 1fed126dd0a2468c9bcf15d376f76f0e823e3f47
+RECALBOX_CONFIGGEN_VERSION = 4076988560a9785eddf5faea6182bf549e615f2e
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
 
