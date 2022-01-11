46d6e0316abfd689358452f24dbcd172ace39ed7
digitalLumberjack <digitallumberjack@gmail.com>
2022-01-11 14:06:38 +0100

    feat(recalbox-crt): add thirdparty dac support
    recalboxrgbdual will be the only module, but two dts are available:
    one for the Recalbox RGB Dual board, and the other for third parties

--- package/recalbox-rgb-dual/recalbox_rgb_dual.mk
+++ package/recalbox-rgb-dual/recalbox_rgb_dual.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_RGB_DUAL_VERSION = e7de9730f898d15cde05d0e6795161fde740132d
+RECALBOX_RGB_DUAL_VERSION = 2aedbe30ef6242298c6696b3c4b5d0b612f09237
 RECALBOX_RGB_DUAL_SITE = $(call gitlab,recalbox,recalbox-rgb-dual,$(RECALBOX_RGB_DUAL_VERSION))
 RECALBOX_RGB_DUAL_LICENSE = GPL
 RECALBOX_RGB_DUAL_DEPENDENCIES = linux
@@ -16,7 +16,9 @@ endef
 define RECALBOX_RGB_DUAL_INSTALL_TARGET_CMDS
 	$(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR) modules_install
 	$(HOST_DIR)/bin/linux-dtc $(@D)/recalboxrgbdual-overlay.dts -o $(@D)/recalboxrgbdual.dtbo
+	$(HOST_DIR)/bin/linux-dtc $(@D)/recalboxrgbdual-thirdparty-overlay.dts -o $(@D)/recalboxrgbdual-thirdparty.dtbo
 	$(INSTALL) -D -m 0644 $(@D)/recalboxrgbdual.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/recalboxrgbdual.dtbo
+	$(INSTALL) -D -m 0644 $(@D)/recalboxrgbdual-thirdparty.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/recalboxrgbdual-thirdparty.dtbo
 endef
 
 $(eval $(kernel-module))
