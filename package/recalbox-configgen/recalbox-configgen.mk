2c7a2dde807b9b6100b44b5f854774e81dd318f3
Bkg2k <bkg2k9@gmail.com>
2022-01-19 14:04:46 +0100

    fix: fix mupen64plus resolution

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = fae2d5a14e0c199014ebf8f6d7ae6a9c2891d368
+RECALBOX_CONFIGGEN_VERSION = 5733024103f17d5cb4545aa8388ab4c1b83cfa3b
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
 
