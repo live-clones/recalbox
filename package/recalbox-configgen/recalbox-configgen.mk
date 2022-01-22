6955b1e3523bee63f612324182045f641c4be746
digitalLumberjack <digitallumberjack@gmail.com>
2022-01-22 23:05:11 +0100

    feat(recalbox-rgb): add geometry screen for crt

--- package/recalbox-configgen/recalbox-configgen.mk
+++ package/recalbox-configgen/recalbox-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_CONFIGGEN_VERSION = f2f70b10ffc2b6d3fc3a594c44d25206aa2b14ad
+RECALBOX_CONFIGGEN_VERSION = fae2d5a14e0c199014ebf8f6d7ae6a9c2891d368
 RECALBOX_CONFIGGEN_SITE = https://gitlab.com/recalbox/recalbox-configgen.git
 RECALBOX_CONFIGGEN_SITE_METHOD = git
 
