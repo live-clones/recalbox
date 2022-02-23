b46f28d73295328c7fada71db1c08ca7fb5967b2
Bkg2k <bkg2k9@gmail.com>
2022-02-23 13:36:29 +0100

    feat(retroarch): bump retroarch assets

--- package/retroarch-assets/retroarch-assets.mk
+++ package/retroarch-assets/retroarch-assets.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RETROARCH_ASSETS_VERSION = 8d114dd55b0a357efec12d34d02dfe190a6ccc3a
+RETROARCH_ASSETS_VERSION = db5f4f4babb7f6f9c7edd27690c3ffa911cc9b24
 RETROARCH_ASSETS_SITE = $(call github,libretro,retroarch-assets,$(RETROARCH_ASSETS_VERSION))
 RETROARCH_ASSETS_LICENSE = GPL
 RETROARCH_ASSETS_DEPENDENCIES = retroarch
@@ -14,6 +14,7 @@ define RETROARCH_ASSETS_INSTALL_TARGET_CMDS
 	cp -r $(@D)/ozone $(TARGET_DIR)/usr/share/retroarch/assets
 	cp -r $(@D)/rgui $(TARGET_DIR)/usr/share/retroarch/assets
 	cp -r $(@D)/glui $(TARGET_DIR)/usr/share/retroarch/assets
+	cp -r $(@D)/FlatUX $(TARGET_DIR)/usr/share/retroarch/assets
 	cp -r $(@D)/xmb/monochrome $(TARGET_DIR)/usr/share/retroarch/assets/xmb
 	cp -r $(@D)/menu_widgets $(TARGET_DIR)/usr/share/retroarch/assets
 endef
