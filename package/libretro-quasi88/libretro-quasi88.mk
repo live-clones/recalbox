80ee823519a997e3e55cc2e7f488df8d71c0ca22
Bkg2k <bkg2k9@gmail.com>
2022-03-04 11:46:05 +0100

    feat(emulator): bump libretro quasi88 to commit of 2022-02-11

--- package/libretro-quasi88/libretro-quasi88.mk
+++ package/libretro-quasi88/libretro-quasi88.mk
@@ -4,7 +4,8 @@
 #
 ################################################################################
 
-LIBRETRO_QUASI88_VERSION = 1df8c3bcab01eea5ba2ccaf379f6335c0672e9d6
+# Commit of 2022-02-11
+LIBRETRO_QUASI88_VERSION = 15ff9fd94aa9db45a5519d00227180b04e074157
 LIBRETRO_QUASI88_SITE = $(call github,libretro,quasi88-libretro,$(LIBRETRO_QUASI88_VERSION))
 LIBRETRO_QUASI88_LICENSE = BSD-3-Clause
 LIBRETRO_QUASI88_LICENSE_FILES = LICENSE
