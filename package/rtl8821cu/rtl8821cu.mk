524b2028f1f2bf2569418f6717cfafb454228899
David Barbion <davidb@230ruedubac.fr>
2022-03-16 10:04:49 +0100

    feat(system): bump rtl8821cu package

--- package/rtl8821cu/rtl8821cu.mk
+++ package/rtl8821cu/rtl8821cu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8821CU_VERSION = c0990e1f86f477275b12c2ce25d5f03684426f3d
+RTL8821CU_VERSION = 4bdd7c8668562e43564cd5d786055633e591ad4d
 RTL8821CU_SITE = $(call github,morrownr,8821cu-20210118,$(RTL8821CU_VERSION))
 RTL8821CU_LICENSE = GPL-2.0
 RTL8821CU_LICENSE_FILES = LICENSE
