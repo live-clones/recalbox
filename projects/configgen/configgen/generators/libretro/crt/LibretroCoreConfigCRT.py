afab8331294e8bb3fc3082e04793898e00b5df39
digitalLumberjack <digitallumberjack@gmail.com>
2022-03-27 13:43:00 +0200

    feat(rrgbd): fix automatic region selection for megadrive

--- projects/configgen/configgen/generators/libretro/crt/LibretroCoreConfigCRT.py
+++ projects/configgen/configgen/generators/libretro/crt/LibretroCoreConfigCRT.py
@@ -111,20 +111,20 @@ class LibretroCoreConfigCRT:
             if system.CRTVideoStandard in VideoStandardToRegionMap[system.Core]["values"]:
                 lines[VideoStandardToRegionMap[system.Core]["prop_name"]] = \
                     VideoStandardToRegionMap[system.Core]["values"][system.CRTVideoStandard]
-        if system.Name == "megadrive":
+        if system.Name == "megadrive" and system.CRTRegion != CRTRegion.AUTO:
             if system.Core in RegionToCoreRegionMap:
                 if system.CRTRegion in RegionToCoreRegionMap[system.Core]["values"]:
                     lines[RegionToCoreRegionMap[system.Core]["prop_name"]] = \
                         RegionToCoreRegionMap[system.Core]["values"][system.CRTRegion]
         if system.Name == "quake2":
             if system.CRTScreenType == CRTScreenType.k15:
-                    rez = "320x240"
+                rez = "320x240"
             else:
                 if system.CRTResolutionType == CRTResolutionType.DoubleFreq:
                     rez = "320x240"
                 else:
                     rez = "640x480"
-            lines["vitaquakeii_resolution"] =  '"{}"'.format(rez)
+            lines["vitaquakeii_resolution"] = '"{}"'.format(rez)
         log = "Forcing core configuration: "
         for config in lines.items():
             log = "{} {}={}".format(log, config[0], config[1]).replace('"', '')
