ecf127cb8f7fff698195d224e61df16833841063
digitalLumberjack <digitallumberjack@gmail.com>
2022-05-10 00:55:11 +0200

    feat(rrgbd): add specific systems.txt file for rpi3 and rpizero2

--- package/recalbox-crt/recalbox-crt.mk
+++ package/recalbox-crt/recalbox-crt.mk
@@ -7,6 +7,12 @@
 RECALBOX_CRT_VERSION = 1
 RECALBOX_CRT_LICENSE = MIT
 
+ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_RPI3)$(BR2_PACKAGE_RECALBOX_TARGET_RPIZERO2),y)
+RECALBOX_CRT_SYSTEM_FILE = systems.rpi3.txt
+else
+RECALBOX_CRT_SYSTEM_FILE = systems.txt
+endif
+
 define RECALBOX_CRT_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 644 $(RECALBOX_CRT_PKGDIR)/crt-config/empty-file.txt $(BINARIES_DIR)/rpi-firmware/crt/recalbox-crt-config.txt
 	$(INSTALL) -D -m 644 $(RECALBOX_CRT_PKGDIR)/crt-config/empty-file.txt $(BINARIES_DIR)/rpi-firmware/crt/recalbox-crt-options.cfg
@@ -15,7 +21,7 @@ define RECALBOX_CRT_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/recalbox/share_init/system/configs/crt/
 	echo "Place your extra CRT configurations in this directory." > $(TARGET_DIR)/recalbox/share_init/system/configs/crt/README.txt
 	$(INSTALL) -D -m 644 $(RECALBOX_CRT_PKGDIR)/crt-config/modes.txt $(TARGET_DIR)/recalbox/system/configs/crt/
-	$(INSTALL) -D -m 644 $(RECALBOX_CRT_PKGDIR)/crt-config/systems.txt $(TARGET_DIR)/recalbox/system/configs/crt/
+	$(INSTALL) -D -m 644 $(RECALBOX_CRT_PKGDIR)/crt-config/$(RECALBOX_CRT_SYSTEM_FILE) $(TARGET_DIR)/recalbox/system/configs/crt/systems.txt
 	$(INSTALL) -D -m 644 $(RECALBOX_CRT_PKGDIR)/crt-config/arcade_games.txt $(TARGET_DIR)/recalbox/system/configs/crt/
 endef
 
