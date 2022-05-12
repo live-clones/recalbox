8fd80855b55a43b1579492398c096dc21186bdf4
Bkg2k <bkg2k9@gmail.com>
2022-05-12 11:41:07 +0200

    fix(scraper): fix crash on loading invalid image data

--- projects/frontend/es-core/src/ImageIO.cpp
+++ projects/frontend/es-core/src/ImageIO.cpp
@@ -7,55 +7,64 @@ std::vector<unsigned char> ImageIO::loadFromMemoryRGBA32(const unsigned char * d
 	std::vector<unsigned char> rawData;
 	width = 0;
 	height = 0;
-	FIMEMORY * fiMemory = FreeImage_OpenMemory((BYTE *)data, (DWORD)size);
-	if (fiMemory != nullptr)
-	{
-		//detect the filetype from data
-		FREE_IMAGE_FORMAT format = FreeImage_GetFileTypeFromMemory(fiMemory);
-		if (format != FIF_UNKNOWN && (FreeImage_FIFSupportsReading(format) != 0))
-		{
-			//file type is supported. load image
-			FIBITMAP* fiBitmap = FreeImage_LoadFromMemory(format, fiMemory);
-			if (fiBitmap != nullptr)
-			{
-				//loaded. convert to 32bit if necessary
-				if (FreeImage_GetBPP(fiBitmap) != 32)
-				{
-					FIBITMAP* fiConverted = FreeImage_ConvertTo32Bits(fiBitmap);
-					if (fiConverted != nullptr)
-					{
-						//free original bitmap data
-						FreeImage_Unload(fiBitmap);
-						fiBitmap = fiConverted;
-					}
-				}
-        width = FreeImage_GetWidth(fiBitmap);
-        height = FreeImage_GetHeight(fiBitmap);
-        // loop through scanlines and add all pixel data to the return vector
-        // this is necessary, because width*height*bpp might not be == pitch
-        // do on-the-fly argb to abgr convertion
-        rawData.resize(width * height *4);
-        unsigned char* tempData = rawData.data();
-        int w = (int)width;
-        for (int y = (int)height; --y >= 0; )
+
+  try
+  {
+    FIMEMORY* fiMemory = FreeImage_OpenMemory((BYTE*) data, (DWORD) size);
+    if (fiMemory != nullptr)
+    {
+      //detect the filetype from data
+      FREE_IMAGE_FORMAT format = FreeImage_GetFileTypeFromMemory(fiMemory);
+      if (format != FIF_UNKNOWN && (FreeImage_FIFSupportsReading(format) != 0))
+      {
+        //file type is supported. load image
+        FIBITMAP* fiBitmap = FreeImage_LoadFromMemory(format, fiMemory);
+        if (fiBitmap != nullptr)
         {
-          unsigned int* argb = (unsigned int*)FreeImage_GetScanLine(fiBitmap, y);
-          unsigned int* abgr = (unsigned int*)(tempData + (y * width * 4));
-          for (int x = w ; --x >= 0;)
+          //loaded. convert to 32bit if necessary
+          if (FreeImage_GetBPP(fiBitmap) != 32)
           {
-            unsigned int c = argb[x];
-            abgr[x] = (c & 0xFF00FF00) | ((c & 0xFF) << 16) | ((c >> 16) & 0xFF);
+            FIBITMAP* fiConverted = FreeImage_ConvertTo32Bits(fiBitmap);
+            if (fiConverted != nullptr)
+            {
+              //free original bitmap data
+              FreeImage_Unload(fiBitmap);
+              fiBitmap = fiConverted;
+            }
           }
+          width = FreeImage_GetWidth(fiBitmap);
+          height = FreeImage_GetHeight(fiBitmap);
+          // loop through scanlines and add all pixel data to the return vector
+          // this is necessary, because width*height*bpp might not be == pitch
+          // do on-the-fly argb to abgr convertion
+          rawData.resize(width * height * 4);
+          unsigned char* tempData = rawData.data();
+          int w = (int)width;
+          for (int y = (int)height; --y >= 0;)
+          {
+            unsigned int* argb = (unsigned int*)FreeImage_GetScanLine(fiBitmap, y);
+            unsigned int* abgr = (unsigned int*)(tempData + (y * width * 4));
+            for (int x = w; --x >= 0;)
+            {
+              unsigned int c = argb[x];
+              abgr[x] = (c & 0xFF00FF00) | ((c & 0xFF) << 16) | ((c >> 16) & 0xFF);
+            }
+          }
+          //free bitmap data
+          FreeImage_Unload(fiBitmap);
         }
-        //free bitmap data
-        FreeImage_Unload(fiBitmap);
-			}
-			else { LOG(LogError) << "[Image] Error - Failed to load image from memory!"; }
-		}
-		else { LOG(LogError) << "[Image] Error - File type " << (format == FIF_UNKNOWN ? "unknown" : "unsupported") << "!"; }
-		//free FIMEMORY again
-		FreeImage_CloseMemory(fiMemory);
-	}
+        else { LOG(LogError) << "[Image] Error - Failed to load image from memory!"; }
+      }
+      else { LOG(LogError) << "[Image] Error - File type " << (format == FIF_UNKNOWN ? "unknown" : "unsupported") << "!"; }
+      //free FIMEMORY again
+      FreeImage_CloseMemory(fiMemory);
+    }
+  }
+  catch(std::exception& ex)
+  {
+    { LOG(LogError) << "[Image] Exception caught loading image data!"; }
+    { LOG(LogError) << "[Image] Exception: " << ex.what(); }
+  }
 	return rawData;
 }
 
