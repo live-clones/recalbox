5ae1fb00dc0adcf64e29951d3a242f24be513050
Bkg2k <bkg2k9@gmail.com>
2022-06-22 19:37:51 +0200

    fix(frontend): fix game renaming and scraping for easyrpg

---
+++ projects/frontend/es-app/src/games/adapter/GameAdapter.h
@@ -0,0 +1,53 @@
+//
+// Created by bkg2k on 22/06/22.
+//
+// As part of the RECALBOX Project
+// http://www.recalbox.com
+//
+#pragma once
+
+#include "games/FileData.h"
+
+/*!
+ * @brief Adapt some methods from FileData to handle special cases
+ * as for EasyRPG
+ */
+class GameAdapter
+{
+  public:
+    explicit GameAdapter(const FileData& filedata)
+      : mGame(filedata)
+    {
+    }
+
+    //! Direct accessor
+    const class FileData& FileData() const { return mGame; }
+
+    //! Get scraping name
+    std::string ScrapingName() const;
+
+    //! Get displayable name if the game has no name
+    std::string DisplayName() const;
+
+    //! Get rom size
+    long long RomSize();
+
+    /*!
+     * @brief Get the raw display filename if a rom from the given system
+     * @param system System the rom belongs to
+     * @param rompath Rom path
+     * @return Name or empty string
+     */
+    static std::string RawDisplayName(SystemData& system, const Path& rompath) ;
+
+  private:
+    //! Easy RPG system name
+    static const std::string sEasyRPGSystemName;
+    //! Easy RPG special name uppercase
+    static const std::string sEasyRPGGameNameUpper;
+    //! Easy RPG special name lowercase
+    static const std::string sEasyRPGGameNameLower;
+
+    //! Underlying FileData structure
+    const class FileData& mGame;
+};
