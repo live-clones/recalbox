11e48303b9bb5627c99aa122ebe1449810977765
digitalLumberjack <digitallumberjack@gmail.com>
2022-06-28 12:01:38 +0200

    feat(hardware): add dts for retroflag cases
    - support power led as exported led in /sys/class/leds
    - support reset and power buttons as keyboard events
    - support power out gpio for hardware shutdown

--- projects/recalbox-hardware/case/installer.py
+++ projects/recalbox-hardware/case/installer.py
@@ -7,8 +7,8 @@ MODULES = {
     "installers.nespi4.install": (cases.NESPI4,),
     "installers.nuxii.install": (cases.NUXII,),
     "installers.piboy.install": (cases.PIBOY,),
+    "installers.retroflags.install": (cases.PISTATION, cases.NESPICASEPLUS, cases.SUPERPICASE, cases.MEGAPICASE),
 }
-
 
 def processHardware(install, case, previousCase):
 
@@ -29,13 +29,14 @@ def processHardware(install, case, previousCase):
     # Call target installer/uninstaller
     for moduleName, caseNames in MODULES.items():
         if case in caseNames:
-            logger.hardlog("Install new case: " + case)
             module = __import__(moduleName, fromlist=["Install"])
             installClass = getattr(module, "Install")
             installer = installClass()
             if install:
+                logger.hardlog("Installing case hardware: " + case)
                 installReboot = installer.InstallHardware(case)
             else:
+                logger.hardlog("Uninstalling case hardware: " + case)
                 installReboot = installer.UninstallHardware(case)
 
     return uninstallReboot | installReboot
@@ -46,13 +47,14 @@ def processSoftware(install, case):
     # Call target installer/uninstaller
     for moduleName, caseNames in MODULES.items():
         if case in caseNames:
-            logger.hardlog("Install new case: " + case)
             module = __import__(moduleName, fromlist=["Install"])
             installClass = getattr(module, "Install")
             installer = installClass()
             if install:
+                logger.hardlog("Installing case software: " + case)
                 case = installer.InstallSoftware(case)
             else:
+                logger.hardlog("Uninstalling case software: " + case)
                 case = installer.UninstallSoftware(case)
 
     return case
@@ -62,7 +64,7 @@ def getInstallScript(case):
 
     for moduleName, caseNames in MODULES.items():
         if case in caseNames:
-            logger.hardlog("Get install picture: " + case)
+            logger.hardlog("Get install script: " + case)
             module = __import__(moduleName, fromlist=["Install"])
             installClass = getattr(module, "Install")
             installer = installClass()
---
+++ projects/recalbox-hardware/case/installers/retroflags/install.py
@@ -0,0 +1,62 @@
+import os
+import logger
+from installers.base.install import InstallBase
+
+
+class Install(InstallBase):
+
+    def __init__(self):
+        InstallBase.__init__(self)
+
+
+    def InstallHardware(self, case):
+        logger.hardlog("Installing Retroflag Case hardware")
+        try:
+            os.system("mount -o remount,rw /boot")
+            if os.system('echo -e "\\ndtoverlay=retroflag-case" >> /boot/recalbox-user-config.txt') != 0:
+                logger.hardlog("Retroflag: error adding dts to recalbox-user-config.txt")
+                return False
+        except Exception as e:
+            logger.hardlog("Retroflag: Exception = {}".format(e))
+            return False
+
+        finally:
+            os.system("mount -o remount,ro /boot")
+
+        logger.hardlog("Retroflag Case hardware installed successfully!")
+        return True
+
+
+    def InstallSoftware(self, case):
+        logger.hardlog("Installing Retroflag Case software")
+        return case
+
+
+    def UninstallHardware(self, case):
+        logger.hardlog("Uninstalling Retroflag Case hardware")
+        try:
+            os.system("mount -o remount,rw /boot")
+            # Uninstall /boot/recalbox-user-config.txt
+            if os.system('sed -i "s|dtoverlay=retroflag-case||g" /boot/recalbox-user-config.txt') != 0:
+                logger.hardlog("Retroflag: Error removing kms driver")
+                return False
+            logger.hardlog("Retroflag: kms driver removed")
+
+        except Exception as e:
+            logger.hardlog("Retroflag: Exception = {}".format(e))
+            return False
+
+        finally:
+            os.system("mount -o remount,ro /boot")
+
+        return True
+
+
+    def UninstallSoftware(self, case):
+        logger.hardlog("Uninstalling Retroflag Case software")
+        return ""
+
+
+    def GetInstallScript(self, case):
+
+        return None
