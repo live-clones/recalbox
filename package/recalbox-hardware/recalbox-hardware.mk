917297898d3587dc5a8859c643930f62f823e2f5
digitalLumberjack <digitallumberjack@gmail.com>
2022-06-29 10:15:34 +0200

    fix: hardware for PC

--- package/recalbox-hardware/recalbox-hardware.mk
+++ package/recalbox-hardware/recalbox-hardware.mk
@@ -13,7 +13,6 @@ RECALBOX_HARDWARE_DEPENDENCIES = recalbox-themes host-python3 linux
 
 define RECALBOX_HARDWARE_BUILD_CMDS
 	PYTHONPATH="$(PYTHON3_PATH)" $(HOST_DIR)/bin/python$(PYTHON3_VERSION_MAJOR) -m compileall $(@D)
-	$(HOST_DIR)/bin/linux-dtc $(@D)/case/installers/retroflags/assets/overlays/retroflag-case.dts -o $(@D)/retroflag-case.dtbo
 endef
 
 define RECALBOX_HARDWARE_INSTALL_TARGET_CMDS
@@ -27,9 +26,6 @@ define RECALBOX_HARDWARE_INSTALL_TARGET_CMDS
 		https://github.com/ian57/dpi-pizero-gpicase/raw/master/dpi-pizero-gpicase.dtbo
 	wget -P $(TARGET_DIR)/recalbox/system/hardware/case/installers/gpi/assets/overlays/ \
 		https://github.com/ian57/pwm-audio-pizero-gpicase/raw/master/pwm-audio-pizero-gpicase.dtbo
-	$(HOST_DIR)/bin/linux-dtc $(@D)/case/installers/retroflags/assets/overlays/retroflag-case.dts -o $(@D)/retroflag-case.dtbo
-	$(INSTALL) -D -m 0644 $(@D)/retroflag-case.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/retroflag-case.dtbo
-
 endef
 
 $(eval $(generic-package))
