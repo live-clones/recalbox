6b47d8305006651a470ae753ae7ba648801310e9
Pit64 <yrigaud@icloud.com>
2022-09-18 00:33:41 +0200

    feat(kodi): update script modules to their latest version
    - Most of the modules were for Kodi Krypton, which seems to be Kodi 17
    - All of the resources, script modules and plugins are using the latest version
    - t0mm0.common could be removed, it seems it's not developed anymore and doesn't exist for Kodi 19+
    - Added new default Kodi configuration to avoid to retype plugins username/password on each update
    - Added all missing languages

--- package/kodi-resource-language-tr_tr/kodi_resource_language_tr_tr.mk
+++ package/kodi-resource-language-tr_tr/kodi_resource_language_tr_tr.mk
@@ -4,16 +4,16 @@
 #
 ################################################################################
 
-KODI_RESOURCE_LANGUAGE_TR_TR_VERSION = 3.0.11
+KODI_RESOURCE_LANGUAGE_TR_TR_VERSION = 9.0.37
 KODI_RESOURCE_LANGUAGE_TR_TR_SOURCE = resource.language.tr_tr-$(KODI_RESOURCE_LANGUAGE_TR_TR_VERSION).zip
-KODI_RESOURCE_LANGUAGE_TR_TR_SITE = http://mirrors.kodi.tv/addons/krypton/resource.language.tr_tr
+KODI_RESOURCE_LANGUAGE_TR_TR_SITE = http://mirrors.kodi.tv/addons/matrix/resource.language.tr_tr
 KODI_RESOURCE_LANGUAGE_TR_TR_PLUGINNAME=resource.language.tr_tr
 KODI_RESOURCE_LANGUAGE_TR_TR_LICENSE = COPYRIGHT
 
 KODI_RESOURCE_LANGUAGE_TR_TR_PLUGIN_TARGET_DIR=$(TARGET_DIR)/usr/share/kodi/addons
 
 define KODI_RESOURCE_LANGUAGE_TR_TR_EXTRACT_CMDS
-	@unzip -q -o $(DL_DIR)/$(KODI_RESOURCE_LANGUAGE_TR_TR_SOURCE) -d $(@D)
+	@unzip -q -o $(DL_DIR)/kodi-resource-language-tr_tr/$(KODI_RESOURCE_LANGUAGE_TR_TR_SOURCE) -d $(@D)
 endef
 
 define KODI_RESOURCE_LANGUAGE_TR_TR_INSTALL_TARGET_CMDS
