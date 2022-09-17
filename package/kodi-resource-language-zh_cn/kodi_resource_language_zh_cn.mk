7d4697a99618e16e534fbbf9949c1442127c708a
Pit64 <yrigaud@icloud.com>
2022-09-18 00:33:41 +0200

    feat(kodi): update script modules to their latest version
    - Most of the modules were for Kodi Krypton, which seems to be Kodi 17
    - All of the resources, script modules and plugins are using the latest version
    - t0mm0.common could be removed, it seems it's not developed anymore and doesn't exist for Kodi 19+
    - Added new default Kodi configuration to avoid to retype plugins username/password on each update
    - Added all missing languages

--- package/kodi-resource-language-zh_cn/kodi_resource_language_zh_cn.mk
+++ package/kodi-resource-language-zh_cn/kodi_resource_language_zh_cn.mk
@@ -4,16 +4,16 @@
 #
 ################################################################################
 
-KODI_RESOURCE_LANGUAGE_ZH_CN_VERSION = 3.0.11
+KODI_RESOURCE_LANGUAGE_ZH_CN_VERSION = 9.0.39
 KODI_RESOURCE_LANGUAGE_ZH_CN_SOURCE = resource.language.zh_cn-$(KODI_RESOURCE_LANGUAGE_ZH_CN_VERSION).zip
-KODI_RESOURCE_LANGUAGE_ZH_CN_SITE = http://mirrors.kodi.tv/addons/krypton/resource.language.zh_cn
+KODI_RESOURCE_LANGUAGE_ZH_CN_SITE = http://mirrors.kodi.tv/addons/matrix/resource.language.zh_cn
 KODI_RESOURCE_LANGUAGE_ZH_CN_PLUGINNAME=resource.language.zh_cn
 KODI_RESOURCE_LANGUAGE_ZH_CN_LICENSE = COPYRIGHT
 
 KODI_RESOURCE_LANGUAGE_ZH_CN_PLUGIN_TARGET_DIR=$(TARGET_DIR)/usr/share/kodi/addons
 
 define KODI_RESOURCE_LANGUAGE_ZH_CN_EXTRACT_CMDS
-	@unzip -q -o $(DL_DIR)/$(KODI_RESOURCE_LANGUAGE_ZH_CN_SOURCE) -d $(@D)
+	@unzip -q -o $(DL_DIR)/kodi-resource-language-zh_cn/$(KODI_RESOURCE_LANGUAGE_ZH_CN_SOURCE) -d $(@D)
 endef
 
 define KODI_RESOURCE_LANGUAGE_ZH_CN_INSTALL_TARGET_CMDS
