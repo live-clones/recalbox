c7920c7a85e4276fb0f7cad1f2468ce6e3e43657
Pit64 <yrigaud@icloud.com>
2022-09-18 00:33:41 +0200

    feat(kodi): update script modules to their latest version
    - Most of the modules were for Kodi Krypton, which seems to be Kodi 17
    - All of the resources, script modules and plugins are using the latest version
    - t0mm0.common could be removed, it seems it's not developed anymore and doesn't exist for Kodi 19+
    - Added new default Kodi configuration to avoid to retype plugins username/password on each update
    - Added all missing languages

--- package/kodi-resource-language-fr_fr/kodi_resource_language_fr_fr.mk
+++ package/kodi-resource-language-fr_fr/kodi_resource_language_fr_fr.mk
@@ -4,16 +4,16 @@
 #
 ################################################################################
 
-KODI_RESOURCE_LANGUAGE_FR_FR_VERSION = 3.0.15
+KODI_RESOURCE_LANGUAGE_FR_FR_VERSION = 9.0.52
 KODI_RESOURCE_LANGUAGE_FR_FR_SOURCE = resource.language.fr_fr-$(KODI_RESOURCE_LANGUAGE_FR_FR_VERSION).zip
-KODI_RESOURCE_LANGUAGE_FR_FR_SITE = http://mirrors.kodi.tv/addons/krypton/resource.language.fr_fr
+KODI_RESOURCE_LANGUAGE_FR_FR_SITE = http://mirrors.kodi.tv/addons/matrix/resource.language.fr_fr
 KODI_RESOURCE_LANGUAGE_FR_FR_PLUGINNAME=resource.language.fr_fr
 KODI_RESOURCE_LANGUAGE_FR_FR_LICENSE = COPYRIGHT
 
 KODI_RESOURCE_LANGUAGE_FR_FR_PLUGIN_TARGET_DIR=$(TARGET_DIR)/usr/share/kodi/addons
 
 define KODI_RESOURCE_LANGUAGE_FR_FR_EXTRACT_CMDS
-	@unzip -q -o $(DL_DIR)/$(KODI_RESOURCE_LANGUAGE_FR_FR_SOURCE) -d $(@D)
+	@unzip -q -o $(DL_DIR)/kodi-resource-language-fr_fr/$(KODI_RESOURCE_LANGUAGE_FR_FR_SOURCE) -d $(@D)
 endef
 
 define KODI_RESOURCE_LANGUAGE_FR_FR_INSTALL_TARGET_CMDS
