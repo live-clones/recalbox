37ca222b3b242cb0bf989b6f70b4e302ff7e6d90
David Barbion <davidb@230ruedubac.fr>
2022-09-23 23:18:45 +0200

    feat(joystick): bump hid-nintendo

--- package/hid-nintendo/hid-nintendo.mk
+++ package/hid-nintendo/hid-nintendo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HID_NINTENDO_VERSION = 86e53442872878e79cb454f5a16ccd1c9716f47f
+HID_NINTENDO_VERSION = 2712136b19eed75bff01c1a6ffe2a23daf78a7bb
 HID_NINTENDO_SITE = $(call github,nicman23,dkms-hid-nintendo,$(HID_NINTENDO_VERSION))
 HID_NINTENDO_LICENSE = GPL-2.0
 HID_NINTENDO_LICENSE_FILES = LICENSE
