612faa16424074e6c7afc6cea996010fab16d498
digitalLumberjack <digitallumberjack@gmail.com>
2022-10-30 16:22:55 +0100

    feat(system): rrgbd: allow to set priority on hdmi when a hdmi cable is plugged

--- projects/frontend/es-app/src/guis/MenuMessages.h
+++ projects/frontend/es-app/src/guis/MenuMessages.h
@@ -167,6 +167,7 @@ class MenuMessages
     #define MENUMESSAGE_RECALBOXRGBDUAL_HELP_MSG "Recalbox RGB Dual options and configuration."
     #define MENUMESSAGE_ADVANCED_CRT_HORIZONTAL_PAL_OFFSET_HELP_MSG "If you PAL images are not centered, you can override the default horizontal offset here."
     #define MENUMESSAGE_ADVANCED_CRT_VERTICAL_PAL_OFFSET_HELP_MSG "If you PAL images are not centered, you can override the default vertical offset here."
+    #define MENUMESSAGE_ADVANCED_CRT_FORCE_HDMI_HELP_MSG "When a HDMI cable is connected, use HDMI output in priority."
 
     #define MENUMESSAGE_ADVANCED_RESOLUTION_HELP_MSG "Select system, frontend and emulator resolutions."
     #define MENUMESSAGE_ADVANCED_RESOLUTION_GLOBAL_HELP_MSG "Global resolution is the resolution used by default when specific resolutions are undefined."
