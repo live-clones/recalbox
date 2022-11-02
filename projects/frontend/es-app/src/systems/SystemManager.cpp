d5f4b1136f18767f62a1408e98d1d07dd2c94743
Bkg2k <bkg2k9@gmail.com>
2022-11-02 08:08:58 +0100

    feat(frontend): add $system-x/$system+x meta-variables in theme's xml

--- projects/frontend/es-app/src/systems/SystemManager.cpp
+++ projects/frontend/es-app/src/systems/SystemManager.cpp
@@ -836,6 +836,10 @@ bool SystemManager::LoadSystemConfigurations(FileNotifier& gamelistWatcher, bool
       if (path.Exists())
         gamelistWatcher.WatchFile(path);
 
+  ThemeData::SetThemeChanged(true);
+  for(SystemData* system : mAllSystemVector)
+    system->loadTheme();
+
   return true;
 }
 
