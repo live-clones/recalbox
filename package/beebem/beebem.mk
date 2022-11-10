138490c37a612b86b67ae7cc509784926a3c5d44
David Barbion <davidb@230ruedubac.fr>
2022-11-10 10:18:56 +0100

    chore: replaced freeimage package in profit of libfreeimage
    The last one is shipped in buildroot. A little patch for PNG
    has been ported.

--- package/beebem/beebem.mk
+++ package/beebem/beebem.mk
@@ -8,7 +8,7 @@ BEEBEM_VERSION = 8a67ac0eb400e92b7a01171ed571342ad5a54030
 BEEBEM_SITE = https://gitlab-ci-token-beebem:$(GITLAB_TOKEN_BEEBEM)@gitlab.com/bkg2k/beebem-linux.git
 BEEBEM_SITE_METHOD = git
 BEEBEM_LICENSE = GPL
-BEEBEM_DEPENDENCIES = sdl2 sdl2_ttf zlib freeimage
+BEEBEM_DEPENDENCIES = sdl2 sdl2_ttf zlib libfreeimage
 
 ifeq ($(BR2_PACKAGE_HAS_LIBGL),y)
 BEEBEM_DEPENDENCIES += libgl
