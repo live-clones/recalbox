f2877b938d6433fe2a88098ab5533127d22fda26
gugueU <gugue_u@hotmail.com>
2022-11-18 12:12:16 +0100

    feat(theme): update theme version for gameclip in 240p

--- package/recalbox-themes/recalbox-themes.mk
+++ package/recalbox-themes/recalbox-themes.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 ifneq ($(GITLAB_TOKEN_THEMES),)
-RECALBOX_THEMES_VERSION = 0e1bec87d6084324dd53b2fd0f3b26d99c3ff2cc
+RECALBOX_THEMES_VERSION = 0fedf4530b33a1b4a03dcec4683f9b1f882642eb
 RECALBOX_THEMES_SITE = https://gitlab-ci-token:$(GITLAB_TOKEN_THEMES)@gitlab.com/recalbox/recalbox-themes-prime
 else
 RECALBOX_THEMES_VERSION = dd10b43c5bf48861274a77af9e604b1fa5bcc2ae
