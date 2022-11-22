84b2589c24384786dc1654a6b283321ef044f8e9
Bkg2k <bkg2k9@gmail.com>
2022-11-22 08:51:38 +0100

    feat(standalone): bump amiberry to v5.4

--- package/amiberry/amiberry.mk
+++ package/amiberry/amiberry.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AMIBERRY_VERSION = v5.3
+AMIBERRY_VERSION = v5.4
 AMIBERRY_SITE = $(call gitlab,recalbox,packages/standalone/amiberry,$(AMIBERRY_VERSION))
 AMIBERRY_DEPENDENCIES = sdl2 sdl2_image sdl2_ttf libcapsimage libmpeg2 mpg123 flac
 AMIBERRY_LICENSE = GPL-3.0
@@ -21,7 +21,7 @@ AMIBERRY_PLATFORM=rpi1-sdl2
 else ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_ODROIDXU4),y)
 AMIBERRY_PLATFORM=xu4
 else ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_ODROIDGO2),y)
-AMIBERRY_PLATFORM=go-advance
+AMIBERRY_PLATFORM=oga
 endif
 
 define AMIBERRY_BUILD_CMDS
