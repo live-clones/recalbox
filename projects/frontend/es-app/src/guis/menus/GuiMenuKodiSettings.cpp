d1d67109d6f50e30b44d158840a888996ec2b6be
digitalLumberjack <digitallumberjack@gmail.com>
2022-12-16 11:08:30 +0100

    feat(frontend): add kodi resolution selection menu

--- projects/frontend/es-app/src/guis/menus/GuiMenuKodiSettings.cpp
+++ projects/frontend/es-app/src/guis/menus/GuiMenuKodiSettings.cpp
@@ -6,6 +6,7 @@
 //
 
 #include "GuiMenuKodiSettings.h"
+#include "ResolutionAdapter.h"
 #include <components/SwitchComponent.h>
 #include <WindowManager.h>
 #include <utils/locale/LocaleHelper.h>
@@ -16,6 +17,9 @@ GuiMenuKodiSettings::GuiMenuKodiSettings(WindowManager& window)
 {
   // Enable Kodi
   mKodiEnabled = AddSwitch(_("ENABLE KODI"), RecalboxConf::Instance().GetKodiEnabled(), (int)Components::Enabled, this, _(MENUMESSAGE_ADVANCED_KODI_ENABLE_HELP_MSG));
+
+  // Select kodi video mode
+  AddList<std::string>(_("KODI RESOLUTION"), (int)Components::KodiResolution, this, GetKodiResolutionsEntries(), _(MENUMESSAGE_ADVANCED_RESOLUTION_KODI_HELP_MSG));
 
   // Run Kodi at startup
   mKodiAtStartup = AddSwitch(_("KODI AT START"), RecalboxConf::Instance().GetKodiAtStartup(), (int)Components::RunAtStartup, this, _(MENUMESSAGE_ADVANCED_KODI_AT_START_HELP_MSG));
@@ -31,5 +35,35 @@ void GuiMenuKodiSettings::SwitchComponentChanged(int id, bool status)
     case Components::Enabled: RecalboxConf::Instance().SetKodiEnabled(status).Save(); break;
     case Components::RunAtStartup: RecalboxConf::Instance().SetKodiAtStartup(status).Save(); break;
     case Components::ButtonX: RecalboxConf::Instance().SetKodiXButton(status).Save(); break;
+    default: break;
+  }
+}
+
+
+std::vector<GuiMenuBase::ListEntry<std::string>> GuiMenuKodiSettings::GetKodiResolutionsEntries()
+{
+  std::vector<GuiMenuBase::ListEntry<std::string>> result;
+  result.push_back({ _("NATIVE"), "default", RecalboxConf::Instance().GetKodiVideoMode() == "default" });
+  result.push_back({ _("USE GLOBAL"), "", !RecalboxConf::Instance().IsDefinedKodiVideoMode() });
+  for(const ResolutionAdapter::Resolution& resolution : mResolutionAdapter.Resolutions(false))
+  {
+    std::string reso = resolution.ToRawString();
+    result.push_back({ resolution.ToString(), reso, reso == RecalboxConf::Instance().GetKodiVideoMode() });
+  }
+  return result;
+}
+
+void GuiMenuKodiSettings::OptionListComponentChanged(int id, int index, const std::string& value)
+{
+  (void)index;
+  switch((Components)id)
+  {
+    case Components::KodiResolution:
+    {
+      if (value.empty()) RecalboxConf::Instance().DeleteKodiVideoMode().Save();
+      else RecalboxConf::Instance().SetKodiVideoMode(value).Save();
+      break;
+    }
+    default: break;
   }
 }
