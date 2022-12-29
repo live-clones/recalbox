bbcffbe620edbce8e08e3159fb81f8107d859af9
David Barbion <davidb@230ruedubac.fr>
2022-12-29 18:15:19 +0100

    feat: add support for volume control of rg353x with volumed

--- package/recalbox-volumed/recalbox-volumed.mk
+++ package/recalbox-volumed/recalbox-volumed.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_VOLUMED_VERSION = 0dc27498eb4e5503921128367fe86b103a983f2c
+RECALBOX_VOLUMED_VERSION = 2cf5a6c6e64bc0434c837addceb2d13f9bf245ff
 RECALBOX_VOLUMED_SITE = https://gitlab.com/recalbox/recalbox-volumed.git
 RECALBOX_VOLUMED_SITE_METHOD = git
 RECALBOX_VOLUMED_LICENSE = GPL-3.0
