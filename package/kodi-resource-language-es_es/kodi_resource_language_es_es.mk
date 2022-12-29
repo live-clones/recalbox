748c4bdd67ace613616c59b78063669225c25698
Pit64 <yrigaud@icloud.com>
2022-12-29 19:42:25 +0100

    feat(kodi): update modules
    - updated modules to use the latest version
    - reorganized the modules .mk files to integrate the release and plugin name more included

--- package/kodi-resource-language-es_es/kodi_resource_language_es_es.mk
+++ package/kodi-resource-language-es_es/kodi_resource_language_es_es.mk
@@ -1,13 +1,14 @@
 ################################################################################
 #
-# kodi es_es language resource
+# kodi spanish language resource
 #
 ################################################################################
 
+KODI_RESOURCE_LANGUAGE_ES_ES_RELEASE = matrix
 KODI_RESOURCE_LANGUAGE_ES_ES_VERSION = 9.0.44
-KODI_RESOURCE_LANGUAGE_ES_ES_SOURCE = resource.language.es_es-$(KODI_RESOURCE_LANGUAGE_ES_ES_VERSION).zip
-KODI_RESOURCE_LANGUAGE_ES_ES_SITE = http://mirrors.kodi.tv/addons/matrix/resource.language.es_es
-KODI_RESOURCE_LANGUAGE_ES_ES_PLUGINNAME=resource.language.es_es
+KODI_RESOURCE_LANGUAGE_ES_ES_PLUGINNAME = resource.language.es_es
+KODI_RESOURCE_LANGUAGE_ES_ES_SOURCE = $(KODI_RESOURCE_LANGUAGE_ES_ES_PLUGINNAME)-$(KODI_RESOURCE_LANGUAGE_ES_ES_VERSION).zip
+KODI_RESOURCE_LANGUAGE_ES_ES_SITE = https://mirrors.kodi.tv/addons/$(KODI_RESOURCE_LANGUAGE_ES_ES_RELEASE)/$(KODI_RESOURCE_LANGUAGE_ES_ES_PLUGINNAME)
 KODI_RESOURCE_LANGUAGE_ES_ES_LICENSE = COPYRIGHT
 
 KODI_RESOURCE_LANGUAGE_ES_ES_PLUGIN_TARGET_DIR=$(TARGET_DIR)/usr/share/kodi/addons
