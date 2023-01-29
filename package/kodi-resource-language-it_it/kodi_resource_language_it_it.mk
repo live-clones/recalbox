df198eec70682b2cf16d6a15fdc50dc730d377c7
Pit64 <yrigaud@icloud.com>
2023-01-29 09:39:26 +0100

    fix(kodi): fix languages versions

--- package/kodi-resource-language-it_it/kodi_resource_language_it_it.mk
+++ package/kodi-resource-language-it_it/kodi_resource_language_it_it.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_RESOURCE_LANGUAGE_IT_IT_VERSION = 9.0.43
+KODI_RESOURCE_LANGUAGE_IT_IT_VERSION = 9.0.44
 KODI_RESOURCE_LANGUAGE_IT_IT_SOURCE = resource.language.it_it-$(KODI_RESOURCE_LANGUAGE_IT_IT_VERSION).zip
 KODI_RESOURCE_LANGUAGE_IT_IT_SITE = http://mirrors.kodi.tv/addons/matrix/resource.language.it_it
 KODI_RESOURCE_LANGUAGE_IT_IT_PLUGINNAME=resource.language.it_it
