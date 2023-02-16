d229d297bdc83dc0cb157c87dc2c448d02276d7c
digitalLumberjack <digitallumberjack@gmail.com>
2023-02-16 14:51:57 +0100

    fix(frontend): cases: fix RaspberryPiTouchDisplay case having a disabled shutdown menu
    - refactor the meaning of the variable and macro menuShutdownEnabled MENU_SHUTDOWN_ENABLED

--- projects/frontend/es-core/src/hardware/Case.cpp
+++ projects/frontend/es-core/src/hardware/Case.cpp
@@ -78,39 +78,39 @@ Case Case::Create(CaseModel model)
   switch (model)
   {
     case CaseModel::None:
-      return Case(CaseModel::None, !CASE_DETECTION_AUTOMATIC, CASE_SHUTDOWN_SUPPORTED, _("NONE"), "", "");
+      return Case(CaseModel::None, !CASE_DETECTION_AUTOMATIC, MENU_SHUTDOWN_ENABLED, _("NONE"), "", "");
     case CaseModel::GPiV1:
-      return Case(CaseModel::GPiV1, CASE_DETECTION_AUTOMATIC, CASE_SHUTDOWN_SUPPORTED, "Gpi Case (v1)", "GPiV1","");
+      return Case(CaseModel::GPiV1, CASE_DETECTION_AUTOMATIC, MENU_SHUTDOWN_ENABLED, "Gpi Case (v1)", "GPiV1", "");
     case CaseModel::GPiV2:
-      return Case(CaseModel::GPiV2, CASE_DETECTION_AUTOMATIC, CASE_SHUTDOWN_SUPPORTED, "Gpi Case (v2)", "GPiV2", "");
+      return Case(CaseModel::GPiV2, CASE_DETECTION_AUTOMATIC, MENU_SHUTDOWN_ENABLED, "Gpi Case (v2)", "GPiV2", "");
     case CaseModel::GPiV3:
-      return Case(CaseModel::GPiV3, CASE_DETECTION_AUTOMATIC, CASE_SHUTDOWN_SUPPORTED, "Gpi Case (v3)", "GPiV3", "");
+      return Case(CaseModel::GPiV3, CASE_DETECTION_AUTOMATIC, MENU_SHUTDOWN_ENABLED, "Gpi Case (v3)", "GPiV3", "");
     case CaseModel::GPi2:
-      return Case(CaseModel::GPi2, CASE_DETECTION_AUTOMATIC, CASE_SHUTDOWN_SUPPORTED, "Gpi Case 2", "GPi2", "");
+      return Case(CaseModel::GPi2, CASE_DETECTION_AUTOMATIC, MENU_SHUTDOWN_ENABLED, "Gpi Case 2", "GPi2", "");
     case CaseModel::GPi2W:
-      return Case(CaseModel::GPi2W, CASE_DETECTION_AUTOMATIC, CASE_SHUTDOWN_SUPPORTED, "Gpi Case 2W", "GPi2W", "");
+      return Case(CaseModel::GPi2W, CASE_DETECTION_AUTOMATIC, MENU_SHUTDOWN_ENABLED, "Gpi Case 2W", "GPi2W", "");
     case CaseModel::Nuxii:
-      return Case(CaseModel::Nuxii, CASE_DETECTION_AUTOMATIC, CASE_SHUTDOWN_SUPPORTED, "Nuxii", "Nuxii", "");
+      return Case(CaseModel::Nuxii, CASE_DETECTION_AUTOMATIC, MENU_SHUTDOWN_ENABLED, "Nuxii", "Nuxii", "");
     case CaseModel::PiBoy:
-      return Case(CaseModel::PiBoy, CASE_DETECTION_AUTOMATIC, CASE_SHUTDOWN_SUPPORTED, "PiBoy DMG", "PiBoy", "");
+      return Case(CaseModel::PiBoy, CASE_DETECTION_AUTOMATIC, MENU_SHUTDOWN_ENABLED, "PiBoy DMG", "PiBoy", "");
     case CaseModel::Nespi4Case:
-      return Case(CaseModel::Nespi4Case, CASE_DETECTION_AUTOMATIC, !CASE_SHUTDOWN_SUPPORTED, "Nespi4Case", "NESPi4", "");
+      return Case(CaseModel::Nespi4Case, CASE_DETECTION_AUTOMATIC, !MENU_SHUTDOWN_ENABLED, "Nespi4Case", "NESPi4", "");
     case CaseModel::Nespi4CaseManual:
-      return Case(CaseModel::Nespi4CaseManual, !CASE_DETECTION_AUTOMATIC, !CASE_SHUTDOWN_SUPPORTED, "Nespi4Case (Retroflag)", "NESPi4Manual", retroflagInstallMessage);
+      return Case(CaseModel::Nespi4CaseManual, !CASE_DETECTION_AUTOMATIC, !MENU_SHUTDOWN_ENABLED, "Nespi4Case (Retroflag)", "NESPi4Manual", retroflagInstallMessage);
     case CaseModel::SuperPi4Case:
-      return Case(CaseModel::SuperPi4Case, !CASE_DETECTION_AUTOMATIC, !CASE_SHUTDOWN_SUPPORTED, "SuperPi4Case (Retroflag)", "SuperPi4Case", retroflagInstallMessage);
+      return Case(CaseModel::SuperPi4Case, !CASE_DETECTION_AUTOMATIC, !MENU_SHUTDOWN_ENABLED, "SuperPi4Case (Retroflag)", "SuperPi4Case", retroflagInstallMessage);
     case CaseModel::NespiCasePlus:
-      return Case(CaseModel::NespiCasePlus, !CASE_DETECTION_AUTOMATIC, !CASE_SHUTDOWN_SUPPORTED, "Nespi Case + (Retroflag)", "NespiCasePlus", retroflagInstallMessage);
+      return Case(CaseModel::NespiCasePlus, !CASE_DETECTION_AUTOMATIC, !MENU_SHUTDOWN_ENABLED, "Nespi Case + (Retroflag)", "NespiCasePlus", retroflagInstallMessage);
     case CaseModel::PiStation:
-      return Case(CaseModel::PiStation, !CASE_DETECTION_AUTOMATIC, !CASE_SHUTDOWN_SUPPORTED, "PiStation (Retroflag)", "PiStation", retroflagInstallMessage);
+      return Case(CaseModel::PiStation, !CASE_DETECTION_AUTOMATIC, !MENU_SHUTDOWN_ENABLED, "PiStation (Retroflag)", "PiStation", retroflagInstallMessage);
     case CaseModel::SuperPiCase:
-      return Case(CaseModel::SuperPiCase, !CASE_DETECTION_AUTOMATIC, !CASE_SHUTDOWN_SUPPORTED, "Super Pi Case (Retroflag)", "SuperPiCase", retroflagInstallMessage);
+      return Case(CaseModel::SuperPiCase, !CASE_DETECTION_AUTOMATIC, !MENU_SHUTDOWN_ENABLED, "Super Pi Case (Retroflag)", "SuperPiCase", retroflagInstallMessage);
     case CaseModel::MegaPiCase:
-      return Case(CaseModel::MegaPiCase, !CASE_DETECTION_AUTOMATIC, !CASE_SHUTDOWN_SUPPORTED, "Mega Pi Case (Retroflag)", "MegaPiCase", retroflagInstallMessage);
+      return Case(CaseModel::MegaPiCase, !CASE_DETECTION_AUTOMATIC, !MENU_SHUTDOWN_ENABLED, "Mega Pi Case (Retroflag)", "MegaPiCase", retroflagInstallMessage);
     case CaseModel::ArgonOne:
-      return Case(CaseModel::ArgonOne, !CASE_DETECTION_AUTOMATIC, CASE_SHUTDOWN_SUPPORTED, "Argon One (Argon40)", "ArgonOne", "");
+      return Case(CaseModel::ArgonOne, !CASE_DETECTION_AUTOMATIC, MENU_SHUTDOWN_ENABLED, "Argon One (Argon40)", "ArgonOne", "");
     case CaseModel::RaspberryPiTouchDisplay:
-      return Case(CaseModel::RaspberryPiTouchDisplay, !CASE_DETECTION_AUTOMATIC, !CASE_SHUTDOWN_SUPPORTED, "Raspberry Pi Touch Display", "RaspberryPiTouchDisplay", "");
+      return Case(CaseModel::RaspberryPiTouchDisplay, !CASE_DETECTION_AUTOMATIC, MENU_SHUTDOWN_ENABLED, "Raspberry Pi Touch Display", "RaspberryPiTouchDisplay", "");
   }
   return Case(CaseModel::None, false, true, _("NONE"), "", "");
 }
