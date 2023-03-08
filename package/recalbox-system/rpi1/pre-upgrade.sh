6fc90582d3c5b9abdcb3512260ca352abbb6a071
digitalLumberjack <digitallumberjack@gmail.com>
2023-03-08 16:54:53 +0100

    feat(system): gpicase2w: add specific image for gpicase2w + rpizero support
    - the image contains a preconfigurer recalbox-user-config.txt with gpicase2w waiting to be installed

--- package/recalbox-system/rpi1/pre-upgrade.sh
+++ package/recalbox-system/rpi1/pre-upgrade.sh
@@ -31,10 +31,6 @@ hdmi_force_hotplug=1
 EOF
 fi
 
-# this next patch can removed post 7.2 as the case
-# reset will be implemented in the caller script
-sed -i -E 's/^case=/;case=/' /boot/recalbox-boot.conf
-
 # Remove old dats files from bios
 rm -rf /recalbox/share/bios/dc/*.dat.zip
 rm -rf /recalbox/share/bios/mame/*.dat.zip
