64278b9f0665a9fccbd29c5925f2db31bfdf48bd
Pit64 <yrigaud@icloud.com>
2023-03-08 16:17:01 +0100

    feat(cores): add lr-dirksimple for daphne

---
+++ package/libretro-dirksimple/libretro-dirksimple.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# LIBRETRO DIRKSIMPLE
+#
+################################################################################
+
+LIBRETRO_DIRKSIMPLE_VERSION = 2b9f9fcc72df1d505b42c1194637a32cec1b82cb
+LIBRETRO_DIRKSIMPLE_SITE = $(call github,icculus,dirksimple,$(LIBRETRO_DIRKSIMPLE_VERSION))
+LIBRETRO_DIRKSIMPLE_DEPENDENCIES = libmpeg2 libogg libvorbis sdl2 zlib
+LIBRETRO_DIRKSIMPLE_LICENSE = ZLIB
+LIBRETRO_DIRKSIMPLE_LICENSE_FILES = LICENSE.TXT
+
+LIBRETRO_DIRKSIMPLE_PLATFORM=$(RETROARCH_LIBRETRO_PLATFORM)
+
+LIBRETRO_DIRKSIMPLE_CONF_OPTS += -DLIBRETRO=ON
+LIBRETRO_DIRKSIMPLE_CONF_OPTS += -DDIRKSIMPLE_FORCE_BASE_DIR=""/recalbox/share/bios/daphne/DirkSimple"
+
+define LIBRETRO_DIRKSIMPLE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/dirksimple_libretro.so $(TARGET_DIR)/usr/lib/libretro/dirksimple_libretro.so
+	mkdir -p $(TARGET_DIR)/recalbox/share_upgrade/bios/daphne/DirkSimple
+	cp -R $(@D)/data $(TARGET_DIR)/recalbox/share_upgrade/bios/daphne/DirkSimple
+endef
+
+$(eval $(cmake-package))
